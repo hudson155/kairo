83ce796c75e7ed5f0c8b740b88e3c2f9ed06929b
Jeff Hudson <jhudson@jhudson.ca>
2019-10-04 13:25:20 -0600

    Delete frontend

--- limber-frontend/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { h, render } from 'preact';
-import App from './App/App';
-import './index.css';
-
-render(<App />, document.getElementById('root') as HTMLElement);
-
-if (module.hot) {
-  module.hot.accept();
-}
