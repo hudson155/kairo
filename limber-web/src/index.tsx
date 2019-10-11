bfa745a4eaf8ef2e7263c55a648fdb016ca557a5
Jeff Hudson <jeff.hudson@toasttab.com>
2019-10-10 22:14:44 -0600

    Introduce Auth0

--- limber-web/src/index.tsx
+++ limber-web/src/index.tsx
@@ -7,12 +7,30 @@ import './index.css';
 import rootReducer from './redux/reducers';
 import * as serviceWorker from './serviceWorker';
 import thunk from 'redux-thunk';
+import { Auth0Provider } from './react-auth0-wrapper';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
+// A function that routes the user to the right place after signing in.
+const onRedirectCallback = (appState: any) => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl ? appState.targetUrl : window.location.pathname,
+  );
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Auth0Provider
+      domain="limber.auth0.com"
+      client_id={process.env['REACT_APP_AUTH0_CLIENT_ID']}
+      redirect_uri={window.location.origin}
+      // @ts-ignore-line
+      onRedirectCallback={onRedirectCallback}
+    >
+      <App />
+    </Auth0Provider>
   </Provider>,
   document.getElementById('root'),
 );
