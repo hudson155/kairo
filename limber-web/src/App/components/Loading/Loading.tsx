474d688471218033211de832def876c0ba90e1c9
Jeff Hudson <jeff.hudson@toasttab.com>
2019-10-14 19:01:48 -0600

    Code/style review

--- limber-web/src/App/components/Loading/Loading.tsx
+++ limber-web/src/App/components/Loading/Loading.tsx
@@ -1,19 +1,7 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 
 const Loading: React.FC = () => {
-  const style: CSSProperties = {
-    alignSelf: 'center',
-    flexGrow: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-  };
-
-  return (
-    <div style={style}>
-      <p>Loading...</p>
-    </div>
-  );
+  return <p>Loading...</p>;
 };
 
 export default Loading;
