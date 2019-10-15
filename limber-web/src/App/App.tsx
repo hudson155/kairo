ae7f2361649f38a07934c0ff829c245fb79b8707
Jeff Hudson <jeff.hudson@toasttab.com>
2019-10-14 18:40:16 -0600

    Only show marketing site if not signed in

--- limber-web/src/App/App.tsx
+++ limber-web/src/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import EventsPage from './pages/EventsPage/EventsPage';
 import { useAuth0 } from '../react-auth0-wrapper';
@@ -7,13 +7,14 @@ import Loading from './components/Loading/Loading';
 import MarketingSiteHomePage from './pages/MarketingSiteHomePage/MarketingSiteHomePage';
 
 const App: React.FC = () => {
-  const { loading: loadingAuth0, loginWithRedirect: signIn } = useAuth0();
+  const { isAuthenticated, loading: loadingAuth0, loginWithRedirect: signIn } = useAuth0();
   if (loadingAuth0) return <Loading />;
 
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={MarketingSiteHomePage} />
+        {!isAuthenticated && <Route path="/" exact component={MarketingSiteHomePage} />}
+        {isAuthenticated && <Route path="/" exact>{() => <Redirect to="/events" />}</Route>}
         <Route path="/events" exact component={EventsPage} />
         <Route path="/signin" exact>{() => signIn()}</Route>
         <Route component={NotFoundPage} />
