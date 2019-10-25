0c2346fa3e3804a5979678c03df4eab40061804f
Jeff Hudson <jeff.hudson@toasttab.com>
2019-10-25 00:39:29 -0400

    Introduce Org model and API to front end

--- limber-web/src/api/Api.tsx
+++ limber-web/src/api/Api.tsx
@@ -1,4 +1,7 @@
+import OrgsApi from './orgs/Api';
+
 const Api = {
+  orgs: OrgsApi,
 };
 
 export default Api;
---
+++ limber-web/src/models/OrgModel.tsx
@@ -0,0 +1,6 @@
+export default interface OrgModel {
+  id: string;
+  created: string;
+  version: string;
+  name: string;
+}
