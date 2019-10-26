f52bdfe4fb4d90895a261486df25ee0ff0491e91
Jeff Hudson <jeff.hudson@toasttab.com>
2019-10-26 12:50:35 -0600

    Add missing type argument

--- limber-backend-application/orgs/src/main/kotlin/io/limberapp/backend/module/orgs/store/org/MongoOrgStore.kt
+++ limber-backend-application/orgs/src/main/kotlin/io/limberapp/backend/module/orgs/store/org/MongoOrgStore.kt
@@ -19,6 +19,6 @@ internal class MongoOrgStore @Inject constructor(
     override fun getByMemberId(memberId: UUID): List<OrgModel.Complete> {
         val filter = Filters.eq("memberships", Document("userId", memberId))
         val documents = collection.find(filter).toList()
-        return documents.map { objectMapper.readValue(it.toJson()) }
+        return documents.map { objectMapper.readValue<OrgModel.Complete>(it.toJson()) }
     }
 }
