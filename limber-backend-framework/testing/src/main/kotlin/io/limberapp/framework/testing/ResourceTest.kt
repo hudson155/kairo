91459b3b942d8db82f2f2f98718706d87009cd12
Jeff Hudson <jeff.hudson@toasttab.com>
2019-10-30 19:55:26 -0600

    Create testing module

---
+++ limber-backend-framework/testing/src/main/kotlin/io/limberapp/framework/testing/ResourceTest.kt
@@ -0,0 +1,57 @@
+package io.limberapp.framework.testing
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.TestApplicationCall
+import io.ktor.server.testing.TestApplicationEngine
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
+import io.ktor.server.testing.withTestApplication
+import io.limberapp.framework.LimberApp
+import io.limberapp.framework.endpoint.ApiEndpoint
+import io.limberapp.framework.endpoint.authorization.jwt.Jwt
+import io.limberapp.framework.endpoint.authorization.jwt.JwtUser
+import io.limberapp.framework.endpoint.authorization.jwt.withJwt
+import io.limberapp.framework.jackson.objectMapper.LimberObjectMapper
+import io.limberapp.framework.rep.CreationRep
+import java.util.UUID
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+private val objectMapper = LimberObjectMapper(prettyPrint = false)
+
+private fun withLimberTestApp(limberApp: LimberApp, test: TestApplicationEngine.() -> Unit) {
+    withTestApplication({ limberApp.bindToApplication(this) }, test)
+}
+
+abstract class ResourceTest {
+
+    protected object LimberTest {
+
+        fun <T : CreationRep> post(
+            limberApp: LimberApp,
+            config: ApiEndpoint.Config,
+            pathParams: Map<String, String> = emptyMap(),
+            body: T,
+            test: TestApplicationCall.() -> Unit
+        ) = withLimberTestApp(limberApp) {
+            val call = handleRequest(config.httpMethod, config.path(pathParams)) {
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                val jwt = JWT.create().withJwt(
+                    Jwt(orgs = emptyMap(), user = JwtUser(UUID.randomUUID()))
+                ).sign(Algorithm.none())
+                addHeader(HttpHeaders.Authorization, "Bearer $jwt")
+                setBody(objectMapper.writeValueAsString(body))
+            }
+            with(call) {
+                assertTrue(requestHandled)
+                assertEquals(HttpStatusCode.OK, response.status())
+                test()
+            }
+        }
+    }
+}
+
