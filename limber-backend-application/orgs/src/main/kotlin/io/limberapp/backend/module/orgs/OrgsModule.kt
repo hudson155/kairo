1fcc5fae53e5d06deb2dcb2516fe2169cb909d00
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-02 17:20:34 -0600

    Delete Module-related code

--- limber-backend-application/orgs/src/main/kotlin/io/limberapp/backend/module/orgs/OrgsModule.kt
+++ limber-backend-application/orgs/src/main/kotlin/io/limberapp/backend/module/orgs/OrgsModule.kt
@@ -5,16 +5,8 @@ import io.limberapp.backend.module.orgs.endpoint.org.GetOrgById
 import io.limberapp.backend.module.orgs.endpoint.org.GetOrgsByMemberId
 import io.limberapp.backend.module.orgs.endpoint.org.UpdateOrg
 import io.limberapp.backend.module.orgs.endpoint.org.membership.CreateMembership
-import io.limberapp.backend.module.orgs.endpoint.org.module.CreateModule
-import io.limberapp.backend.module.orgs.endpoint.org.module.GetModuleById
-import io.limberapp.backend.module.orgs.endpoint.org.module.GetModulesByOrgId
-import io.limberapp.backend.module.orgs.endpoint.org.module.UpdateModule
-import io.limberapp.backend.module.orgs.service.module.ModuleService
-import io.limberapp.backend.module.orgs.service.module.ModuleServiceImpl
 import io.limberapp.backend.module.orgs.service.org.OrgService
 import io.limberapp.backend.module.orgs.service.org.OrgServiceImpl
-import io.limberapp.backend.module.orgs.store.module.ModuleStore
-import io.limberapp.backend.module.orgs.store.module.MongoModuleStore
 import io.limberapp.backend.module.orgs.store.org.MongoOrgStore
 import io.limberapp.backend.module.orgs.store.org.OrgStore
 import io.limberapp.framework.module.Module
@@ -31,11 +23,6 @@ class OrgsModule : Module() {
 
         CreateMembership::class.java,
 
-        CreateModule::class.java,
-        GetModuleById::class.java,
-        GetModulesByOrgId::class.java,
-        UpdateModule::class.java,
-
         CreateOrg::class.java,
         GetOrgById::class.java,
         GetOrgsByMemberId::class.java,
@@ -43,12 +30,10 @@ class OrgsModule : Module() {
     )
 
     override fun bindServices() {
-        bind(ModuleService::class, ModuleServiceImpl::class)
         bind(OrgService::class, OrgServiceImpl::class)
     }
 
     override fun bindStores() {
-        bind(ModuleStore::class, MongoModuleStore::class)
         bind(OrgStore::class, MongoOrgStore::class)
     }
 }
