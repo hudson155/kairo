649e49f1893ede360f2b7c655417ee7a03c74bb5
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-08 16:01:22 -0700

    Move EncryptedValue into its own file

---
+++ limber-backend-framework/src/main/kotlin/io/limberapp/framework/config/EncryptedValue.kt
@@ -0,0 +1,26 @@
+package io.limberapp.framework.config
+
+import com.google.cloud.kms.v1.CryptoKeyName
+import com.google.cloud.kms.v1.KeyManagementServiceClient
+import com.google.protobuf.ByteString
+import java.nio.charset.Charset
+import java.util.Base64
+
+data class EncryptedValue(
+    private val encrypted: Boolean,
+    private val value: String
+) {
+
+    val decryptedValue: String = run {
+        if (!encrypted) return@run value
+        val decoded = Base64.getDecoder().decode(value)
+        // Create the KeyManagementServiceClient using try-with-resources to manage client cleanup.
+        val decrypted = KeyManagementServiceClient.create().use { client ->
+            return@use client.decrypt(
+                CryptoKeyName.of("limberapp", "global", "limberapp", "secrets"),
+                ByteString.copyFrom(decoded)
+            ).plaintext.toString(Charset.defaultCharset())
+        }
+        return@run decrypted
+    }
+}
--- limber-backend-framework/src/main/kotlin/io/limberapp/framework/config/database/DatabaseConfig.kt
+++ limber-backend-framework/src/main/kotlin/io/limberapp/framework/config/database/DatabaseConfig.kt
@@ -1,28 +1,6 @@
 package io.limberapp.framework.config.database
 
-import com.google.cloud.kms.v1.CryptoKeyName
-import com.google.cloud.kms.v1.KeyManagementServiceClient
-import com.google.protobuf.ByteString
-import java.nio.charset.Charset
-import java.util.Base64
-
-data class EncryptedValue(
-    private val encrypted: Boolean,
-    private val value: String
-) {
-
-    val decryptedValue: String = run {
-        if (!encrypted) return@run value
-        val decoded = Base64.getDecoder().decode(value)
-        // Create the KeyManagementServiceClient using try-with-resources to manage client cleanup.
-        return@run KeyManagementServiceClient.create().use { client ->
-            return@use client.decrypt(
-                CryptoKeyName.of("limberapp", "global", "limberapp", "secrets"),
-                ByteString.copyFrom(decoded)
-            ).plaintext.toString(Charset.defaultCharset())
-        }
-    }
-}
+import io.limberapp.framework.config.EncryptedValue
 
 /**
  * This class encapsulates the configuration for the connection to MongoDB. The class name does not
