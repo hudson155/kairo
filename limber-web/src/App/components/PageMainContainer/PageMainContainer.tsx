4ecbc4811cae2d68492ea6150d1d3ba8fe2b8b69
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-10 15:51:01 -0700

    Create PageMainContainer

---
+++ limber-web/src/App/components/PageMainContainer/PageMainContainer.tsx
@@ -0,0 +1,21 @@
+import React, { CSSProperties, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+const PageMainContainer: React.FC<Props> = (props: Props) => {
+  const style: CSSProperties = {
+    width: '1080px',
+    maxWidth: '100%',
+    alignSelf: 'center',
+  };
+
+  return (
+    <div style={style}>
+      {props.children}
+    </div>
+  );
+};
+
+export default PageMainContainer;
--- limber-web/src/App/pages/MarketingSite/pages/MarketingSiteNotFoundPage/MarketingSiteNotFoundPage.tsx
+++ limber-web/src/App/pages/MarketingSite/pages/MarketingSiteNotFoundPage/MarketingSiteNotFoundPage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PageMainContainer from '../../../../components/PageMainContainer/PageMainContainer';
 
 const MarketingSiteNotFoundPage: React.FC = () => {
-  return <>
-    <h1>Not Found</h1>
-    <p>We looked everywhere, but we couldn&apos;t find the page you were looking for.</p>
-    <p><Link to="/signin">Click here to sign in</Link></p>
-    <p><Link to="/">Click here to go home</Link></p>
-  </>;
+  return (
+    <PageMainContainer>
+      <h1>Not Found</h1>
+      <p>We looked everywhere, but we couldn&apos;t find the page you were looking for.</p>
+      <p><Link to="/signin">Click here to sign in</Link></p>
+      <p><Link to="/">Click here to go home</Link></p>
+    </PageMainContainer>
+  );
 };
 
 export default MarketingSiteNotFoundPage;
