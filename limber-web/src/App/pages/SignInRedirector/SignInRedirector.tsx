49679351c4586c664974dd0e3e03b3b924197678
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-10 14:01:13 -0700

    Don't redirect if back/forward takes to sign in/out page

--- limber-web/src/App/pages/SignInRedirector/SignInRedirector.tsx
+++ limber-web/src/App/pages/SignInRedirector/SignInRedirector.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useAuth0 } from '../../../react-auth0-wrapper';
+import { useHistory } from 'react-router-dom';
 
 const SignInRedirector: React.FC = () => {
   const auth0 = useAuth0();
+  const history = useHistory();
 
-  auth0.loginWithRedirect();
+  if (history.action !== 'POP') {
+    auth0.loginWithRedirect();
+  }
 
   return null;
 };
--- limber-web/src/App/pages/SignOutRedirector/SignOutRedirector.tsx
+++ limber-web/src/App/pages/SignOutRedirector/SignOutRedirector.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useAuth0 } from '../../../react-auth0-wrapper';
+import { useHistory } from 'react-router';
 
 const SignOutRedirector: React.FC = () => {
   const auth0 = useAuth0();
+  const history = useHistory();
 
-  auth0.logout({ returnTo: process.env['REACT_APP_ROOT_URL'] });
+  if (history.action !== 'POP') {
+    auth0.logout({ returnTo: process.env['REACT_APP_ROOT_URL'] });
+  }
 
   return null;
 };
