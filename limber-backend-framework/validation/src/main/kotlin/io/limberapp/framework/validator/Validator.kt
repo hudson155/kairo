c435e145150554bb64ca34be36e6615069c6fdda
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-11 17:16:33 -0700

    Refactor validation package

--- limber-backend-framework/validation/src/main/kotlin/io/limberapp/framework/validator/Validator.kt
+++ limber-backend-framework/validation/src/main/kotlin/io/limberapp/framework/validator/Validator.kt
@@ -6,9 +6,21 @@ package io.limberapp.framework.validator
 object Validator {
 
     private object Regex {
+
         val hex = Regex("[a-f0-9]", RegexOption.IGNORE_CASE)
         val uuid = Regex("$hex{8}-?$hex{4}-?$hex{4}-?$hex{4}-?$hex{12}", RegexOption.IGNORE_CASE)
+
+        val hostname = Regex(
+            "[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)+",
+            RegexOption.IGNORE_CASE
+        )
+        val emailAddress = Regex(
+            "[a-z0-9.!#\$%&'*+/=?^_`{|}~-]+@$hostname",
+            RegexOption.IGNORE_CASE
+        )
     }
+
+    fun emailAddress(value: String) = Regex.emailAddress.matches(value)
 
     fun uuid(value: String) = Regex.uuid.matches(value)
 }
