d7c6191ed9a291e70572708aecc32e91d42d2b5f
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-12 19:39:22 -0700

    Validate URLs

--- limber-backend-application/users/src/main/kotlin/io/limberapp/backend/module/users/rep/user/UserRep.kt
+++ limber-backend-application/users/src/main/kotlin/io/limberapp/backend/module/users/rep/user/UserRep.kt
@@ -6,6 +6,7 @@ import io.limberapp.framework.rep.UpdateRep
 import io.limberapp.framework.validation.util.emailAddress
 import io.limberapp.framework.validation.util.ifPresent
 import io.limberapp.framework.validation.util.shortText
+import io.limberapp.framework.validation.util.url
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -21,7 +22,7 @@ object UserRep {
             validate(UserRep.Creation::firstName) { ifPresent { shortText(allowEmpty = false) } }
             validate(UserRep.Creation::lastName) { ifPresent { shortText(allowEmpty = false) } }
             validate(UserRep.Creation::emailAddress) { emailAddress() }
-            validate(UserRep.Creation::profilePhotoUrl) { /* TODO: Validate this */ }
+            validate(UserRep.Creation::profilePhotoUrl) { ifPresent { url() } }
         }
     }
 
--- limber-backend-framework/validation/src/main/kotlin/io/limberapp/framework/validator/Validator.kt
+++ limber-backend-framework/validation/src/main/kotlin/io/limberapp/framework/validator/Validator.kt
@@ -8,12 +8,33 @@ object Validator {
     private object Regex {
 
         val hex = Regex("[a-f0-9]", RegexOption.IGNORE_CASE)
+
         val uuid = Regex("$hex{8}-?$hex{4}-?$hex{4}-?$hex{4}-?$hex{12}", RegexOption.IGNORE_CASE)
 
         val hostname = Regex(
             "[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)+",
             RegexOption.IGNORE_CASE
         )
+
+        val url = run {
+
+            val validChar = Regex("[a-z0-9\\-_.]", RegexOption.IGNORE_CASE)
+
+            val protocol = Regex("https?://", RegexOption.IGNORE_CASE)
+            val path = "(?:/$validChar+)*/?"
+            val queryString = Regex(
+                pattern = "(?:\\?$validChar+=$validChar*(?:&$validChar+=$validChar*)*)?",
+                option = RegexOption.IGNORE_CASE
+            )
+            val hash = Regex("(?:#$validChar*)?")
+
+            return@run Regex(
+                pattern = listOf(protocol, hostname, path, queryString, hash)
+                    .joinToString(""),
+                option = RegexOption.IGNORE_CASE
+            )
+        }
+
         val emailAddress = Regex(
             "[a-z0-9.!#\$%&'*+/=?^_`{|}~-]+@$hostname",
             RegexOption.IGNORE_CASE
@@ -21,6 +42,12 @@ object Validator {
     }
 
     fun emailAddress(value: String) = Regex.emailAddress.matches(value)
+
+    /**
+     * This URL validator is definitely not perfect, but there really is no such thing as a perfect
+     * URL validator - just one that fits the use case.
+     */
+    fun url(value: String) = Regex.url.matches(value)
 
     fun uuid(value: String) = Regex.uuid.matches(value)
 }
