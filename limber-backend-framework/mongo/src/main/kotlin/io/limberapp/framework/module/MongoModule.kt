9e4d67568a78b50151fccebed87721383b756914
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-15 20:50:26 -0700

    Use better binding syntax in Mongo module

--- limber-backend-framework/mongo/src/main/kotlin/io/limberapp/framework/module/MongoModule.kt
+++ limber-backend-framework/mongo/src/main/kotlin/io/limberapp/framework/module/MongoModule.kt
@@ -1,8 +1,6 @@
 package io.limberapp.framework.module
 
 import com.google.inject.AbstractModule
-import com.google.inject.Provides
-import com.google.inject.Singleton
 import com.mongodb.client.MongoDatabase
 import io.limberapp.framework.config.database.DatabaseConfig
 import io.limberapp.framework.createClient
@@ -14,12 +12,8 @@ class MongoModule(
     private val databaseConfig: DatabaseConfig
 ) : AbstractModule() {
 
-    override fun configure() = Unit
-
-    @Provides
-    @Singleton
-    fun mongoDatabase(): MongoDatabase {
-        val mongoClient = databaseConfig.createClient()
-        return mongoClient.getDatabase(databaseConfig.database)
+    override fun configure() {
+        bind(MongoDatabase::class.java)
+            .toInstance(databaseConfig.createClient().getDatabase(databaseConfig.database))
     }
 }
