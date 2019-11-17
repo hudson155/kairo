3476d402ef2c068f34d858d656b8dd63d46d8d47
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-17 15:32:52 -0700

    Make Claims public

--- limber-backend-framework/core/src/main/kotlin/io/limberapp/framework/endpoint/authorization/jwt/Jwt.kt
+++ limber-backend-framework/core/src/main/kotlin/io/limberapp/framework/endpoint/authorization/jwt/Jwt.kt
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import io.limberapp.framework.jackson.objectMapper.LimberObjectMapper
 import java.util.UUID
 
-private object Claims {
+object Claims {
     const val orgs = "https://limberapp.io/orgs"
     const val roles = "https://limberapp.io/roles"
     const val user = "https://limberapp.io/user"
