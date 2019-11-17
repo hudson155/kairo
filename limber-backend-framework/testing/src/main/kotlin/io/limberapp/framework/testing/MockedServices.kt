5a29d5b02f98112fc94514d89821499b0780fb4b
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-17 14:11:52 -0700

    Introduce MockedServices

---
+++ limber-backend-framework/testing/src/main/kotlin/io/limberapp/framework/testing/MockedServices.kt
@@ -0,0 +1,21 @@
+package io.limberapp.framework.testing
+
+import com.google.inject.AbstractModule
+import io.mockk.mockkClass
+import kotlin.reflect.KClass
+
+class MockedServices(servicesToMock: List<KClass<*>>) : AbstractModule() {
+
+    private val mocks = servicesToMock.associateWith { mockkClass(it) }
+
+    override fun configure() {
+        mocks.forEach { bindMock(it.key) }
+    }
+
+    private fun <T : Any> bindMock(clazz: KClass<T>) {
+        bind(clazz.java).toInstance(get(clazz))
+    }
+
+    @Suppress("UNCHECKED_CAST", "UnsafeCast")
+    operator fun <T : Any> get(clazz: KClass<T>) = mocks[clazz] as T
+}
