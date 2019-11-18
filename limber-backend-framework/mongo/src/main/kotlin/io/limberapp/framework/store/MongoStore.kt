bd5ffb66b3b236449ced2aa4038ebb835e1a55eb
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-18 00:20:30 -0700

    Remove irrelevant comment

--- limber-backend-framework/mongo/src/main/kotlin/io/limberapp/framework/store/MongoStore.kt
+++ limber-backend-framework/mongo/src/main/kotlin/io/limberapp/framework/store/MongoStore.kt
@@ -6,9 +6,6 @@ import java.util.UUID
 
 /**
  * MongoStore is an implementation of Store for MongoDB. It implements some default methods.
- *
- * TODO: This class uses Jackson to go to String and then to Document. Could we save time by using a
- *  stream, JsonNode, or some other intermediary structure?
  */
 abstract class MongoStore<Complete : CompleteEntity, Update : UpdateEntity>(
     protected val collection: MongoCollection<Complete>
