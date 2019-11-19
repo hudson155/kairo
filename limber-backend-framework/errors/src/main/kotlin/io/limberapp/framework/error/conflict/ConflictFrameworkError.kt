d5a7a2cd016e002495c45f0563e88f29f2d3df10
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-18 18:11:55 -0700

    Fix lint

--- limber-backend-framework/errors/src/main/kotlin/io/limberapp/framework/error/conflict/ConflictFrameworkError.kt
+++ limber-backend-framework/errors/src/main/kotlin/io/limberapp/framework/error/conflict/ConflictFrameworkError.kt
@@ -3,7 +3,7 @@ package io.limberapp.framework.error.conflict
 import io.limberapp.framework.error.FrameworkError
 
 /**
- * An error representing the HTTP 409 response code
+ * An error representing the HTTP 409 response code.
  */
 class ConflictFrameworkError : FrameworkError {
     override val key = FrameworkError.Key.CONFLICT
--- limber-backend-framework/errors/src/main/kotlin/io/limberapp/framework/error/notFound/NotFoundFrameworkError.kt
+++ limber-backend-framework/errors/src/main/kotlin/io/limberapp/framework/error/notFound/NotFoundFrameworkError.kt
@@ -3,7 +3,7 @@ package io.limberapp.framework.error.notFound
 import io.limberapp.framework.error.FrameworkError
 
 /**
- * An error representing the HTTP 404 response code
+ * An error representing the HTTP 404 response code.
  */
 class NotFoundFrameworkError : FrameworkError {
     override val key = FrameworkError.Key.NOT_FOUND
