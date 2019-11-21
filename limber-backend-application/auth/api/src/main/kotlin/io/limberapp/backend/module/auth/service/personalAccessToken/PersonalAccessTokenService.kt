7649a2c22788bab75fc33086069aac6856663617
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-21 08:35:25 -0700

    Create API layer for personal access tokens

---
+++ limber-backend-application/auth/api/src/main/kotlin/io/limberapp/backend/module/auth/service/personalAccessToken/PersonalAccessTokenService.kt
@@ -0,0 +1,13 @@
+package io.limberapp.backend.module.auth.service.personalAccessToken
+
+import io.limberapp.backend.module.auth.model.personalAccessToken.PersonalAccessTokenModel
+import java.util.UUID
+
+interface PersonalAccessTokenService {
+
+    fun create(model: PersonalAccessTokenModel)
+
+    fun getByUserId(userId: UUID): List<PersonalAccessTokenModel>
+
+    fun delete(userId: UUID, id: UUID)
+}
