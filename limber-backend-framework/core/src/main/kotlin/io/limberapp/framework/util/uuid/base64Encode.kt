f63e90f9b3eaf48fe8bcbc85c7f1cfb741bb975c
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-21 19:03:24 -0700

    Combine UUID utils

--- limber-backend-framework/core/src/main/kotlin/io/limberapp/framework/util/uuid/base64Encode.kt
+++ limber-backend-framework/core/src/main/kotlin/io/limberapp/framework/util/uuid/base64Encode.kt
@@ -1,18 +1,6 @@
 package io.limberapp.framework.util.uuid
 
-import java.nio.ByteBuffer
 import java.util.Base64
 import java.util.UUID
 
-private const val UUID_BYTES = 16
-
-fun UUID.base64Encode(): String {
-    val encoder = Base64.getEncoder()
-    val byteBuffer = ByteBuffer.wrap(ByteArray(UUID_BYTES))
-    byteBuffer.apply {
-        putLong(mostSignificantBits)
-        putLong(leastSignificantBits)
-    }
-    val byteArray = byteBuffer.array()
-    return encoder.encodeToString(byteArray)
-}
+fun UUID.base64Encode(): String = Base64.getEncoder().encodeToString(asByteArray())
