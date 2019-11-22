983ed6c9283da63c6b037b4b76188867d832b0a3
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-22 15:40:49 -0700

    Update line length for limber app

--- limber-backend-application/src/main/kotlin/io/limberapp/backend/Application.kt
+++ limber-backend-application/src/main/kotlin/io/limberapp/backend/Application.kt
@@ -4,15 +4,14 @@ import io.ktor.application.Application
 import io.ktor.server.cio.EngineMain
 
 /**
- * Main entry point to run the application locally. Google App Engine does not use EngineMain, which
- * is from ktor_server_cio.
+ * Main entry point to run the application locally. Google App Engine does not use EngineMain, which is from
+ * ktor_server_cio.
  */
 internal fun main(args: Array<String>) = EngineMain.main(args)
 
 /**
- * Application configuration method, used automatically by Ktor to configure and set up the
- * application. Regardless of how the application is started (whether from the Main function above
- * or some other way (e.g. Google App Engine)
+ * Application configuration method, used automatically by Ktor to configure and set up the application. Regardless of
+ * how the application is started (whether from the Main function above or some other way (e.g. Google App Engine).
  */
 @Suppress("unused")
 internal fun Application.main() = LimberAppMonolith().bindToApplication(this)
--- limber-backend-application/src/main/kotlin/io/limberapp/backend/Config.kt
+++ limber-backend-application/src/main/kotlin/io/limberapp/backend/Config.kt
@@ -6,8 +6,7 @@ import io.limberapp.framework.config.database.MongoDatabaseConfig
 import io.limberapp.framework.config.serving.ServingConfig
 
 /**
- * The Config class contains all custom configuration for the app. It doesn't contain Ktor built-in
- * configuration.
+ * The Config class contains all custom configuration for the app. It doesn't contain Ktor built-in configuration.
  */
 data class Config(
     val mongoDatabase: MongoDatabaseConfig,
