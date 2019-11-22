8b1210e0389b40f812eed73a2d82dab3b844ea56
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-22 15:50:01 -0700

    Update line length for framework validation

--- limber-backend-framework/validation/src/main/kotlin/io/limberapp/framework/validation/Validation.kt
+++ limber-backend-framework/validation/src/main/kotlin/io/limberapp/framework/validation/Validation.kt
@@ -1,7 +1,7 @@
 package io.limberapp.framework.validation
 
 /**
- * The Validation class provides utilities for parameter validation. They're all stored outside of
- * the class in extension functions, or else this class would get huge.
+ * The Validation class provides utilities for parameter validation. They're all stored outside of the class in
+ * extension functions, or else this class would get huge.
  */
 data class Validation<T>(val subject: T, val name: String)
