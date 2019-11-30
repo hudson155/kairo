04a763d9417a38f7928dcf9d294f7e234464ac48
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-30 12:11:48 -0700

    Fix text length validation

--- piper/validation/src/main/kotlin/com/piperframework/validation/util/length.kt
+++ piper/validation/src/main/kotlin/com/piperframework/validation/util/length.kt
@@ -8,12 +8,12 @@ private const val MEDIUM_TEXT_MAX_CHARS = 100
 private const val LONG_TEXT_MAX_CHARS = 10_000
 
 fun Validation<String>.shortText(allowEmpty: Boolean) =
-    length(IntRange(start = if (allowEmpty) 0 else MIN_CHARS, endInclusive = LONG_TEXT_MAX_CHARS))
+    length(IntRange(start = if (allowEmpty) 0 else MIN_CHARS, endInclusive = SHORT_TEXT_MAX_CHARS))
 
 fun Validation<String>.mediumText(allowEmpty: Boolean) =
     length(IntRange(start = if (allowEmpty) 0 else MIN_CHARS, endInclusive = MEDIUM_TEXT_MAX_CHARS))
 
 fun Validation<String>.longText(allowEmpty: Boolean) =
-    length(IntRange(start = if (allowEmpty) 0 else MIN_CHARS, endInclusive = SHORT_TEXT_MAX_CHARS))
+    length(IntRange(start = if (allowEmpty) 0 else MIN_CHARS, endInclusive = LONG_TEXT_MAX_CHARS))
 
 fun Validation<String>.length(range: IntRange) = boolean(subject.length in range)
