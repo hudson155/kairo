e48db788d08ec73de94ee479d3591a175e1f6972
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-30 10:34:13 -0700

    Write service layer for features

--- limber-backend-application/module/orgs/app/src/main/kotlin/io/limberapp/backend/module/orgs/service/org/OrgServiceImpl.kt
+++ limber-backend-application/module/orgs/app/src/main/kotlin/io/limberapp/backend/module/orgs/service/org/OrgServiceImpl.kt
@@ -1,17 +1,20 @@
 package io.limberapp.backend.module.orgs.service.org
 
 import com.google.inject.Inject
+import com.piperframework.exception.ConflictException
+import com.piperframework.exception.NotFoundException
+import io.limberapp.backend.module.orgs.mapper.app.feature.FeatureMapper
 import io.limberapp.backend.module.orgs.mapper.app.membership.MembershipMapper
 import io.limberapp.backend.module.orgs.mapper.app.org.OrgMapper
+import io.limberapp.backend.module.orgs.model.org.FeatureModel
 import io.limberapp.backend.module.orgs.model.org.MembershipModel
 import io.limberapp.backend.module.orgs.model.org.OrgModel
 import io.limberapp.backend.module.orgs.store.org.OrgStore
-import com.piperframework.exception.ConflictException
-import com.piperframework.exception.NotFoundException
 import java.util.UUID
 
 internal class OrgServiceImpl @Inject constructor(
     private val orgStore: OrgStore,
+    private val featureMapper: FeatureMapper,
     private val membershipMapper: MembershipMapper,
     private val orgMapper: OrgMapper
 ) : OrgService {
@@ -44,6 +47,16 @@ internal class OrgServiceImpl @Inject constructor(
 
     override fun deleteMembership(orgId: UUID, memberId: UUID) {
         orgStore.deleteMembership(orgId, memberId) ?: throw NotFoundException()
+    }
+
+    override fun createFeature(orgId: UUID, model: FeatureModel) {
+        get(orgId) ?: throw NotFoundException()
+        val entity = featureMapper.entity(model)
+        orgStore.createFeature(orgId, entity) ?: throw ConflictException()
+    }
+
+    override fun deleteFeature(orgId: UUID, featureId: UUID) {
+        orgStore.deleteFeature(orgId, featureId) ?: throw NotFoundException()
     }
 
     override fun delete(id: UUID) = orgStore.delete(id) ?: throw NotFoundException()
