a4309065239a70acbc5db317d98f58415b0fdd09
Jeff Hudson <jeff.hudson@toasttab.com>
2019-11-30 09:33:18 -0700

    Introduce path validation

--- piper/validation/src/main/kotlin/com/piperframework/validation/util/url.kt
+++ piper/validation/src/main/kotlin/com/piperframework/validation/util/url.kt
@@ -3,4 +3,6 @@ package com.piperframework.validation.util
 import com.piperframework.validation.Validation
 import com.piperframework.validator.Validator
 
+fun Validation<String>.path() = boolean(Validator.path(subject))
+
 fun Validation<String>.url() = boolean(Validator.url(subject))
--- piper/validation/src/main/kotlin/com/piperframework/validator/Validator.kt
+++ piper/validation/src/main/kotlin/com/piperframework/validator/Validator.kt
@@ -16,18 +16,17 @@ object Validator {
             option = RegexOption.IGNORE_CASE
         )
 
+        private val urlChar = Regex("[a-z0-9\\-_.]", RegexOption.IGNORE_CASE)
+
+        val path = Regex("(?:/$urlChar+)*/?", RegexOption.IGNORE_CASE)
+
         val url = run {
-
-            val validChar = Regex("[a-z0-9\\-_.]", RegexOption.IGNORE_CASE)
-
             val protocol = Regex("https?://", RegexOption.IGNORE_CASE)
-            val path = "(?:/$validChar+)*/?"
             val queryString = Regex(
-                pattern = "(?:\\?$validChar+=$validChar*(?:&$validChar+=$validChar*)*)?",
+                pattern = "(?:\\?$urlChar+=$urlChar*(?:&$urlChar+=$urlChar*)*)?",
                 option = RegexOption.IGNORE_CASE
             )
-            val hash = Regex("(?:#$validChar*)?")
-
+            val hash = Regex("(?:#$urlChar*)?", RegexOption.IGNORE_CASE)
             return@run Regex(
                 pattern = listOf(protocol, hostname, path, queryString, hash).joinToString(""),
                 option = RegexOption.IGNORE_CASE
@@ -41,6 +40,8 @@ object Validator {
     }
 
     fun emailAddress(value: String) = Regex.emailAddress.matches(value)
+
+    fun path(value: String) = Regex.path.matches(value)
 
     /**
      * This URL validator is definitely not perfect, but there really is no such thing as a perfect URL validator - just
