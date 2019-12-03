19c4b37dfb09b5a473be44c5e7ac13b39845cbd6
Jeff Hudson <jeff.hudson@toasttab.com>
2019-12-02 17:15:15 -0700

    Improve CreateFeatureTest

--- limber-backend-application/module/orgs/app/src/test/kotlin/io/limberapp/backend/module/orgs/endpoint/org/feature/CreateFeatureTest.kt
+++ limber-backend-application/module/orgs/app/src/test/kotlin/io/limberapp/backend/module/orgs/endpoint/org/feature/CreateFeatureTest.kt
@@ -11,83 +11,165 @@ import io.limberapp.backend.module.orgs.rep.org.OrgRep
 import io.limberapp.backend.module.orgs.testing.ResourceTest
 import org.junit.Test
 import java.time.LocalDateTime
+import java.util.UUID
 import kotlin.test.assertEquals
 
 internal class CreateFeatureTest : ResourceTest() {
 
     @Test
-    fun create() {
+    fun orgDoesNotExist() {
 
+        // Setup
+        val orgId = UUID.randomUUID()
+
+        // CreateFeature
+        val featureCreationRep = FeatureRep.Creation("Events", "/events", FeatureModel.Type.HOME)
+        piperTest.test(
+            endpointConfig = CreateFeature.endpointConfig,
+            pathParams = mapOf(CreateFeature.orgId to orgId.toString()),
+            body = featureCreationRep,
+            expectedStatusCode = HttpStatusCode.NotFound
+        ) {}
+    }
+
+    @Test
+    fun duplicateName() {
+
+        // CreateOrg
         val orgCreationRep = OrgRep.Creation("Cranky Pasta")
-        val orgId = deterministicUuidGenerator[0]
-        val defaultFeatureId = deterministicUuidGenerator[1]
-        val featureId = deterministicUuidGenerator[2]
+        val defaultFeatureRep = FeatureRep.Complete(
+            id = deterministicUuidGenerator[1],
+            created = LocalDateTime.now(fixedClock),
+            name = DEFAULT_FEATURE_CREATION_REP.name,
+            path = DEFAULT_FEATURE_CREATION_REP.path,
+            type = DEFAULT_FEATURE_CREATION_REP.type
+        )
+        val orgRep = OrgRep.Complete(
+            id = deterministicUuidGenerator[0],
+            created = LocalDateTime.now(fixedClock),
+            name = orgCreationRep.name,
+            features = listOf(defaultFeatureRep),
+            members = emptyList()
+        )
         piperTest.test(
             endpointConfig = CreateOrg.endpointConfig,
             body = orgCreationRep
         ) {}
 
+        // CreateFeature
+        val featureCreationRep = FeatureRep.Creation("Events", "/events", FeatureModel.Type.HOME)
+            .copy(name = DEFAULT_FEATURE_CREATION_REP.name)
+        piperTest.test(
+            endpointConfig = CreateFeature.endpointConfig,
+            pathParams = mapOf(CreateFeature.orgId to orgRep.id.toString()),
+            body = featureCreationRep,
+            expectedStatusCode = HttpStatusCode.Conflict
+        ) {}
+
+        // GetOrg
+        piperTest.test(
+            endpointConfig = GetOrg.endpointConfig,
+            pathParams = mapOf(GetOrg.orgId to orgRep.id.toString())
+        ) {
+            val actual = objectMapper.readValue<OrgRep.Complete>(response.content!!)
+            assertEquals(orgRep, actual)
+        }
+    }
+
+    @Test
+    fun duplicatePath() {
+
+        // CreateOrg
+        val orgCreationRep = OrgRep.Creation("Cranky Pasta")
+        val defaultFeatureRep = FeatureRep.Complete(
+            id = deterministicUuidGenerator[1],
+            created = LocalDateTime.now(fixedClock),
+            name = DEFAULT_FEATURE_CREATION_REP.name,
+            path = DEFAULT_FEATURE_CREATION_REP.path,
+            type = DEFAULT_FEATURE_CREATION_REP.type
+        )
+        val orgRep = OrgRep.Complete(
+            id = deterministicUuidGenerator[0],
+            created = LocalDateTime.now(fixedClock),
+            name = orgCreationRep.name,
+            features = listOf(defaultFeatureRep),
+            members = emptyList()
+        )
+        piperTest.test(
+            endpointConfig = CreateOrg.endpointConfig,
+            body = orgCreationRep
+        ) {}
+
+        // CreateFeature
+        val featureCreationRep = FeatureRep.Creation("Events", "/events", FeatureModel.Type.HOME)
+            .copy(path = DEFAULT_FEATURE_CREATION_REP.path)
+        piperTest.test(
+            endpointConfig = CreateFeature.endpointConfig,
+            pathParams = mapOf(CreateFeature.orgId to orgRep.id.toString()),
+            body = featureCreationRep,
+            expectedStatusCode = HttpStatusCode.Conflict
+        ) {}
+
+        // GetOrg
+        piperTest.test(
+            endpointConfig = GetOrg.endpointConfig,
+            pathParams = mapOf(GetOrg.orgId to orgRep.id.toString())
+        ) {
+            val actual = objectMapper.readValue<OrgRep.Complete>(response.content!!)
+            assertEquals(orgRep, actual)
+        }
+    }
+
+    @Test
+    fun happyPath() {
+
+        // CreateOrg
+        val orgCreationRep = OrgRep.Creation("Cranky Pasta")
+        val defaultFeatureRep = FeatureRep.Complete(
+            id = deterministicUuidGenerator[1],
+            created = LocalDateTime.now(fixedClock),
+            name = DEFAULT_FEATURE_CREATION_REP.name,
+            path = DEFAULT_FEATURE_CREATION_REP.path,
+            type = DEFAULT_FEATURE_CREATION_REP.type
+        )
+        var orgRep = OrgRep.Complete(
+            id = deterministicUuidGenerator[0],
+            created = LocalDateTime.now(fixedClock),
+            name = orgCreationRep.name,
+            features = listOf(defaultFeatureRep),
+            members = emptyList()
+        )
+        piperTest.test(
+            endpointConfig = CreateOrg.endpointConfig,
+            body = orgCreationRep
+        ) {}
+
+        // CreateFeature
         val featureCreationRep = FeatureRep.Creation("Events", "/events", FeatureModel.Type.HOME)
         val featureRep = FeatureRep.Complete(
-            id = featureId,
+            id = deterministicUuidGenerator[2],
             created = LocalDateTime.now(fixedClock),
             name = featureCreationRep.name,
             path = featureCreationRep.path,
             type = featureCreationRep.type
         )
+        orgRep = orgRep.copy(features = orgRep.features.plus(featureRep))
         piperTest.test(
             endpointConfig = CreateFeature.endpointConfig,
-            pathParams = mapOf(CreateFeature.orgId to orgId.toString()),
+            pathParams = mapOf(CreateFeature.orgId to orgRep.id.toString()),
             body = featureCreationRep
         ) {
             val actual = objectMapper.readValue<FeatureRep.Complete>(response.content!!)
             assertEquals(featureRep, actual)
         }
 
+        // GetOrg
         piperTest.test(
             endpointConfig = GetOrg.endpointConfig,
-            pathParams = mapOf(GetOrg.orgId to orgId.toString())
+            pathParams = mapOf(GetOrg.orgId to orgRep.id.toString())
         ) {
             val actual = objectMapper.readValue<OrgRep.Complete>(response.content!!)
-            val defaultFeature = FeatureRep.Complete(
-                id = defaultFeatureId,
-                created = LocalDateTime.now(fixedClock),
-                name = DEFAULT_FEATURE_CREATION_REP.name,
-                path = DEFAULT_FEATURE_CREATION_REP.path,
-                type = DEFAULT_FEATURE_CREATION_REP.type
-            )
-            val expected = OrgRep.Complete(
-                id = orgId,
-                created = LocalDateTime.now(fixedClock),
-                name = orgCreationRep.name,
-                features = listOf(defaultFeature, featureRep),
-                members = emptyList()
-            )
-            assertEquals(expected, actual)
+            assertEquals(orgRep, actual)
         }
-    }
-
-    @Test
-    fun createDuplicate() {
-
-        val orgCreationRep = OrgRep.Creation("Cranky Pasta")
-        val orgId = deterministicUuidGenerator[0]
-        piperTest.test(
-            endpointConfig = CreateOrg.endpointConfig,
-            body = orgCreationRep
-        ) {}
-
-        val featureCreationRep = FeatureRep.Creation("Events", "/events", FeatureModel.Type.HOME)
-        piperTest.test(
-            endpointConfig = CreateFeature.endpointConfig,
-            pathParams = mapOf(CreateFeature.orgId to orgId.toString()),
-            body = featureCreationRep
-        ) {}
-        piperTest.test(
-            endpointConfig = CreateFeature.endpointConfig,
-            pathParams = mapOf(CreateFeature.orgId to orgId.toString()),
-            body = featureCreationRep,
-            expectedStatusCode = HttpStatusCode.Conflict
-        ) {}
     }
 }
