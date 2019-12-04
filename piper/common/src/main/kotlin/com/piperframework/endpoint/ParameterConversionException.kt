060179047f9855ae7bee3a3f3c08948e83d0173a
Jeff Hudson <jeff.hudson@toasttab.com>
2019-12-03 23:35:48 -0700

    Error messages should end with a period

--- piper/common/src/main/kotlin/com/piperframework/endpoint/ParameterConversionException.kt
+++ piper/common/src/main/kotlin/com/piperframework/endpoint/ParameterConversionException.kt
@@ -4,4 +4,7 @@ import com.piperframework.exception.exception.badRequest.BadRequestException
 import kotlin.reflect.KClass
 
 class ParameterConversionException(name: String?, clazz: KClass<*>, cause: Throwable? = null) :
-    BadRequestException("Could not convert parameter${name?.let { " $it" } ?: ""} to ${clazz.simpleName}", cause)
+    BadRequestException(
+        message = "Could not convert parameter${name?.let { " $it" } ?: ""} to ${clazz.simpleName ?: "?"}.",
+        cause = cause
+    )
