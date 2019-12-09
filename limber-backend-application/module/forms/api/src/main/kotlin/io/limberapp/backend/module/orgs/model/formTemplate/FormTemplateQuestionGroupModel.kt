ba6a1b729437c52b542fdd7f40fe42c6271739f4
Jeff Hudson <jeff.hudson@toasttab.com>
2019-12-08 20:04:28 -0700

    Revert "Create API for form templates"
    This reverts commit f04cfef7d98becee54a0a2cbec63e18ac9b6b573.

--- limber-backend-application/module/forms/api/src/main/kotlin/io/limberapp/backend/module/orgs/model/formTemplate/FormTemplateQuestionGroupModel.kt
+++
@@ -1,8 +0,0 @@
-package io.limberapp.backend.module.orgs.model.formTemplate
-
-import java.util.UUID
-
-data class FormTemplateQuestionGroupModel(
-    val id: UUID,
-    val questions: List<FormTemplateQuestionModel>
-)
