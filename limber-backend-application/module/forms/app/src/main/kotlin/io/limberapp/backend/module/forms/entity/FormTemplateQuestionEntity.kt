9f7be8c8bde5a27985d16be3b458051f3da1ac52
Jeff Hudson <jeff.hudson@toasttab.com>
2020-01-09 01:32:54 -0700

    Update form template entities

--- limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/entity/FormTemplateQuestionEntity.kt
+++ limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/entity/FormTemplateQuestionEntity.kt
@@ -1,10 +1,19 @@
 package io.limberapp.backend.module.forms.entity
 
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.piperframework.entity.CompleteSubentity
 import com.piperframework.entity.UpdateSubentity
-import io.limberapp.backend.module.orgs.model.formTemplate.FormTemplateQuestionModel
+import io.limberapp.backend.module.forms.entity.formTemplateQuestion.FormTemplateDateQuestionEntity
+import io.limberapp.backend.module.forms.entity.formTemplateQuestion.FormTemplateTextQuestionEntity
+import io.limberapp.backend.module.forms.model.formTemplate.FormTemplateQuestionModel
 import java.util.UUID
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes(
+    JsonSubTypes.Type(value = FormTemplateDateQuestionEntity::class, name = "DATE"),
+    JsonSubTypes.Type(value = FormTemplateTextQuestionEntity::class, name = "TEXT")
+)
 interface FormTemplateQuestionEntity : CompleteSubentity {
 
     val id: UUID
