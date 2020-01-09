5539a8fbeb71093af2bb7551471d636ea7547dd4
Jeff Hudson <jeff.hudson@toasttab.com>
2020-01-08 21:04:24 -0700

    Update Mongo library

--- piper/mongo/src/main/kotlin/com/piperframework/store/MongoStore.kt
+++ piper/mongo/src/main/kotlin/com/piperframework/store/MongoStore.kt
@@ -2,7 +2,7 @@ package com.piperframework.store
 
 import com.piperframework.entity.CompleteEntity
 
-typealias MongoIndex<T> = MongoCollection<T>.() -> Unit
+typealias MongoIndex<Complete> = MongoCollection<Complete>.() -> Unit
 
 /**
  * MongoStore is an implementation of Store for MongoDB.
@@ -11,6 +11,8 @@ abstract class MongoStore<Complete : CompleteEntity>(
     protected val collection: MongoCollection<Complete>,
     indices: List<MongoIndex<Complete>>
 ) : Store<Complete> {
+
+    constructor(collection: MongoCollection<Complete>, index: MongoIndex<Complete>) : this(collection, listOf(index))
 
     init {
         indices.forEach { collection.it() }
