b5c2798d1dde33a483bafdfdb90bbfa87d08ff84
Jeff Hudson <jeff.hudson@toasttab.com>
2020-01-09 01:38:04 -0700

    Update users tests

--- limber-backend-application/module/users/app/src/test/kotlin/io/limberapp/backend/module/users/endpoint/user/CreateUserTest.kt
+++ limber-backend-application/module/users/app/src/test/kotlin/io/limberapp/backend/module/users/endpoint/user/CreateUserTest.kt
@@ -1,7 +1,7 @@
 package io.limberapp.backend.module.users.endpoint.user
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import io.limberapp.backend.module.users.exception.conflict.ConflictsWithAnotherUser
+import io.limberapp.backend.module.users.exception.conflict.EmailAddressAlreadyTaken
 import io.limberapp.backend.module.users.rep.user.UserRep
 import io.limberapp.backend.module.users.testing.ResourceTest
 import io.limberapp.backend.module.users.testing.fixtures.user.UserRepFixtures
@@ -23,7 +23,7 @@ internal class CreateUserTest : ResourceTest() {
         piperTest.test(
             endpointConfig = CreateUser.endpointConfig,
             body = UserRepFixtures[1].creation().copy(emailAddress = UserRepFixtures[0].creation().emailAddress),
-            expectedException = ConflictsWithAnotherUser()
+            expectedException = EmailAddressAlreadyTaken(UserRepFixtures[0].creation().emailAddress)
         )
     }
 
