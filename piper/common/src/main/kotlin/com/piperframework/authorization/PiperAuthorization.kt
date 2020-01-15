c543aacdbf0bde979d5bbe4ef64f8c7c2d0229a1
Jeff Hudson <jeff.hudson@toasttab.com>
2020-01-14 21:29:10 -0700

    Document methods

--- piper/common/src/main/kotlin/com/piperframework/authorization/PiperAuthorization.kt
+++ piper/common/src/main/kotlin/com/piperframework/authorization/PiperAuthorization.kt
@@ -2,7 +2,14 @@ package com.piperframework.authorization
 
 import io.ktor.auth.Principal
 
+/**
+ * Encapsulates some arbitrary way of authorizing access to an endpoint.
+ */
 interface PiperAuthorization<P : Principal> {
 
+    /**
+     * If this method returns true, the principal is authorized to access the endpoint. Otherwise, authorization is
+     * forbidden.
+     */
     fun authorize(principal: P?): Boolean
 }
