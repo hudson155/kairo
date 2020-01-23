a8b0d18fd469df17c599cab45dea95453e5b9020
Jeff Hudson <jeff.hudson@toasttab.com>
2020-01-23 15:10:18 -0700

    Create store interfaces for form instances

---
+++ limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/store/formInstance/FormInstanceQuestionStore.kt
@@ -0,0 +1,5 @@
+package io.limberapp.backend.module.forms.store.formInstance
+
+import io.limberapp.backend.module.forms.service.formInstance.FormInstanceQuestionService
+
+interface FormInstanceQuestionStore : FormInstanceQuestionService
---
+++ limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/store/formInstance/FormInstanceStore.kt
@@ -0,0 +1,5 @@
+package io.limberapp.backend.module.forms.store.formInstance
+
+import io.limberapp.backend.module.forms.service.formInstance.FormInstanceService
+
+interface FormInstanceStore : FormInstanceService
