b74b2d073ac6efe9acce1e178bebaf256efefe41
Jeff Hudson <jeff.hudson@toasttab.com>
2020-01-23 15:20:51 -0700

    Create exceptions for form instances

---
+++ limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/exception/notFound/FormInstanceNotFound.kt
@@ -0,0 +1,5 @@
+package io.limberapp.backend.module.forms.exception.notFound
+
+import com.piperframework.exception.exception.notFound.EntityNotFound
+
+internal class FormInstanceNotFound : EntityNotFound("Form Instance")
