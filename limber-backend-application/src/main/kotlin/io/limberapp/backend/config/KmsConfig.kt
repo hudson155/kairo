1a1c750c408c24c0f0c02353a0cdec2f95df6881
Jeff Hudson <jeff.hudson@toasttab.com>
2020-01-26 18:16:42 -0500

    Remove references to App Engine

--- limber-backend-application/src/main/kotlin/io/limberapp/backend/config/KmsConfig.kt
+++
@@ -1,8 +0,0 @@
-package io.limberapp.backend.config
-
-internal data class KmsConfig(
-    val project: String,
-    val location: String,
-    val keyRing: String,
-    val cryptoKey: String
-)
--- limber-backend-application/src/main/kotlin/io/limberapp/backend/config/LimberConfigLoader.kt
+++ limber-backend-application/src/main/kotlin/io/limberapp/backend/config/LimberConfigLoader.kt
@@ -3,14 +3,25 @@ package io.limberapp.backend.config
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.piperframework.PiperConfigLoader
 import com.piperframework.jackson.objectMapper.PiperObjectMapper
+import org.slf4j.LoggerFactory
+
+private const val LIMBER_ENV = "LIMBER_ENV"
 
 internal class LimberConfigLoader : PiperConfigLoader<LimberAppMonolithConfig>(LimberAppMonolithConfig::class) {
+
+    private val logger = LoggerFactory.getLogger(LimberConfigLoader::class.java)
 
     override val objectMapper = PiperObjectMapper(YAMLFactory())
 
     override fun load(): LimberAppMonolithConfig {
-        val envString = System.getenv("LIMBERAPP_ENV") ?: "prod"
-        val config = loadInternal("/config/$envString.yml") ?: error("Config for LIMBER_ENV=$envString not found.")
-        return config.decrypt()
+        logger.info("Loading config...")
+        val envString = System.getenv(LIMBER_ENV) ?: error("Environment variable $LIMBER_ENV not specified.")
+        logger.info("Loading config for environment $envString...")
+        val config = loadInternal("/config/$envString.yml") ?: error("Config for $LIMBER_ENV=$envString not found.")
+        logger.info("Loaded config for environment $envString.")
+        logger.info("Decrypting config for environment $envString...")
+        val decryptedConfig = config.decrypt()
+        logger.info("Decrypted config for environment $envString.")
+        return decryptedConfig
     }
 }
