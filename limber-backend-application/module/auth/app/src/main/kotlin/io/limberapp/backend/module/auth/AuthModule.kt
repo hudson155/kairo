a9780e1afad21afe0123c1979cbee0a1dbc7379c
Jeff Hudson <jeff.hudson@toasttab.com>
2020-01-31 10:41:15 -0500

    Move module documentation from JavaDoc to READMEs

--- limber-backend-application/module/auth/app/src/main/kotlin/io/limberapp/backend/module/auth/AuthModule.kt
+++ limber-backend-application/module/auth/app/src/main/kotlin/io/limberapp/backend/module/auth/AuthModule.kt
@@ -12,18 +12,6 @@ import io.limberapp.backend.module.auth.service.jwtClaimsRequest.JwtClaimsReques
 import io.limberapp.backend.module.auth.store.accessToken.AccessTokenStore
 import io.limberapp.backend.module.auth.store.accessToken.SqlAccessTokenStore
 
-/**
- * Authentication is performed by Auth0, so the auth module is not actually responsible for authenticating users. It's
- * also not really responsible for authorization either, since authorization is handled on a per-endpoint basis
- * regardless of which module it's in. What, might you ask, does this auth module actually do, you might ask? Well,
- * since I've already told you what it doesn't do, here's what it does do.
- *
- * - The auth module is responsible for communication with Auth0. This could mean a few different things. The primary
- *     use case right now is that when users authenticate with Auth0, Auth0 needs to get enough information about them
- *     to create a JWT for them. Auth0 has some of this information, but reaches out to the auth module for the rest.
- *
- * - The auth module is responsible for access tokens. Access tokens are only for admins at this time.
- */
 class AuthModule : Module() {
 
     override val endpoints = listOf(
--- limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/FormsModule.kt
+++ limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/FormsModule.kt
@@ -18,18 +18,6 @@ import io.limberapp.backend.module.forms.store.formTemplate.FormTemplateStore
 import io.limberapp.backend.module.forms.store.formTemplate.SqlFormTemplateQuestionStore
 import io.limberapp.backend.module.forms.store.formTemplate.SqlFormTemplateStore
 
-/**
- * The forms module handles any form features an org has enabled. An org can have 0 form features, or many form
- * features, although it's probably most common for an org to have 1 form feature. A forms feature manifests itself as a
- * distinct section within the app. There's no communication between forms features, even within a single org.
- *
- * So what is a forms feature? A forms feature allows users to create form templates dynamically, and instantiate or
- * "fill out" those templates. A form template akin to a real world blank form (or an infinite stack of blank forms)
- * that are all identical. A form instance is akin to a real world single form that has been filled out. It pertains to
- * a version of a form template, and the form template still exists.
- *
- * Form templates are made up of questions, which are the atomic unit.
- */
 class FormsModule : Module() {
 
     override val endpoints = listOf(
