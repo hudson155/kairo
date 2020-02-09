ccbba2dfd0f97cd167ba864c5736a23892fce11e
Jeff Hudson <jeff.hudson@toasttab.com>
2020-02-08 22:02:37 -0700

    Don't use blank lines in tables

--- limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/entity/formInstance/FormInstanceQuestionTable.kt
+++ limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/entity/formInstance/FormInstanceQuestionTable.kt
@@ -5,16 +5,11 @@ import com.piperframework.store.SqlTable
 import io.limberapp.backend.module.forms.entity.formTemplate.FormTemplateQuestionTable
 
 object FormInstanceQuestionTable : SqlTable("forms", "form_instance_question") {
-
     val formInstanceGuid = uuid("form_instance_guid").references(FormInstanceTable.guid)
-
     val formTemplateQuestionGuid = uuid("form_template_question_guid")
         .references(FormTemplateQuestionTable.guid)
         .nullable()
-
     val type = text("type")
-
     val text = text("text").nullable()
-
     val date = localdate("date").nullable()
 }
