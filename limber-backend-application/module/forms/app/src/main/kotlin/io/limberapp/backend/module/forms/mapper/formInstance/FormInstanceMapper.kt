3968364052e7ecff733bd2f17cd5b8a5dd40c821
Jeff Hudson <jeff.hudson@toasttab.com>
2020-02-08 23:23:32 -0700

    Create mappers for form instances

---
+++ limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/mapper/formInstance/FormInstanceMapper.kt
@@ -0,0 +1,29 @@
+package io.limberapp.backend.module.forms.mapper.formInstance
+
+import com.google.inject.Inject
+import com.piperframework.util.uuid.uuidGenerator.UuidGenerator
+import io.limberapp.backend.module.forms.model.formInstance.FormInstanceModel
+import io.limberapp.backend.module.forms.rep.formInstance.FormInstanceRep
+import java.time.Clock
+import java.time.LocalDateTime
+
+internal class FormInstanceMapper @Inject constructor(
+    private val clock: Clock,
+    private val uuidGenerator: UuidGenerator,
+    private val formInstanceQuestionMapper: FormInstanceQuestionMapper
+) {
+
+    fun model(rep: FormInstanceRep.Creation) = FormInstanceModel(
+        id = uuidGenerator.generate(),
+        created = LocalDateTime.now(clock),
+        formTemplateId = rep.formTemplateId,
+        questions = rep.questions.map { formInstanceQuestionMapper.model(it) }
+    )
+
+    fun completeRep(model: FormInstanceModel) = FormInstanceRep.Complete(
+        id = model.id,
+        created = model.created,
+        formTemplateId = model.formTemplateId,
+        questions = model.questions.map { formInstanceQuestionMapper.completeRep(it) }
+    )
+}
---
+++ limber-backend-application/module/forms/app/src/main/kotlin/io/limberapp/backend/module/forms/mapper/formInstance/FormInstanceQuestionMapper.kt
@@ -0,0 +1,50 @@
+package io.limberapp.backend.module.forms.mapper.formInstance
+
+import com.google.inject.Inject
+import com.piperframework.util.uuid.unknown
+import com.piperframework.util.uuid.uuidGenerator.UuidGenerator
+import io.limberapp.backend.module.forms.model.formInstance.FormInstanceQuestionModel
+import io.limberapp.backend.module.forms.model.formInstance.formInstanceQuestion.FormInstanceDateQuestionModel
+import io.limberapp.backend.module.forms.model.formInstance.formInstanceQuestion.FormInstanceTextQuestionModel
+import io.limberapp.backend.module.forms.rep.formInstance.FormInstanceQuestionRep
+import io.limberapp.backend.module.forms.rep.formInstance.formInstanceQuestion.FormInstanceDateQuestionRep
+import io.limberapp.backend.module.forms.rep.formInstance.formInstanceQuestion.FormInstanceTextQuestionRep
+import java.time.Clock
+import java.time.LocalDateTime
+import kotlin.reflect.KClass
+
+internal class FormInstanceQuestionMapper @Inject constructor(
+    private val clock: Clock,
+    private val uuidGenerator: UuidGenerator
+) {
+
+    fun model(rep: FormInstanceQuestionRep.Creation) = when (rep) {
+        is FormInstanceDateQuestionRep.Creation -> FormInstanceDateQuestionModel(
+            created = LocalDateTime.now(clock),
+            formTemplateQuestionId = rep.formTemplateQuestionId,
+            date = rep.date
+        )
+        is FormInstanceTextQuestionRep.Creation -> FormInstanceTextQuestionModel(
+            created = LocalDateTime.now(clock),
+            formTemplateQuestionId = rep.formTemplateQuestionId,
+            text = rep.text
+        )
+        else -> unknown(rep::class)
+    }
+
+    fun completeRep(model: FormInstanceQuestionModel) = when (model) {
+        is FormInstanceDateQuestionModel -> FormInstanceDateQuestionRep.Complete(
+            created = model.created,
+            formTemplateQuestionId = model.formTemplateQuestionId,
+            date = model.date
+        )
+        is FormInstanceTextQuestionModel -> FormInstanceTextQuestionRep.Complete(
+            created = model.created,
+            formTemplateQuestionId = model.formTemplateQuestionId,
+            text = model.text
+        )
+        else -> unknown(model::class)
+    }
+
+    private fun unknown(clazz: KClass<*>): Nothing = unknown("form instance question", clazz)
+}
