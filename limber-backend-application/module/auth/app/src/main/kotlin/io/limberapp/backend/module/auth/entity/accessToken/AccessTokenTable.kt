3357f65eff77cfed505b3fa241c97713d4b3fd73
Jeff Hudson <jeff.hudson@toasttab.com>
2020-02-10 15:50:17 -0700

    Vertically space column names

--- limber-backend-application/module/auth/app/src/main/kotlin/io/limberapp/backend/module/auth/entity/accessToken/AccessTokenTable.kt
+++ limber-backend-application/module/auth/app/src/main/kotlin/io/limberapp/backend/module/auth/entity/accessToken/AccessTokenTable.kt
@@ -3,7 +3,10 @@ package io.limberapp.backend.module.auth.entity.accessToken
 import com.piperframework.store.SqlTable
 
 object AccessTokenTable : SqlTable("auth", "access_token") {
+
     val guid = uuid("guid")
+
     val accountGuid = uuid("account_guid")
+
     val token = text("token")
 }
