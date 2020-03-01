cfef474d6dccac57fea36647d7c2ddf3a3c392c6
Jeff Hudson <jeff.hudson@toasttab.com>
2020-03-01 14:26:07 -0700

    Review React AppRouter code

--- limber-web/src/App/AppRouter.tsx
+++ limber-web/src/App/AppRouter.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AnyAction } from 'redux';
-import { ThunkDispatch } from 'redux-thunk';
-import { TD } from '../index';
-import TenantModel from '../models/tenant/TenantModel';
-import LoadableState from '../redux/util/LoadableState';
 import State from '../state';
 import MainApp from './pages/MainApp/MainApp';
 import MarketingSite from './pages/MarketingSite/MarketingSite';
@@ -13,26 +8,9 @@ import SignInRedirector from './pages/SignInRedirector/SignInRedirector';
 import SignOutRedirector from './pages/SignOutRedirector/SignOutRedirector';
 import { useAuth } from './useAuth';
 
-interface Props {
-  tenantState: LoadableState<TenantModel>;
-  dispatch: TD;
-}
-
-const AppRouter: React.FC<Props> = (props: Props) => {
+const AppRouter: React.FC = () => {
   const auth = useAuth();
   if (auth.isLoading()) return null;
-
-  // props.dispatch(TenantActions.ensureLoaded());
-  // console.log(props.tenantState);
-  // if (props.tenantState.loadingStatus !== 'LOADED') {
-  //   /**
-  //    * Don't render anything if the tenant loading status is not loaded yet. Normally we wouldn't
-  //    * care to add a restriction like this because we'd rather do a partial load, but for the case
-  //    * of tenant at the app level, it's important to wait to load before continuing because the
-  //    * Auth0 SPA wrapper requires tenant information.
-  //    */
-  //   return null;
-  // }
 
   return <Router>
     <Switch>
--- limber-web/src/App/useAuth.tsx
+++ limber-web/src/App/useAuth.tsx
@@ -3,27 +3,37 @@ import { useAuth0 } from '../react-auth0-wrapper';
 
 export function useAuth() {
   const mechanism = process.env['REACT_APP_AUTH_MECHANISM'];
+
   const auth0 = useAuth0();
-  if (mechanism === 'LOCAL_STORAGE_JWT') {
-    return {
-      isLoading: () => false,
-      isAuthenticated: () => Boolean(localStorage.getItem('jwt')),
-      getTokenSilently: () => localStorage.getItem('jwt'),
-      login: () => {
-        // TODO: Redirect to rootUrl
-      },
-      logout: () => {
-        localStorage.removeItem('jwt');
-        // TODO: Redirect to rootUrl
-      },
-    };
-  } else {
-    return {
-      isLoading: () => auth0.loading,
-      isAuthenticated: () => auth0.isAuthenticated,
-      getTokenSilently: () => auth0.getTokenSilently(),
-      login: () => auth0.loginWithRedirect(),
-      logout: () => auth0.logout({ returnTo: rootUrl }),
-    };
+
+  switch (mechanism) {
+    case 'LOCAL_STORAGE_JWT':
+      return localStorageAuth();
+    default:
+      return auth0Auth(auth0);
   }
+}
+
+function localStorageAuth() {
+  return {
+    isLoading: () => false,
+    isAuthenticated: () => Boolean(localStorage.getItem('jwt')),
+    getTokenSilently: () => localStorage.getItem('jwt'),
+    login: () => {
+    },
+    logout: () => {
+      localStorage.removeItem('jwt');
+      window.location.assign(rootUrl);
+    },
+  };
+}
+
+function auth0Auth(auth0: any) {
+  return {
+    isLoading: () => auth0.loading,
+    isAuthenticated: () => auth0.isAuthenticated,
+    getTokenSilently: () => auth0.getTokenSilently(),
+    login: () => auth0.loginWithRedirect(),
+    logout: () => auth0.logout({ returnTo: rootUrl }),
+  };
 }
