f668bb9e47d688937d76670c311d6da07a2a66e6
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-02 16:53:56 -0600

    Remove old comment

--- piper/validation/src/main/kotlin/com/piperframework/validation/ValueValidation.kt
+++ piper/validation/src/main/kotlin/com/piperframework/validation/ValueValidation.kt
@@ -1,9 +1,5 @@
 package com.piperframework.validation
 
-/**
- * The Validation class provides utilities for parameter validation. They're all stored outside of the class in
- * extension functions, or else this class would get huge.
- */
 data class ValueValidation<T>(val name: String, val value: T)
 
 fun <T : Any> ValueValidation<T?>.ifPresent(validator: ValueValidation<T>.() -> Boolean): Boolean {
