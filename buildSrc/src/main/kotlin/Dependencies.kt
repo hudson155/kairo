827ae2a42120fd19d691577b2d154cd3f8c0f235
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-01 19:57:48 -0600

    Alphabetize dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,30 @@
 object Dependencies {
 
+    object Bcrypt {
+        const val jbcrypt = "org.mindrot:jbcrypt:${Versions.jbcrypt}"
+    }
+
+    object Guice {
+        const val guice = "com.google.inject:guice:${Versions.guice}"
+    }
+
+    object JUnit {
+        const val api = "org.junit.jupiter:junit-jupiter-api:${Versions.junit}"
+        const val engine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit}"
+    }
+
+    object Jackson {
+        const val annotations = "com.fasterxml.jackson.core:jackson-annotations:${Versions.jackson}"
+        const val dataFormatYaml = "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:${Versions.jackson}"
+        const val dataTypeJsr310 = "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jackson}"
+        const val moduleKotlin = "com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}"
+    }
+
+    object Jwt {
+        const val auth0JavaJwt = "com.auth0:java-jwt:${Versions.auth0JavaJwt}"
+        const val auth0JwksRsa = "com.auth0:jwks-rsa:${Versions.auth0JwksRsa}"
+    }
+
     object Ktor {
         const val auth = "io.ktor:ktor-auth:${Versions.ktor}"
         const val httpJvm = "io.ktor:ktor-http-jvm:${Versions.ktor}"
@@ -10,28 +35,12 @@ object Dependencies {
         const val test = "io.ktor:ktor-server-test-host:${Versions.ktor}"
     }
 
-    object Bcrypt {
-        const val jbcrypt = "org.mindrot:jbcrypt:${Versions.jbcrypt}"
-    }
-
-    object Jwt {
-        const val auth0JavaJwt = "com.auth0:java-jwt:${Versions.auth0JavaJwt}"
-        const val auth0JwksRsa = "com.auth0:jwks-rsa:${Versions.auth0JwksRsa}"
-    }
-
-    object Jackson {
-        const val annotations = "com.fasterxml.jackson.core:jackson-annotations:${Versions.jackson}"
-        const val dataFormatYaml = "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:${Versions.jackson}"
-        const val dataTypeJsr310 = "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jackson}"
-        const val moduleKotlin = "com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}"
-    }
-
-    object Guice {
-        const val guice = "com.google.inject:guice:${Versions.guice}"
-    }
-
     object Logback {
         const val logbackClassic = "ch.qos.logback:logback-classic:${Versions.logback}"
+    }
+
+    object MockK {
+        const val mockK = "io.mockk:mockk:${Versions.mockK}"
     }
 
     object Sql {
@@ -39,14 +48,5 @@ object Dependencies {
         const val flyway = "org.flywaydb:flyway-core:${Versions.flyway}"
         const val hikari = "com.zaxxer:HikariCP:${Versions.hikari}"
         const val postgres = "org.postgresql:postgresql:${Versions.postgres}"
-    }
-
-    object JUnit {
-        const val api = "org.junit.jupiter:junit-jupiter-api:${Versions.junit}"
-        const val engine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit}"
-    }
-
-    object MockK {
-        const val mockK = "io.mockk:mockk:${Versions.mockK}"
     }
 }
