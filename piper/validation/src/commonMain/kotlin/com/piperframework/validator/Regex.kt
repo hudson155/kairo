167865d693bbd4d8e92c8d83a2891e9a1ba40993
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-02 20:55:12 -0600

    Don't use Regex flags

--- piper/validation/src/commonMain/kotlin/com/piperframework/validator/Regex.kt
+++ piper/validation/src/commonMain/kotlin/com/piperframework/validator/Regex.kt
@@ -2,38 +2,29 @@ package com.piperframework.validator
 
 internal object Regex {
 
-    val auth0ClientId = Regex("[a-z0-9]{22}", RegexOption.IGNORE_CASE)
+    val auth0ClientId = Regex("[A-Za-z0-9]{22}")
 
     val base64EncodedUuid = Regex("[A-Za-z0-9+/]{21}[AQgw]==")
 
-    val hostname = Regex(
-        pattern = "[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)+",
-        option = RegexOption.IGNORE_CASE
-    )
-
-    val emailAddress = Regex(
-        pattern = "[a-z0-9.!#\$%&'*+/=?^_`{|}~-]+@$hostname",
-        option = RegexOption.IGNORE_CASE
-    )
-
-    private val urlChar = Regex("[a-z0-9\\-_.]", RegexOption.IGNORE_CASE)
-
-    val path = Regex("(?:/$urlChar+)*/?", RegexOption.IGNORE_CASE)
-
-    val url = run {
-        val protocol = Regex("https?://", RegexOption.IGNORE_CASE)
-        val queryString = Regex(
-            pattern = "(?:\\?$urlChar+=$urlChar*(?:&$urlChar+=$urlChar*)*)?",
-            option = RegexOption.IGNORE_CASE
-        )
-        val hash = Regex("(?:#$urlChar*)?", RegexOption.IGNORE_CASE)
-        return@run Regex(
-            pattern = listOf(protocol, hostname, path, queryString, hash).joinToString(""),
-            option = RegexOption.IGNORE_CASE
-        )
+    val hostname = run {
+        val portion = Regex("[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])")
+        return@run Regex("$portion?(?:\\.$portion?)+")
     }
 
-    private val hex = Regex("[a-f0-9]", RegexOption.IGNORE_CASE)
+    val emailAddress = Regex("[A-Za-z0-9.!#\$%&'*+/=?^_`{|}~-]+@$hostname")
 
-    val uuid = Regex("$hex{8}-?$hex{4}-?$hex{4}-?$hex{4}-?$hex{12}", RegexOption.IGNORE_CASE)
+    private val urlChar = Regex("[A-Za-z0-9\\-_.]")
+
+    val path = Regex("(?:/$urlChar+)*/?")
+
+    val url = run {
+        val protocol = Regex("[Hh][Tt][Tt][Pp][Ss]?://")
+        val queryString = Regex("(?:\\?$urlChar+=$urlChar*(?:&$urlChar+=$urlChar*)*)?")
+        val hash = Regex("(?:#$urlChar*)?")
+        return@run Regex(listOf(protocol, hostname, path, queryString, hash).joinToString(""))
+    }
+
+    private val hex = Regex("[A-Fa-f0-9]")
+
+    val uuid = Regex("$hex{8}-?$hex{4}-?$hex{4}-?$hex{4}-?$hex{12}")
 }
