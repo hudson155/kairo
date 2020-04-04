cf40a3a98be97014e68face03027ea2cec53b327
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-04 14:02:15 -0600

    Remove unnecessary property

--- piper/src/main/kotlin/com/piperframework/contentNegotiation/JsonContentConverter.kt
+++ piper/src/main/kotlin/com/piperframework/contentNegotiation/JsonContentConverter.kt
@@ -4,4 +4,4 @@ import com.piperframework.serialization.Json
 import io.ktor.features.ContentConverter
 import io.ktor.jackson.JacksonConverter
 
-class JsonContentConverter(private val json: Json) : ContentConverter by JacksonConverter(json.objectMapper)
+class JsonContentConverter(json: Json) : ContentConverter by JacksonConverter(json.objectMapper)
