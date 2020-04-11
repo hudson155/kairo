6fbfd3f3decb6a12be4dda83b5805080fec7c4a3
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-11 15:38:59 -0600

    Remove unnecessary API call

--- limber-web/src/main/kotlin/io/limberapp/web/app/App.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/App.kt
@@ -1,10 +1,7 @@
 package io.limberapp.web.app
 
-import io.limberapp.web.api.tenant.getTenant
 import io.limberapp.web.app.pages.signInPage.signInPage
 import io.limberapp.web.app.pages.signOutPage.signOutPage
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 import react.RBuilder
 import react.RProps
 import react.buildElement
@@ -15,10 +12,6 @@ import react.router.dom.route
 import react.router.dom.switch
 
 private val app = functionalComponent<RProps> {
-    val mainScope = MainScope()
-    mainScope.launch {
-        val videos = getTenant("foo")
-    }
     browserRouter {
         switch {
             route(path = "/signin", exact = true) { buildElement { signInPage() } }
