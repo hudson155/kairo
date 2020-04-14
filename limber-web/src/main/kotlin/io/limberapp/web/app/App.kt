93057a1bb643b08ddf788188d42f01587e939cc9
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-13 19:16:32 -0600

    Refactor App file

--- limber-web/src/main/kotlin/io/limberapp/web/app/App.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/App.kt
@@ -1,7 +1,16 @@
 package io.limberapp.web.app
 
+import io.limberapp.backend.module.orgs.rep.org.FeatureRep
 import io.limberapp.web.api.tenant.getTenant
+import io.limberapp.web.app.components.footer.footer
+import io.limberapp.web.app.components.navbar.navbar
+import io.limberapp.web.app.components.page.page
+import io.limberapp.web.app.pages.featurePage.featurePage
+import io.limberapp.web.app.pages.notFoundPage.notFoundPage
+import io.limberapp.web.app.pages.signInPage.signInPage
+import io.limberapp.web.app.pages.signOutPage.signOutPage
 import io.limberapp.web.context.auth0.authProvider
+import io.limberapp.web.context.auth0.useAuth
 import io.limberapp.web.context.globalState.action.tenant.BeginLoadingTenant
 import io.limberapp.web.context.globalState.action.tenant.SetTenant
 import io.limberapp.web.context.globalState.useGlobalState
@@ -11,11 +20,43 @@ import io.limberapp.web.util.rootDomain
 import kotlinext.js.jsObject
 import react.RBuilder
 import react.RProps
+import react.buildElement
 import react.child
 import react.functionalComponent
+import react.router.dom.browserRouter
+import react.router.dom.redirect
+import react.router.dom.route
+import react.router.dom.switch
 import react.useEffect
 import kotlin.browser.document
 import kotlin.browser.window
+
+private val app = functionalComponent<RProps> {
+
+    val global = useGlobalState()
+
+    useEffect {
+        if (global.state.tenant.hasBegunLoading) return@useEffect
+        global.dispatch(BeginLoadingTenant)
+        async {
+            val tenant = getTenant(rootDomain)
+            global.dispatch(SetTenant(tenant))
+        }
+    }
+
+    if (!global.state.tenant.isLoaded) return@functionalComponent
+
+    authProvider(
+        clientId = checkNotNull(global.state.tenant.state).auth0ClientId,
+        onRedirectCallback = onRedirectCallback
+    ) {
+        child(appWithAuth)
+    }
+}
+
+internal fun RBuilder.app() {
+    child(app)
+}
 
 private val onRedirectCallback: (AppState?) -> Unit = {
     window.history.replaceState(
@@ -25,25 +66,34 @@ private val onRedirectCallback: (AppState?) -> Unit = {
     )
 }
 
-private val app = functionalComponent<RProps> {
-    val global = useGlobalState()
-    useEffect {
-        if (global.state.tenant.hasBegunLoading) return@useEffect
-        global.dispatch(BeginLoadingTenant)
-        async {
-            val tenant = getTenant(rootDomain)
-            global.dispatch(SetTenant(tenant))
+private val appWithAuth = functionalComponent<RProps> {
+
+    val auth = useAuth()
+    if (auth.isLoading) return@functionalComponent
+
+    browserRouter {
+        switch {
+            route(path = "/signin", exact = true) { buildElement { signInPage() } }
+            route(path = "/signout", exact = true) { buildElement { signOutPage() } }
+            route(path = "/") { buildElement { child(appMain) } }
         }
-    }
-    if (!global.state.tenant.isLoaded) return@functionalComponent
-    authProvider(
-        clientId = checkNotNull(global.state.tenant.state).auth0ClientId,
-        onRedirectCallback = onRedirectCallback
-    ) {
-        appWithGlobalState()
     }
 }
 
-internal fun RBuilder.app() {
-    child(app)
+private val appMain = functionalComponent<RProps> {
+
+    val global = useGlobalState()
+    val features = global.state.org.state!!.features.toList()
+
+    page(header = buildElement { navbar(features, "Firstname Lastname") }, footer = buildElement { footer() }) {
+        switch {
+            features.default?.let { route(path = "/", exact = true) { redirect(from = "/", to = it.path) } }
+            features.map { feature ->
+                route(path = feature.path, exact = true) { buildElement { featurePage(feature) } }
+            }
+            route(path = "/") { buildElement { notFoundPage() } }
+        }
+    }
 }
+
+internal val List<FeatureRep.Complete>.default get() = singleOrNull { it.isDefaultFeature }
