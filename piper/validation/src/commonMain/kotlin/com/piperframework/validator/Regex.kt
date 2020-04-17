9cf4eb3677b67a28565d7839d8b8875eb288aebf
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-17 16:13:02 -0600

    Fix regex expressions for JS

--- piper/validation/src/commonMain/kotlin/com/piperframework/validator/Regex.kt
+++ piper/validation/src/commonMain/kotlin/com/piperframework/validator/Regex.kt
@@ -1,5 +1,10 @@
 package com.piperframework.validator
 
+/**
+ * Important Note: When building a Regex object by referencing other Regex options, it's important to use regex.pattern
+ * instead of just regex, since when Regex objects are compiled to JS they have "/g" added to the end, which screws up
+ * concatenation.
+ */
 internal object Regex {
 
     val auth0ClientId = Regex("[A-Za-z0-9]{32}")
@@ -8,23 +13,24 @@ internal object Regex {
 
     val hostname = run {
         val portion = Regex("[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])")
-        return@run Regex("$portion?(?:\\.$portion?)+")
+        return@run Regex("${portion.pattern}?(?:\\.${portion.pattern}?)+")
     }
 
-    val emailAddress = Regex("[A-Za-z0-9.!#\$%&'*+/=?^_`{|}~-]+@$hostname")
+    val emailAddress = Regex("[A-Za-z0-9.!#\$%&'*+/=?^_`{|}~-]+@${hostname.pattern}")
 
     private val urlChar = Regex("[A-Za-z0-9\\-_.]")
 
-    val path = Regex("(?:/$urlChar+)*/?")
+    val path = Regex("(?:/${urlChar.pattern}+)*/?")
 
     val url = run {
         val protocol = Regex("[Hh][Tt][Tt][Pp][Ss]?://")
-        val queryString = Regex("(?:\\?$urlChar+=$urlChar*(?:&$urlChar+=$urlChar*)*)?")
-        val hash = Regex("(?:#$urlChar*)?")
+        val queryString =
+            Regex("(?:\\?${urlChar.pattern}+=${urlChar.pattern}*(?:&${urlChar.pattern}+=${urlChar.pattern}*)*)?")
+        val hash = Regex("(?:#${urlChar.pattern}*)?")
         return@run Regex(listOf(protocol, hostname, path, queryString, hash).joinToString(""))
     }
 
     private val hex = Regex("[A-Fa-f0-9]")
 
-    val uuid = Regex("$hex{8}-?$hex{4}-?$hex{4}-?$hex{4}-?$hex{12}")
+    val uuid = Regex("${hex.pattern}{8}-?${hex.pattern}{4}-?${hex.pattern}{4}-?${hex.pattern}{4}-?${hex.pattern}{12}")
 }
