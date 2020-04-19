40f5b28cdf33ebe5496b6b68c1dd209ebc7a828d
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-18 18:55:01 -0600

    Remove unnecessary detekt step

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,18 +18,6 @@ tasks.create("downloadDependencies") {
     }
 }
 
-detekt {
-    config = files("$rootDir/.detekt/config.yml")
-    input = files(
-        "src/commonMain/kotlin",
-        "src/commonTest/kotlin",
-        "src/jsMain/kotlin",
-        "src/jsTest/kotlin",
-        "src/jvmMain/kotlin",
-        "src/jvmTest/kotlin"
-    )
-}
-
 subprojects {
 
     buildscript {
