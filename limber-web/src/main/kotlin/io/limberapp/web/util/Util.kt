c9c4b859b4b1f4b355d217d68a10d19ae017a883
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-24 12:06:22 -0600

    Fix lint

--- limber-web/src/main/kotlin/io/limberapp/web/util/Util.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/util/Util.kt
@@ -16,6 +16,4 @@ internal fun async(block: suspend () -> Unit) {
 }
 
 @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
-internal fun buildElements(handler: RBuilder.() -> Unit): ReactElement? {
-    return react.buildElements(handler) as ReactElement?
-}
+internal fun buildElements(handler: RBuilder.() -> Unit) = react.buildElements(handler) as ReactElement?
