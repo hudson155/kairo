aa85a4d7ce445d29430f3cdfbecf448d6363e11a
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-25 14:43:55 -0600

    Health check rep must be serializable

--- limber-backend-application/src/main/kotlin/io/limberapp/backend/module/healthCheck/rep/healthCheck/HealthCheckRep.kt
+++ limber-backend-application/src/main/kotlin/io/limberapp/backend/module/healthCheck/rep/healthCheck/HealthCheckRep.kt
@@ -1,6 +1,8 @@
 package io.limberapp.backend.module.healthCheck.rep.healthCheck
 
 import com.piperframework.rep.CompleteRep
+import com.piperframework.serialization.serializer.LocalDateTimeSerializer
+import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 
 internal object HealthCheckRep {
@@ -9,7 +11,9 @@ internal object HealthCheckRep {
         UNHEALTHY;
     }
 
+    @Serializable
     data class Complete(
+        @Serializable(with = LocalDateTimeSerializer::class)
         override val createdDate: LocalDateTime,
         val state: State
     ) : CompleteRep
