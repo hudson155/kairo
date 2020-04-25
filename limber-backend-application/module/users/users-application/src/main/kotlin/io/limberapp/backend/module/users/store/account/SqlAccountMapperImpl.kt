224c42eb794dfaae0d4374cd6f7375bdacd04ed1
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-24 21:32:02 -0600

    Preserve ordering

--- limber-backend-application/module/users/users-application/src/main/kotlin/io/limberapp/backend/module/users/store/account/SqlAccountMapperImpl.kt
+++ limber-backend-application/module/users/users-application/src/main/kotlin/io/limberapp/backend/module/users/store/account/SqlAccountMapperImpl.kt
@@ -14,17 +14,17 @@ internal class SqlAccountMapperImpl @Inject constructor() : SqlAccountMapper {
     override fun accountEntity(insertStatement: InsertStatement<*>, model: UserModel) {
         insertStatement[AccountTable.createdDate] = model.created
         insertStatement[AccountTable.guid] = model.id
-        insertStatement[AccountTable.name] = "${model.firstName} ${model.lastName}"
         insertStatement[AccountTable.identityProvider] = model.identityProvider
         insertStatement[AccountTable.superuser] = model.superuser
+        insertStatement[AccountTable.name] = "${model.firstName} ${model.lastName}"
     }
 
     override fun userEntity(insertStatement: InsertStatement<*>, model: UserModel) {
         insertStatement[UserTable.createdDate] = model.created
         insertStatement[UserTable.guid] = model.id
-        insertStatement[UserTable.name] = "${model.firstName} ${model.lastName}"
         insertStatement[UserTable.identityProvider] = model.identityProvider
         insertStatement[UserTable.superuser] = model.superuser
+        insertStatement[UserTable.name] = "${model.firstName} ${model.lastName}"
         insertStatement[UserTable.orgGuid] = model.orgId
         insertStatement[UserTable.emailAddress] = model.emailAddress
         insertStatement[UserTable.firstName] = model.firstName
@@ -33,10 +33,10 @@ internal class SqlAccountMapperImpl @Inject constructor() : SqlAccountMapper {
     }
 
     override fun userEntity(updateStatement: UpdateStatement, update: UserModel.Update) {
-        update.firstName?.let { updateStatement[UserTable.firstName] = it }
-        update.lastName?.let { updateStatement[UserTable.lastName] = it }
         update.identityProvider?.let { updateStatement[UserTable.identityProvider] = it }
         update.superuser?.let { updateStatement[UserTable.superuser] = it }
+        update.firstName?.let { updateStatement[UserTable.firstName] = it }
+        update.lastName?.let { updateStatement[UserTable.lastName] = it }
     }
 
     override fun accountModel(resultRow: ResultRow) = AccountModel(
@@ -50,12 +50,12 @@ internal class SqlAccountMapperImpl @Inject constructor() : SqlAccountMapper {
     override fun userModel(resultRow: ResultRow) = UserModel(
         id = resultRow[UserTable.guid],
         created = resultRow[UserTable.createdDate],
+        identityProvider = resultRow[UserTable.identityProvider],
+        superuser = resultRow[UserTable.superuser],
         orgId = resultRow[UserTable.orgGuid],
         firstName = resultRow[UserTable.firstName],
         lastName = resultRow[UserTable.lastName],
         emailAddress = resultRow[UserTable.emailAddress],
-        profilePhotoUrl = resultRow[UserTable.profilePhotoUrl],
-        identityProvider = resultRow[UserTable.identityProvider],
-        superuser = resultRow[UserTable.superuser]
+        profilePhotoUrl = resultRow[UserTable.profilePhotoUrl]
     )
 }
