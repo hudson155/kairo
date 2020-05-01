8dc692f936a36626bb6ef59c34fa3ee88e848ef5
Jeff Hudson <jeff.hudson@toasttab.com>
2020-04-30 22:14:35 -0600

    Write manual release script

---
+++ adhoc/manualRelease.sh
@@ -0,0 +1,16 @@
+# Manually releases the current code
+#
+# adhoc/manualRelease.sh
+
+./gradlew clean
+./gradlew build
+docker build -t registry.digitalocean.com/limber/limber-backend:master limber-backend-application
+docker tag registry.digitalocean.com/limber/limber-backend:master registry.digitalocean.com/limber/limber-backend:$(git rev-parse HEAD)
+docker push registry.digitalocean.com/limber/limber-backend:master
+docker push registry.digitalocean.com/limber/limber-backend:$(git rev-parse HEAD)
+docker build -t registry.digitalocean.com/limber/limber-web:master limber-web
+docker tag registry.digitalocean.com/limber/limber-web:master registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)
+docker push registry.digitalocean.com/limber/limber-web:master
+docker push registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)
+kubectl set image deployment/limber-backend limber-backend=registry.digitalocean.com/limber/limber-backend:$(git rev-parse HEAD) --namespace limber
+kubectl set image deployment/limber-web limber-web=registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD) --namespace limber
