c506b1d7902390c74f1d28bcf2512b5a0afdbf53
Jeff Hudson <jeff.hudson@toasttab.com>
2020-05-04 13:32:00 -0600

    Center not found page

--- limber-web/src/main/kotlin/io/limberapp/web/app/pages/notFoundPage/NotFoundPage.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/pages/notFoundPage/NotFoundPage.kt
@@ -1,12 +1,16 @@
 package io.limberapp.web.app.pages.notFoundPage
 
-import io.limberapp.web.app.components.layout.components.layoutTitle.layoutTitle
-import io.limberapp.web.app.components.layout.components.standardLayout.standardLayout
+import io.limberapp.web.app.components.layout.components.centeredContentLayout.centeredContentLayout
+import kotlinx.css.TextAlign
+import kotlinx.css.textAlign
 import react.RBuilder
 import react.RProps
 import react.child
+import react.dom.h1
 import react.dom.p
 import react.functionalComponent
+import styled.css
+import styled.styledDiv
 
 /**
  * "Not found" error message page when no page is found.
@@ -16,8 +20,11 @@ internal fun RBuilder.notFoundPage() {
 }
 
 private val notFoundPage = functionalComponent<RProps> {
-    standardLayout {
-        layoutTitle("Not Found")
-        p { +"We looked everywhere, but we couldn't find the page you were looking for." }
+    centeredContentLayout {
+        styledDiv {
+            css { textAlign = TextAlign.center }
+            h1 { +"Not Found" }
+            p { +"We looked everywhere, but we couldn't find the page you were looking for." }
+        }
     }
 }
