0dcdaeff767bf83c9d012850b083a444b28d6f7a
Jeff Hudson <jeff.hudson@toasttab.com>
2020-05-05 21:04:27 -0600

    Refactor LayoutTitle styles

--- limber-web/src/main/kotlin/io/limberapp/web/app/components/layout/components/layoutTitle/LayoutTitle.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/components/layout/components/layoutTitle/LayoutTitle.kt
@@ -1,6 +1,8 @@
 package io.limberapp.web.app.components.layout.components.layoutTitle
 
+import io.limberapp.web.util.Styles
 import io.limberapp.web.util.Theme
+import io.limberapp.web.util.injectStyles
 import kotlinx.css.BorderStyle
 import kotlinx.css.marginBottom
 import kotlinx.css.marginTop
@@ -9,11 +11,11 @@ import kotlinx.css.px
 import react.RBuilder
 import react.RProps
 import react.child
+import react.dom.div
+import react.dom.h1
+import react.dom.p
 import react.functionalComponent
-import styled.css
-import styled.styledDiv
-import styled.styledH1
-import styled.styledP
+import styled.getClassName
 
 /**
  * The title at the top of a layout, plus an optional description. All pages should have one of these.
@@ -24,27 +26,26 @@ internal fun RBuilder.layoutTitle(title: String, description: String? = null) {
 
 internal data class Props(val title: String, val description: String?) : RProps
 
+private val styles = object : Styles("LayoutTitle") {
+    val container by css {
+        marginBottom = 48.px
+        borderBottom(1.px, BorderStyle.solid, Theme.borderLight)
+    }
+    val title by css {
+        marginTop = 0.px
+        marginBottom = 8.px
+    }
+    val subtitle by css {
+        marginTop = 0.px
+        marginBottom = 8.px
+    }
+}
+
 private val layoutTitle = functionalComponent<Props> { props ->
-    styledDiv {
-        css {
-            marginBottom = 48.px
-            borderBottom(1.px, BorderStyle.solid, Theme.borderLight)
-        }
-        styledH1 {
-            css {
-                marginTop = 0.px
-                marginBottom = 8.px
-            }
-            +props.title
-        }
-        props.description?.let {
-            styledP {
-                css {
-                    marginTop = 0.px
-                    marginBottom = 8.px
-                }
-                +it
-            }
-        }
+    injectStyles(styles)
+
+    div(classes = styles.getClassName { it::container }) {
+        h1(classes = styles.getClassName { it::title }) { +props.title }
+        props.description?.let { p(classes = styles.getClassName { it::subtitle }) { +it } }
     }
 }
