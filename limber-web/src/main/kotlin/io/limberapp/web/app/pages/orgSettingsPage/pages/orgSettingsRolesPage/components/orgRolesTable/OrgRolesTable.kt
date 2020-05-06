6abb55169de8d2a7955a5696bc2a482fa6287cae
Jeff Hudson <jeff.hudson@toasttab.com>
2020-05-05 23:03:58 -0600

    Refactor OrgRolesTable styles

--- limber-web/src/main/kotlin/io/limberapp/web/app/pages/orgSettingsPage/pages/orgSettingsRolesPage/components/orgRolesTable/OrgRolesTable.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/pages/orgSettingsPage/pages/orgSettingsRolesPage/components/orgRolesTable/OrgRolesTable.kt
@@ -7,6 +7,8 @@ import io.limberapp.web.app.pages.orgSettingsPage.pages.orgSettingsRolesPage.com
 import io.limberapp.web.context.api.useApi
 import io.limberapp.web.context.globalState.action.orgRole.ensureOrgRolesLoaded
 import io.limberapp.web.context.globalState.useGlobalState
+import io.limberapp.web.util.Styles
+import io.limberapp.web.util.injectStyles
 import io.limberapp.web.util.withContext
 import kotlinx.css.maxWidth
 import kotlinx.css.padding
@@ -15,19 +17,33 @@ import react.RBuilder
 import react.RProps
 import react.child
 import react.dom.p
+import react.dom.table
 import react.dom.tbody
+import react.dom.th
 import react.dom.thead
 import react.dom.tr
 import react.functionalComponent
-import styled.css
-import styled.styledTable
-import styled.styledTh
+import styled.getClassName
 
 internal fun RBuilder.orgRolesTable() {
     child(orgRolesTable)
 }
 
+private val styles = object : Styles("OrgRolesTable") {
+    val table by css {
+        maxWidth = 768.px
+        kotlinx.css.th {
+            padding(4.px)
+        }
+        kotlinx.css.td {
+            padding(4.px)
+        }
+    }
+}
+
 private val orgRolesTable = functionalComponent<RProps> {
+    injectStyles(styles)
+
     val api = useApi()
     val global = useGlobalState()
 
@@ -43,21 +59,12 @@ private val orgRolesTable = functionalComponent<RProps> {
         return@functionalComponent
     }
 
-    styledTable {
-        css {
-            maxWidth = 768.px
-            kotlinx.css.th {
-                padding(4.px)
-            }
-            kotlinx.css.td {
-                padding(4.px)
-            }
-        }
+    table(classes = styles.getClassName { it::table }) {
         thead {
             tr {
-                styledTh { +"Name" }
-                styledTh { +"Permissions" }
-                styledTh { +"Members" }
+                th { +"Name" }
+                th { +"Permissions" }
+                th { +"Members" }
             }
         }
         tbody {
