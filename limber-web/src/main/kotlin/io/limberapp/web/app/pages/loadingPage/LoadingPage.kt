75f9f1c8d30cb0fdc7abde32098f7f2143a39142
Jeff Hudson <jeff.hudson@toasttab.com>
2020-05-06 16:15:13 -0600

    Update spinner animation code

--- limber-web/src/main/kotlin/io/limberapp/web/app/pages/loadingPage/LoadingPage.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/pages/loadingPage/LoadingPage.kt
@@ -1,32 +1,11 @@
 package io.limberapp.web.app.pages.loadingPage
 
+import io.limberapp.web.app.components.inlineIcon.inlineIcon
 import io.limberapp.web.app.components.layout.components.centeredContentLayout.centeredContentLayout
 import io.limberapp.web.util.Styles
-import io.limberapp.web.util.Theme
-import kotlinx.css.BorderStyle
-import kotlinx.css.BoxSizing
-import kotlinx.css.Color
-import kotlinx.css.Display
-import kotlinx.css.Position
-import kotlinx.css.borderBottomColor
-import kotlinx.css.borderLeftColor
-import kotlinx.css.borderRadius
-import kotlinx.css.borderRightColor
-import kotlinx.css.borderTopColor
-import kotlinx.css.boxSizing
-import kotlinx.css.content
-import kotlinx.css.display
-import kotlinx.css.height
-import kotlinx.css.margin
-import kotlinx.css.pct
-import kotlinx.css.position
-import kotlinx.css.properties.IterationCount
-import kotlinx.css.properties.animation
-import kotlinx.css.properties.border
-import kotlinx.css.properties.s
+import io.limberapp.web.util.globalStyles
+import kotlinx.css.fontSize
 import kotlinx.css.px
-import kotlinx.css.quoted
-import kotlinx.css.width
 import react.RBuilder
 import react.RProps
 import react.child
@@ -45,32 +24,16 @@ internal fun RBuilder.loadingPage(loadingText: String) {
 internal data class Props(val loadingText: String) : RProps
 
 private val styles = object : Styles("LoadingPage") {
-    val spinner by css {
-        display = Display.inlineBlock
-        position = Position.relative
-        width = 80.px
-        height = 80.px
-        after {
-            content = "".quoted
-            display = Display.block
-            borderRadius = 50.pct
-            width = 0.px
-            height = 0.px
-            margin(8.px)
-            boxSizing = BoxSizing.borderBox
-            border(width = 32.px, style = BorderStyle.solid, color = Theme.backgroundDark)
-            borderTopColor = Theme.backgroundDark
-            borderRightColor = Color.transparent
-            borderBottomColor = Theme.backgroundDark
-            borderLeftColor = Color.transparent
-            animation("loading-spinner", duration = 1.2.s, iterationCount = IterationCount.infinite)
-        }
+    val spinnerContainer by css {
+        fontSize = 48.px
     }
 }.apply { inject() }
 
 private val loadingPage = functionalComponent<Props> { props ->
     centeredContentLayout {
-        div(classes = styles.getClassName { it::spinner }) {}
+        div(classes = styles.getClassName { it::spinnerContainer }) {
+            inlineIcon("spinner", classes = globalStyles.getClassName { it::spinner })
+        }
         p { +props.loadingText }
     }
 }
