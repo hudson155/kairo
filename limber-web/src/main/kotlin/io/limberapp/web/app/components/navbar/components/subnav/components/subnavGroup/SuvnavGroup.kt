62b6aaa5ba2e407a557c31fd72e7b20c18a23e9d
Jeff Hudson <jeff.hudson@toasttab.com>
2020-05-05 22:33:56 -0600

    Refactor SubnavGroup styles

--- limber-web/src/main/kotlin/io/limberapp/web/app/components/navbar/components/subnav/components/subnavGroup/SuvnavGroup.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/components/navbar/components/subnav/components/subnavGroup/SuvnavGroup.kt
@@ -1,6 +1,8 @@
 package io.limberapp.web.app.components.navbar.components.subnav.components.subnavGroup
 
+import io.limberapp.web.util.Styles
 import io.limberapp.web.util.Theme
+import io.limberapp.web.util.injectStyles
 import kotlinx.css.BorderStyle
 import kotlinx.css.Display
 import kotlinx.css.FlexDirection
@@ -13,9 +15,9 @@ import react.RBuilder
 import react.RHandler
 import react.RProps
 import react.child
+import react.dom.div
 import react.functionalComponent
-import styled.css
-import styled.styledDiv
+import styled.getClassName
 
 /**
  * A group of items on a subnav. Items in the same group should be conceptually grouped. The physical spacing between
@@ -25,16 +27,21 @@ internal fun RBuilder.subnavGroup(children: RHandler<RProps>) {
     child(subnavGroup, handler = children)
 }
 
-private val subnavGroup = functionalComponent<RProps> { props ->
-    styledDiv {
-        css {
-            display = Display.flex
-            flexDirection = FlexDirection.column
-            borderBottom(1.px, BorderStyle.solid, Theme.borderLight)
-            lastOfType {
-                borderBottomStyle = BorderStyle.none
-            }
+private val styles = object : Styles("SubnavGroup") {
+    val container by css {
+        display = Display.flex
+        flexDirection = FlexDirection.column
+        borderBottom(1.px, BorderStyle.solid, Theme.borderLight)
+        lastOfType {
+            borderBottomStyle = BorderStyle.none
         }
+    }
+}
+
+private val subnavGroup = functionalComponent<RProps> { props ->
+    injectStyles(styles)
+
+    div(classes = styles.getClassName { it::container }) {
         props.children()
     }
 }
