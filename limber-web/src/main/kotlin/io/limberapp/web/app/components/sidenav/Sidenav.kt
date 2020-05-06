8eff3b777f0193bfd21cc063adbf6875df7a54aa
Jeff Hudson <jeff.hudson@toasttab.com>
2020-05-05 22:25:32 -0600

    Refactor Sidenav styles

--- limber-web/src/main/kotlin/io/limberapp/web/app/components/sidenav/Sidenav.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/components/sidenav/Sidenav.kt
@@ -2,6 +2,8 @@ package io.limberapp.web.app.components.sidenav
 
 import io.limberapp.web.app.components.sidenav.components.sidenavGroup.sidenavGroup
 import io.limberapp.web.app.components.sidenav.components.sidenavItem.sidenavItem
+import io.limberapp.web.util.Styles
+import io.limberapp.web.util.injectStyles
 import kotlinx.css.Display
 import kotlinx.css.FlexDirection
 import kotlinx.css.display
@@ -10,9 +12,9 @@ import react.RBuilder
 import react.RHandler
 import react.RProps
 import react.dom.b
+import react.dom.div
 import react.functionalComponent
-import styled.css
-import styled.styledDiv
+import styled.getClassName
 
 /**
  * Sidenav for navigation within a feature.
@@ -23,12 +25,17 @@ internal fun RBuilder.sidenav(title: String, children: RHandler<RProps>) {
 
 internal data class Props(val title: String) : RProps
 
+private val styles = object : Styles("Sidenav") {
+    val container by css {
+        display = Display.flex
+        flexDirection = FlexDirection.column
+    }
+}
+
 private val sidenav = functionalComponent<Props> { props ->
-    styledDiv {
-        css {
-            display = Display.flex
-            flexDirection = FlexDirection.column
-        }
+    injectStyles(styles)
+
+    div(classes = styles.getClassName { it::container }) {
         sidenavGroup { sidenavItem { b { +props.title } } }
         props.children()
     }
