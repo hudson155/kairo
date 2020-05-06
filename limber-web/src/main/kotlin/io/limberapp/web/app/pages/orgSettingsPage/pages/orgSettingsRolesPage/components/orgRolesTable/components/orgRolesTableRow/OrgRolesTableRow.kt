39053247683ae33c796fe620a096576dd553343e
Jeff Hudson <jeff.hudson@toasttab.com>
2020-05-05 21:52:26 -0600

    Refactor OrgRolesTableRow styles

--- limber-web/src/main/kotlin/io/limberapp/web/app/pages/orgSettingsPage/pages/orgSettingsRolesPage/components/orgRolesTable/components/orgRolesTableRow/OrgRolesTableRow.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/pages/orgSettingsPage/pages/orgSettingsRolesPage/components/orgRolesTable/components/orgRolesTableRow/OrgRolesTableRow.kt
@@ -1,6 +1,8 @@
 package io.limberapp.web.app.pages.orgSettingsPage.pages.orgSettingsRolesPage.components.orgRolesTable.components.orgRolesTableRow
 
+import io.limberapp.web.util.Styles
 import io.limberapp.web.util.Theme
+import io.limberapp.web.util.injectStyles
 import kotlinx.css.BorderStyle
 import kotlinx.css.backgroundColor
 import kotlinx.css.properties.borderBottom
@@ -10,25 +12,30 @@ import react.RBuilder
 import react.RHandler
 import react.RProps
 import react.child
+import react.dom.tr
 import react.functionalComponent
-import styled.css
-import styled.styledTr
+import styled.getClassName
 
 internal fun RBuilder.orgRolesTableRow(children: RHandler<RProps>) {
     child(orgRolesTableRow, handler = children)
 }
 
-private val orgRolesTableRow = functionalComponent<RProps> { props ->
-    styledTr {
-        css {
-            borderTop(1.px, BorderStyle.solid, Theme.borderLight)
-            lastOfType {
-                borderBottom(1.px, BorderStyle.solid, Theme.borderLight)
-            }
-            hover {
-                backgroundColor = Theme.backgroundLightSubtleAccent
-            }
+private val styles = object : Styles("OrgRolesTableRow") {
+    val row by css {
+        borderTop(1.px, BorderStyle.solid, Theme.borderLight)
+        lastOfType {
+            borderBottom(1.px, BorderStyle.solid, Theme.borderLight)
         }
+        hover {
+            backgroundColor = Theme.backgroundLightSubtleAccent
+        }
+    }
+}
+
+private val orgRolesTableRow = functionalComponent<RProps> { props ->
+    injectStyles(styles)
+
+    tr(classes = styles.getClassName { it::row }) {
         props.children()
     }
 }
