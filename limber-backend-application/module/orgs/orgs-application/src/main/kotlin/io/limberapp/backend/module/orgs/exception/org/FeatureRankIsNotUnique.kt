6f62314bd44012bff4eb6a80a89cec71b5508823
Jeff Hudson <jeff.hudson@toasttab.com>
2020-06-04 14:27:56 -0600

    Fix typo

--- limber-backend-application/module/orgs/orgs-application/src/main/kotlin/io/limberapp/backend/module/orgs/exception/org/FeatureRankIsNotUnique.kt
+++ limber-backend-application/module/orgs/orgs-application/src/main/kotlin/io/limberapp/backend/module/orgs/exception/org/FeatureRankIsNotUnique.kt
@@ -3,7 +3,7 @@ package io.limberapp.backend.module.orgs.exception.org
 import com.piperframework.exception.exception.conflict.ConflictException
 
 internal class FeatureRankIsNotUnique : ConflictException(
-  message = "The feature cannot have the same r as another feature.",
+  message = "The feature cannot have the same rank as another feature.",
   developerMessage = "This exception should be thrown when an attempt is made to create or update a feature within an" +
     " org and there is already a feature with the same rank."
 )
