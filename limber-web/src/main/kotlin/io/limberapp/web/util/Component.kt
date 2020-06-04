b1005a10858857a161d6302a80ac806d0a916a86
Jeff Hudson <jeff.hudson@toasttab.com>
2020-06-04 01:22:48 -0600

    Document component classes

--- limber-web/src/main/kotlin/io/limberapp/web/util/Component.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/util/Component.kt
@@ -4,14 +4,24 @@ import io.limberapp.web.context.api.useApi
 import io.limberapp.web.context.globalState.useGlobalState
 import react.*
 
+/**
+ * Many components will use this class. It should be the default unless behaviours for more advanced components are
+ * required.
+ */
 internal open class Component
 
+/**
+ * If a component needs to access the global state it should do so by using this class.
+ */
 internal open class ComponentWithGlobalState : Component() {
   private val global = useGlobalState()
   val gs = global.state
   val dispatch = global.dispatch
 }
 
+/**
+ * If a component needs to access the API it should do so by using this class.
+ */
 internal open class ComponentWithApi : ComponentWithGlobalState() {
   val api = useApi()
 }
