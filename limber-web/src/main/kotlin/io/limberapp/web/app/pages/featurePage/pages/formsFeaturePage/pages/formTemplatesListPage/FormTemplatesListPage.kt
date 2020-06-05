a2985217640011b0f388b3b5f7160365bee54c9a
Jeff Hudson <jeff.hudson@toasttab.com>
2020-06-05 15:06:01 -0600

    Use dynamic title for form pages

--- limber-web/src/main/kotlin/io/limberapp/web/app/pages/featurePage/pages/formsFeaturePage/pages/formTemplatesListPage/FormTemplatesListPage.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/pages/featurePage/pages/formsFeaturePage/pages/formTemplatesListPage/FormTemplatesListPage.kt
@@ -1,5 +1,6 @@
 package io.limberapp.web.app.pages.featurePage.pages.formsFeaturePage.pages.formTemplatesListPage
 
+import io.limberapp.backend.module.orgs.rep.org.FeatureRep
 import io.limberapp.web.app.components.layout.components.layoutTitle.layoutTitle
 import io.limberapp.web.util.component
 import react.*
@@ -7,15 +8,17 @@ import react.*
 /**
  * Lists all form templates within the feature.
  */
-internal fun RBuilder.formTemplatesListPage() {
-  child(component)
+internal fun RBuilder.formTemplatesListPage(feature: FeatureRep.Complete) {
+  child(component, Props(feature))
 }
+
+internal data class Props(val feature: FeatureRep.Complete) : RProps
 
 internal object FormTemplatesListPage {
   const val name = "Templates"
   const val subpath = "/templates"
 }
 
-private val component = component<RProps> component@{
-  layoutTitle(FormTemplatesListPage.name)
+private val component = component<Props> component@{ props ->
+  layoutTitle("${props.feature.name} (${FormTemplatesListPage.name.toLowerCase()})")
 }
