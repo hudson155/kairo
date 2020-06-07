f67d94fa48047e7c1713eea7244768f3ef65ef99
Jeff Hudson <jhudson@jhudson.ca>
2020-06-07 15:21:50 -0600

    Change Docker registry URL (#530)

--- adhoc/manualRelease.sh
+++ adhoc/manualRelease.sh
@@ -6,13 +6,13 @@ set -e
 
 ./gradlew clean
 ENV=production ./gradlew build
-docker build -t registry.digitalocean.com/limber/limber-backend:master limber-backend-application
-docker tag registry.digitalocean.com/limber/limber-backend:master registry.digitalocean.com/limber/limber-backend:$(git rev-parse HEAD)
-docker push registry.digitalocean.com/limber/limber-backend:master
-docker push registry.digitalocean.com/limber/limber-backend:$(git rev-parse HEAD)
-docker build -t registry.digitalocean.com/limber/limber-web:master limber-web
-docker tag registry.digitalocean.com/limber/limber-web:master registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)
-docker push registry.digitalocean.com/limber/limber-web:master
-docker push registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)
-kubectl set image deployment/limber-backend limber-backend=registry.digitalocean.com/limber/limber-backend:$(git rev-parse HEAD) --namespace limber
-kubectl set image deployment/limber-web limber-web=registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD) --namespace limber
+docker build -t registry.digitalocean.com/limber-registry/limber-backend:master limber-backend-application
+docker tag registry.digitalocean.com/limber-registry/limber-backend:master registry.digitalocean.com/limber-registry/limber-backend:$(git rev-parse HEAD)
+docker push registry.digitalocean.com/limber-registry/limber-backend:master
+docker push registry.digitalocean.com/limber-registry/limber-backend:$(git rev-parse HEAD)
+docker build -t registry.digitalocean.com/limber-registry/limber-web:master limber-web
+docker tag registry.digitalocean.com/limber-registry/limber-web:master registry.digitalocean.com/limber-registry/limber-web:$(git rev-parse HEAD)
+docker push registry.digitalocean.com/limber-registry/limber-web:master
+docker push registry.digitalocean.com/limber-registry/limber-web:$(git rev-parse HEAD)
+kubectl set image deployment/limber-backend limber-backend=registry.digitalocean.com/limber-registry/limber-backend:$(git rev-parse HEAD) --namespace limber
+kubectl set image deployment/limber-web limber-web=registry.digitalocean.com/limber-registry/limber-web:$(git rev-parse HEAD) --namespace limber
