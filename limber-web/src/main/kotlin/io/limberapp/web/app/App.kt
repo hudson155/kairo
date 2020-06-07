f7de10b51d539ed177da7bddc5a50d930bc0ef41
Jeff Hudson <jeff.hudson@toasttab.com>
2020-06-06 19:37:04 -0600

    Remove some KDoc

--- limber-web/src/main/kotlin/io/limberapp/web/app/App.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/App.kt
@@ -7,9 +7,7 @@ import react.*
 import react.router.dom.*
 
 /**
- * The application root. There are a number of components in this directory that start with "With". Each of them is part
- * of the application root, but unfortunately they need to be in different files due to the way providers and hooks
- * work. We can't call a hook to "use" something that is provided in the same component.
+ * The application root.
  */
 internal fun RBuilder.app() {
   child(component)
