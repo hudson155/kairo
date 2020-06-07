e83d7a95320795bb5f6148048dbd42d14b8821e1
Jeff Hudson <jeff.hudson@toasttab.com>
2020-06-06 19:30:49 -0600

    Refactor application root to better support Segment

--- limber-web/src/main/kotlin/io/limberapp/web/app/App.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/App.kt
@@ -1,15 +1,30 @@
 package io.limberapp.web.app
 
+import io.limberapp.web.context.api.apiProvider
+import io.limberapp.web.context.globalState.globalStateProvider
 import io.limberapp.web.util.component
 import react.*
+import react.router.dom.*
 
 /**
- * Delegate for the application root.
+ * The application root. There are a number of components in this directory that start with "With". Each of them is part
+ * of the application root, but unfortunately they need to be in different files due to the way providers and hooks
+ * work. We can't call a hook to "use" something that is provided in the same component.
  */
 internal fun RBuilder.app() {
   child(component)
 }
 
 private val component = component<RProps> component@{
-  appWithGlobalState()
+  globalStateProvider {
+    withAuth {
+      apiProvider {
+        browserRouter {
+          switch {
+            appRootRouter()
+          }
+        }
+      }
+    }
+  }
 }
--- limber-web/src/main/kotlin/io/limberapp/web/app/AppRootRouter.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/AppRootRouter.kt
@@ -2,6 +2,7 @@ package io.limberapp.web.app
 
 import io.limberapp.web.app.components.minimalPage.LinkType
 import io.limberapp.web.app.components.minimalPage.minimalPage
+import io.limberapp.web.app.components.pageTracker.pageTracker
 import io.limberapp.web.app.pages.loadingPage.loadingPage
 import io.limberapp.web.app.pages.signInPage.SignInPage
 import io.limberapp.web.app.pages.signInPage.signInPage
@@ -15,11 +16,9 @@ import react.*
 import react.router.dom.*
 
 /**
- * Part of the application root.
- *   - Handles auth loading state.
- *   - Handles routing for the unauthenticated application.
+ * Part of the application root. Handles routing for the unauthenticated application.
  */
-internal fun RBuilder.appRouter() {
+internal fun RBuilder.appRootRouter() {
   child(component)
 }
 
@@ -29,15 +28,11 @@ private val component = component<RProps> component@{
   // While auth is loading, show the loading page.
   if (auth.isLoading) return@component minimalPage(linkType = null) { loadingPage("Identifying you...") }
 
-  browserRouter {
-    switch {
-      route(path = SignInPage.path, exact = true) { buildElement { signInPage() } }
-      route(path = SignOutPage.path, exact = true) { buildElement { signOutPage() } }
-      if (auth.isAuthenticated) {
-        route(path = rootPath) { buildElement { appFeatureRouter() } }
-      } else {
-        route(path = rootPath) { buildElement { minimalPage(linkType = LinkType.SIGN_IN) { unauthenticatedPage() } } }
-      }
-    }
+  route(path = SignInPage.path, exact = true) { buildElement { signInPage() } }
+  route(path = SignOutPage.path, exact = true) { buildElement { signOutPage() } }
+  if (auth.isAuthenticated) {
+    route(path = rootPath) { buildElement { appFeatureRouter() } }
+  } else {
+    route(path = rootPath) { buildElement { minimalPage(linkType = LinkType.SIGN_IN) { unauthenticatedPage() } } }
   }
 }
