dd0732bc41cf2f874f3435b4e67da628d0684ab9
Jeff Hudson <jeff.hudson@toasttab.com>
2020-06-11 17:51:07 -0600

    Write service interface for features

---
+++ limber-backend-application/module/auth/auth-service-interface/src/main/kotlin/io/limberapp/backend/module/auth/model/feature/FeatureRoleModel.kt
@@ -0,0 +1,17 @@
+package io.limberapp.backend.module.auth.model.feature
+
+import io.limberapp.backend.authorization.permissions.featurePermissions.FeaturePermissions
+import java.time.LocalDateTime
+import java.util.*
+
+data class FeatureRoleModel(
+  val guid: UUID,
+  val createdDate: LocalDateTime,
+  val featureGuid: UUID,
+  val orgRoleGuid: UUID,
+  val permissions: FeaturePermissions
+) {
+  data class Update(
+    val permissions: FeaturePermissions?
+  )
+}
---
+++ limber-backend-application/module/auth/auth-service-interface/src/main/kotlin/io/limberapp/backend/module/auth/service/feature/FeatureRoleService.kt
@@ -0,0 +1,14 @@
+package io.limberapp.backend.module.auth.service.feature
+
+import io.limberapp.backend.module.auth.model.feature.FeatureRoleModel
+import java.util.*
+
+interface FeatureRoleService {
+  fun create(model: FeatureRoleModel): FeatureRoleModel
+
+  fun getByFeatureGuid(featureGuid: UUID): Set<FeatureRoleModel>
+
+  fun update(featureGuid: UUID, featureRoleGuid: UUID, update: FeatureRoleModel.Update): FeatureRoleModel
+
+  fun delete(featureGuid: UUID, featureRoleGuid: UUID)
+}
