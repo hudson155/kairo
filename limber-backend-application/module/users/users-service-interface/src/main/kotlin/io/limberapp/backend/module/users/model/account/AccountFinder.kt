b5459a25e066be35f521b47bf04912ec7c9c52e1
Jeff Hudson <jeff.hudson@toasttab.com>
2020-08-03 12:41:15 -0600

    Add missing module annotations to dynamic query interfaces (#644)

--- limber-backend-application/module/users/users-service-interface/src/main/kotlin/io/limberapp/backend/module/users/model/account/AccountFinder.kt
+++ limber-backend-application/module/users/users-service-interface/src/main/kotlin/io/limberapp/backend/module/users/model/account/AccountFinder.kt
@@ -1,7 +1,9 @@
 package io.limberapp.backend.module.users.model.account
 
+import io.limberapp.backend.LimberModule
 import java.util.*
 
+@LimberModule.Users
 interface AccountFinder {
   fun accountGuid(accountGuid: UUID)
 }
