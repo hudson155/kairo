243fd72de1bdf301203c5b609d547688332a0dd7
Noah Guld <nguld12@gmail.com>
2020-08-02 19:58:55 -0700

    Update load to support deps (#635)

--- limber-web/src/main/kotlin/io/limberapp/web/api/Load.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/api/Load.kt
@@ -4,8 +4,8 @@ import com.piperframework.util.Outcome
 import io.limberapp.web.util.async
 import react.*
 
-internal fun <T : Any> load(function: suspend () -> Outcome<T>): Outcome<T>? {
+internal fun <T : Any> load(dependencies: RDependenciesList = emptyList(), function: suspend () -> Outcome<T>?): Outcome<T>? {
   val (state, setState) = useState<Outcome<T>?>(null)
-  useEffect(emptyList()) { async { setState(function()) } }
+  useEffect(dependencies) { async { setState(function()) } }
   return state
 }
