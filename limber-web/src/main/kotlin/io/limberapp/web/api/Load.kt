f1c7c8201666967684782a1656c025eb5d7eac08
Noah Guld <nguld12@gmail.com>
2020-08-02 20:25:06 -0700

    fix formating (#642)

--- limber-web/src/main/kotlin/io/limberapp/web/api/Load.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/api/Load.kt
@@ -4,7 +4,10 @@ import com.piperframework.util.Outcome
 import io.limberapp.web.util.async
 import react.*
 
-internal fun <T : Any> load(dependencies: RDependenciesList = emptyList(), function: suspend () -> Outcome<T>?): Outcome<T>? {
+internal fun <T : Any> load(
+  dependencies: RDependenciesList = emptyList(),
+  function: suspend () -> Outcome<T>?
+): Outcome<T>? {
   val (state, setState) = useState<Outcome<T>?>(null)
   useEffect(dependencies) { async { setState(function()) } }
   return state
