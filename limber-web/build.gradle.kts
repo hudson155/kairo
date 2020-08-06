0ea2f143ac8e8bab3d97d0ff431e7838d94d34d1
Noah Guld <nguld12@gmail.com>
2020-08-05 23:39:51 -0700

    Use history hook for page tracking (#645)
    Co-authored-by: Jeff Hudson <jeff.hudson@toasttab.com>

--- limber-web/build.gradle.kts
+++ limber-web/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
   implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-1.3.72")
   implementation(npm("react-dom", "16.13.1"))
 
-  implementation("org.jetbrains:kotlin-react-router-dom:5.1.2-pre.106-kotlin-1.3.72")
+  implementation("org.jetbrains:kotlin-react-router-dom:5.1.2-pre.110-kotlin-1.3.72")
   implementation(npm("react-router-dom", "5.1.2"))
 
   implementation("org.jetbrains:kotlin-styled:1.0.0-pre.110-kotlin-1.3.72")
--- limber-web/src/main/kotlin/io/limberapp/web/app/components/pageTracker/PageTracker.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/app/components/pageTracker/PageTracker.kt
@@ -18,7 +18,7 @@ internal typealias Props = RProps
 
 private val component = functionalComponent(RBuilder::component)
 private fun RBuilder.component(props: Props) {
-  val location = useLocation()
+  val history = useHistory()
 
   useEffect(emptyList()) {
     process.env.SEGMENT_WRITE_KEY?.let {
@@ -27,7 +27,7 @@ private fun RBuilder.component(props: Props) {
     }
   }
 
-  useEffect(listOf(location.pathname)) {
+  useEffect(listOf(history.location.pathname, history.location.search)) {
     if (process.env.SEGMENT_WRITE_KEY == null) return@useEffect
     window.segment.page()
   }
