439364d4eb4061ae00ad3d5b6590de6fb4be1ef4
Jeff Hudson <jeff.hudson@toasttab.com>
2020-08-12 23:53:03 -0600

    Create org constraints separately from tables (#655)
    This approach is nicer because (1) we get to choose the name of the constraint, which gets referenced in application code, and (2) it's compatible with how Cloud Spanner requires constraints be implemented, and I have a dream of moving to Cloud Spanner.

--- limber-backend-application/src/main/resources/db/migration/V3__create_org_tables.sql
+++ limber-backend-application/src/main/resources/db/migration/V3__create_org_tables.sql
@@ -1,7 +1,10 @@
 CREATE TABLE orgs.org
 (
-    guid               UUID UNIQUE NOT NULL,
-    created_date       TIMESTAMP   NOT NULL,
-    name               VARCHAR     NOT NULL,
-    owner_account_guid UUID        NOT NULL
+    guid               UUID      NOT NULL,
+    created_date       TIMESTAMP NOT NULL,
+    name               VARCHAR   NOT NULL,
+    owner_account_guid UUID      NOT NULL
 );
+
+CREATE UNIQUE INDEX uniq__org__guid
+    ON orgs.org (guid);
