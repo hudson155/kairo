4a2b4fc1432985e134dd9f94bfbc4bdd3fb0627d
Jeff Hudson <jeff.hudson@toasttab.com>
2020-09-14 16:23:31 -0600

    Remove unused function (#762)

--- limber-backend/common/util/src/commonMain/kotlin/io/limberapp/common/util/Url.kt
+++ limber-backend/common/util/src/commonMain/kotlin/io/limberapp/common/util/Url.kt
@@ -7,7 +7,6 @@ import kotlin.jvm.JvmName
 /**
  * Encodes a URL component.
  */
-@Suppress("UnusedPrivateMember") // False positive - Detekt bug?
 expect fun enc(value: Any): String
 
 /**
@@ -18,16 +17,9 @@ fun String.slugify() = enc(toLowerCase())
 /**
  * Creates an href. The [path] and [queryParams] must be already encoded.
  */
-@Suppress("UnusedPrivateMember") // False positive - Detekt bug?
 fun href(path: String, queryParams: List<Pair<String, String>> = emptyList()): String {
   val queryString = queryParams.joinToString("&") { "${it.first}=${it.second}" }
   var href = path
   if (queryString.isNotEmpty()) href += "?$queryString"
   return href
 }
-
-/**
- * Replaces the last component of a path (where components are separated by slashes) with the given value.
- */
-fun String.replaceLastPathComponentWith(value: String) =
-  "${split('/').dropLastWhile { it.isEmpty() }.dropLast(1).joinToString("/")}/$value"
