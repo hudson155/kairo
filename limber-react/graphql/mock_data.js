94e832587507d6986b529919c48bf1e44a3dfacd
Noah Guld <nguld12@gmail.com>
2020-09-15 22:27:41 -0700

    Add mock graphql data (#773)

---
+++ limber-react/graphql/mock_data.js
@@ -0,0 +1,38 @@
+const formInstances = [
+    {
+      id: '87a2641f-6dd8-4cb3-a8cb-449a1e95d77d',
+      createdDate: '1/2/3456',
+      number: 1,
+      submittedDate: '1/2/3456',
+    },
+    {
+      id: '494b53fc-4874-43a6-a502-835c52bd9cfe',
+      createdDate: '7/8/9012',
+      number: 2,
+      submittedDate: '7/8/9012',
+    },
+  ];
+
+const formTemplates = [
+  {
+    id: 'b1959d44-b39d-453d-aa27-fb4394c1d550',
+    createdDate: '6/9/1969',
+    title: 'Super serious form',
+    description: 'Not one joke is in this form',
+  },
+];
+
+const user = {
+  id: 'b9e46df7-fdb7-4437-87d3-8f48ca47804c',
+  firstName: 'Noah',
+  lastName: 'Guld',
+  fullName: 'Noah Guld',
+  emailAddress: 'noah.guld@limberapp.io',
+  profilePhotoUrl: 'https://avatars0.githubusercontent.com/u/8917186?s=460&u=364b0d5270cb9657b4222c0816713831805957c9&v=4',
+}
+
+module.exports = {
+  formInstances,
+  formTemplates,
+  user
+};
--- limber-react/graphql/server.js
+++ limber-react/graphql/server.js
@@ -1,18 +1,40 @@
+var { formTemplates, formInstances, user } = require('./mock_data');
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 var cors = require('cors');
+var fs = require('fs');
+
+// Construct a schema, using GraphQL schema language
+var schema = fs.readFileSync('./graphql/schema.graphql').toString('utf-8');
+
+var root = {
+  node: (input) => {
+    const formTemplate = formTemplates.find(formTemplate => formTemplate.id === input.id);
+    const formInstanceEdges = formInstances.map(formInstance => ({
+      cursor: formInstance.guid,
+      // Note this isn't strictly correct since the formTemplate here won't have the instances set on it. In the real
+      // application that's okay because you wouldn't write an infinitely recursive query so it can be filled.
+      node: { ...formInstance, formTemplate: formTemplate, creator: user },
+    }));
+    const formInstancesConnection = {
+      edges: formInstanceEdges,
+      // ignore the pageInfo for now since we won't paginate the mock data
+    };
+
+    formTemplate.formInstances = formInstancesConnection;
+    formTemplate.__typename = "FormTemplate";
+
+    return formTemplate;
+  },
+};
 
 var app = express();
 app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
-  schema: buildSchema(`
-    type Hello {
-      world: String!
-    }
-  `),
-  rootValue: {},
+  schema: buildSchema(schema),
+  rootValue: root,
   graphiql: true,
 }));
 
