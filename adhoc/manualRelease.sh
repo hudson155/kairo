fe6e669950882b8643702d5d799f0707807f965f
Jeff Hudson <jeff.hudson@toasttab.com>
2020-09-20 13:32:38 -0600

    Create production backend for GraphQL server (#800)

--- adhoc/manualRelease.sh
+++ adhoc/manualRelease.sh
@@ -6,13 +6,22 @@ set -e
 
 ./gradlew clean
 ENV=production ./gradlew build
+
+docker build -t registry.digitalocean.com/limber/limber-graphql-server:master limber-backend/graphql-server
+docker tag registry.digitalocean.com/limber/limber-graphql-server:master "registry.digitalocean.com/limber/limber-graphql-server:$(git rev-parse HEAD)"
+docker push registry.digitalocean.com/limber/limber-graphql-server:master
+docker push "registry.digitalocean.com/limber/limber-graphql-server:$(git rev-parse HEAD)"
+
 docker build -t registry.digitalocean.com/limber/limber-monolith:master limber-backend/monolith
-docker tag registry.digitalocean.com/limber/limber-monolith:master registry.digitalocean.com/limber/limber-monolith:$(git rev-parse HEAD)
+docker tag registry.digitalocean.com/limber/limber-monolith:master "registry.digitalocean.com/limber/limber-monolith:$(git rev-parse HEAD)"
 docker push registry.digitalocean.com/limber/limber-monolith:master
-docker push registry.digitalocean.com/limber/limber-monolith:$(git rev-parse HEAD)
+docker push "registry.digitalocean.com/limber/limber-monolith:$(git rev-parse HEAD)"
+
 docker build -t registry.digitalocean.com/limber/limber-web:master limber-web
-docker tag registry.digitalocean.com/limber/limber-web:master registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)
+docker tag registry.digitalocean.com/limber/limber-web:master "registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)"
 docker push registry.digitalocean.com/limber/limber-web:master
-docker push registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)
-kubectl set image deployment/limber-monolith limber-monolith=registry.digitalocean.com/limber/limber-monolith:$(git rev-parse HEAD) --namespace limber
-kubectl set image deployment/limber-web limber-web=registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD) --namespace limber
+docker push "registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)"
+
+kubectl set image deployment/limber-graphql-server limber-graphql-server="registry.digitalocean.com/limber/limber-graphql-server:$(git rev-parse HEAD)" --namespace limber
+kubectl set image deployment/limber-monolith limber-monolith="registry.digitalocean.com/limber/limber-monolith:$(git rev-parse HEAD)" --namespace limber
+kubectl set image deployment/limber-web limber-web="registry.digitalocean.com/limber/limber-web:$(git rev-parse HEAD)" --namespace limber
