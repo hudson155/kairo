44782269cf355e6b64daeb48f2bb0c749230c657
Jeff Hudson <jeff.hudson@toasttab.com>
2020-09-24 23:39:54 -0600

    Use implementation-agnostic classes for query builder (#824)
    This helps us in 2 ways. Firstly, it allows evolution of the implementation details of QueryBuilder's Conditions, Bindings, and Sorts in a backwards-compatible way. Secondly and more acutely, it enables explicit type binding using JDBI's `bindByType()` function, which allows binding lists without using JDBI definitions.

--- limber-backend/common/sql/src/main/kotlin/io/limberapp/common/store/QueryBuilder.kt
+++ limber-backend/common/sql/src/main/kotlin/io/limberapp/common/store/QueryBuilder.kt
@@ -1,25 +1,69 @@
 package io.limberapp.common.store
 
+import io.limberapp.common.store.QueryBuilder.Bindings.Companion.bind
 import org.jdbi.v3.core.statement.SqlStatement
+import kotlin.reflect.KType
+import kotlin.reflect.javaType
+import kotlin.reflect.typeOf
 
 @Suppress("UnnecessaryAbstractClass")
 abstract class QueryBuilder {
-  val conditions = mutableListOf<String>()
-  val bindings = mutableMapOf<String, Any>()
-  val sorts = mutableListOf<Pair<String, Boolean>>() // Column name and ascending flag.
-}
+  class Conditions {
+    private val conditions = mutableListOf<String>()
 
-fun <S : SqlStatement<S>> S.withFinder(query: QueryBuilder): S {
-  val conditions = query.conditions.joinToString(" AND ")
-  val sorts = query.sorts.let {
-    return@let if (it.isNotEmpty()) {
-      it.joinToString(", ") { "${it.first} ${if (it.second) "ASC" else "DESC"}" }
-    } else {
-      "1" // If there are no sorts, using "1" means the rows don't get sorted.
+    operator fun plusAssign(condition: String) {
+      conditions += condition
+    }
+
+    override fun toString() = conditions.joinToString(" AND ")
+  }
+
+  @OptIn(ExperimentalStdlibApi::class)
+  class Bindings {
+    private val bindings = mutableMapOf<String, Pair<Any, KType>>()
+
+    inline operator fun <reified T : Any> set(name: String, value: T) {
+      this[name] = value to typeOf<T>()
+    }
+
+    operator fun set(name: String, value: Pair<Any, KType>) {
+      bindings[name] = value
+    }
+
+    companion object {
+      fun <This : SqlStatement<This>> SqlStatement<This>.bind(bindings: Bindings): This {
+        bindings.bindings.forEach { (key, value) ->
+          bindByType(key, value.first, value.second.javaType)
+        }
+        return this as This
+      }
     }
   }
-  return this
-    .define("conditions", conditions)
-    .define("sort", sorts)
-    .bindMap(query.bindings)
+
+  class Sorts {
+    private val sorts = mutableListOf<Pair<String, Boolean>>() // Column name and ascending flag.
+
+    operator fun plusAssign(sort: Pair<String, Boolean>) {
+      sorts += sort
+    }
+
+    override fun toString() = sorts.let {
+      return@let if (it.isNotEmpty()) {
+        it.joinToString(", ") { "${it.first} ${if (it.second) "ASC" else "DESC"}" }
+      } else {
+        "1" // If there are no sorts, using "1" means the rows don't get sorted.
+      }
+    }
+  }
+
+  val conditions = Conditions()
+
+  val bindings = Bindings()
+
+  val sorts = Sorts()
 }
+
+fun <S : SqlStatement<S>> S.withFinder(query: QueryBuilder): S = this
+  .define("conditions", query.conditions.toString())
+  .define("sort", query.sorts.toString())
+  .bind(query.bindings)
