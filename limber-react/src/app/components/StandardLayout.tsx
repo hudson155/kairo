31a254def7cf39cb1998c7c100be769634e427ec
Noah Guld <nguld12@gmail.com>
2020-10-05 19:50:10 -0700

    Incomplete standard Layout (#877)

---
+++ limber-react/src/app/components/StandardLayout.tsx
@@ -0,0 +1,28 @@
+import { CSSObject } from '@emotion/core';
+import React, { ReactElement, ReactNode, Suspense } from 'react';
+
+import { EmotionThemeType } from '../EmotionTheme';
+
+// TODO (ENG-81): Properly implement and style this component to support navigation component etc.
+const styles = {
+  root: (theme: EmotionThemeType): CSSObject => ({
+    marginLeft: theme.size.$64,
+    marginRight: theme.size.$64,
+  }),
+};
+
+interface Props {
+  readonly children: ReactNode;
+}
+
+function StandardLayout(props: Props): ReactElement {
+  return (
+    <div css={theme => styles.root(theme)}>
+      <Suspense fallback={<p>Loading</p>}>
+        {props.children}
+      </Suspense>
+    </div>
+  );
+}
+
+export default StandardLayout;
