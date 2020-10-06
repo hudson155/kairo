191d8154929197605ce047855f3a23e117e285a4
Jeff Hudson <jeff.hudson@toasttab.com>
2020-10-05 21:52:52 -0600

    Create user module clients (#880)

---
+++ limber-backend/module/users/client/src/main/kotlin/io/limberapp/backend/module/users/client/account/UserClient.kt
@@ -0,0 +1,50 @@
+package io.limberapp.backend.module.users.client.account
+
+import io.limberapp.backend.module.users.api.user.UserApi
+import io.limberapp.backend.module.users.rep.account.UserRep
+import io.limberapp.client.LimberHttpClient
+import io.limberapp.client.LimberHttpClientRequestBuilder
+
+class UserClient(private val httpClient: LimberHttpClient) {
+  suspend operator fun invoke(
+    endpoint: UserApi.Post,
+    builder: LimberHttpClientRequestBuilder.() -> Unit = {},
+  ) = httpClient.request(endpoint, builder) {
+    parse<UserRep.Complete>(checkNotNull(it))
+  }
+
+  suspend operator fun invoke(
+    endpoint: UserApi.Get,
+    builder: LimberHttpClientRequestBuilder.() -> Unit = {},
+  ) = httpClient.request(endpoint, builder) {
+    it?.let { parse<UserRep.Complete>(it) }
+  }
+
+  suspend operator fun invoke(
+    endpoint: UserApi.GetByOrgGuidAndEmailAddress,
+    builder: LimberHttpClientRequestBuilder.() -> Unit = {},
+  ) = httpClient.request(endpoint, builder) {
+    it?.let { parse<UserRep.Complete>(it) }
+  }
+
+  suspend operator fun invoke(
+    endpoint: UserApi.GetByOrgGuid,
+    builder: LimberHttpClientRequestBuilder.() -> Unit = {},
+  ) = httpClient.request(endpoint, builder) {
+    parse<Set<UserRep.Summary>>(checkNotNull(it))
+  }
+
+  suspend operator fun invoke(
+    endpoint: UserApi.Patch,
+    builder: LimberHttpClientRequestBuilder.() -> Unit = {},
+  ) = httpClient.request(endpoint, builder) {
+    it?.let { parse<UserRep.Complete>(it) }
+  }
+
+  suspend operator fun invoke(
+    endpoint: UserApi.Delete,
+    builder: LimberHttpClientRequestBuilder.() -> Unit = {},
+  ) = httpClient.request(endpoint, builder) {
+    it?.let { parse<Unit>(it) }
+  }
+}
---
+++ limber-backend/module/users/client/src/main/kotlin/io/limberapp/backend/module/users/client/account/UserRoleClient.kt
@@ -0,0 +1,21 @@
+package io.limberapp.backend.module.users.client.account
+
+import io.limberapp.backend.module.users.api.user.role.UserRoleApi
+import io.limberapp.client.LimberHttpClient
+import io.limberapp.client.LimberHttpClientRequestBuilder
+
+class UserRoleClient(private val httpClient: LimberHttpClient) {
+  suspend operator fun invoke(
+    endpoint: UserRoleApi.Put,
+    builder: LimberHttpClientRequestBuilder.() -> Unit = {},
+  ) = httpClient.request(endpoint, builder) {
+    parse<Unit>(checkNotNull(it))
+  }
+
+  suspend operator fun invoke(
+    endpoint: UserRoleApi.Delete,
+    builder: LimberHttpClientRequestBuilder.() -> Unit = {},
+  ) = httpClient.request(endpoint, builder) {
+    it?.let { parse<Unit>(it) }
+  }
+}
