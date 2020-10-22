8ef53576b8f11914541eded144924a91c241cea9
Jeff Hudson <jeff.hudson@toasttab.com>
2020-10-21 21:21:24 -0600

    Fix limber-web JWT (#988)
    When the JWT claims were changed to no longer be stringified, the front end code was changed to support this, but the change was not correct.

--- limber-web/src/main/kotlin/io/limberapp/web/auth/Jwt.kt
+++ limber-web/src/main/kotlin/io/limberapp/web/auth/Jwt.kt
@@ -3,12 +3,13 @@ package io.limberapp.web.auth
 import io.limberapp.backend.authorization.principal.Claims
 import io.limberapp.backend.authorization.principal.JwtOrg
 import io.limberapp.backend.authorization.principal.JwtUser
+import io.limberapp.web.api.json
 import io.limberapp.web.util.external.decodeJwt
 
 internal data class Jwt(val raw: String) {
   private val decoded = decodeJwt(raw)
 
-  val org: JwtOrg = decoded[Claims.org]
+  val org = json.parse<JwtOrg>(JSON.stringify(decoded[Claims.org]))
 
-  val user: JwtUser = decoded[Claims.user]
+  val user = json.parse<JwtUser>(JSON.stringify(decoded[Claims.user]))
 }
