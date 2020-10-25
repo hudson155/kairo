1ce15ef6aa8acda9435c8593c30f3d57afbca354
Jeff Hudson <jeff.hudson@toasttab.com>
2020-10-25 10:32:41 -0600

    Audit testing module (#991)

--- limber-backend/common/testing/unit/build.gradle.kts
+++ limber-backend/common/testing/unit/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
-  api(kotlin("test-junit5"))
-  api(Dependencies.JUnit.api)
-  runtimeOnly(Dependencies.JUnit.engine)
-  api(Dependencies.MockK.mockK)
+  api(kotlin("test-junit5")) // Not used by this module - provided to consumers.
+  api(Dependencies.JUnit.api) // Not used by this module - provided to consumers.
+  runtimeOnly(Dependencies.JUnit.engine) // Not used by this module - provided to consumers.
+  api(Dependencies.MockK.mockK) // Not used by this module - provided to consumers.
 }
