f069232c8340ca9ffe46f5e125e93204d45dab2b
Jeff Hudson <jeff.hudson@toasttab.com>
2021-01-18 09:12:30 -0700

    Re-organize settings.gradle (#1030)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "limber"
 
+// Common
 include(":limber-backend:common:config")
 include(":limber-backend:common:exceptions")
 include(":limber-backend:common:jwt")
@@ -7,13 +8,20 @@ include(":limber-backend:common:module")
 include(":limber-backend:common:reps")
 include(":limber-backend:common:rest-interface")
 include(":limber-backend:common:serialization")
-include(":limber-backend:common:type-conversion:implementation")
-include(":limber-backend:common:type-conversion:interface")
+include(
+    ":limber-backend:common:type-conversion:implementation",
+    ":limber-backend:common:type-conversion:interface")
 include(":limber-backend:common:util")
 include(":limber-backend:common:validation")
 
+// DB
 include(":limber-backend:db:limber")
 
+// Module
+
+// Server
+
+// Multiplatform
 include(":limber-multiplatform:darb")
 include(":limber-multiplatform:logging")
 include(":limber-multiplatform:permissions")
