8ca56b11e78df03ed29a4c10793a4adcad098a17
Jeff Hudson <jeff.hudson@toasttab.com>
2021-01-26 16:32:57 -0700

    Improve object mapper (#1037)
    The object mapper previously defaulted primitives to their default value (e.g. integers to 0). It now fails instead.

--- limber-backend/common/serialization/src/test/kotlin/io/limberapp/common/serialization/ObjectMapperTest.kt
+++ limber-backend/common/serialization/src/test/kotlin/io/limberapp/common/serialization/ObjectMapperTest.kt
@@ -1,24 +1,25 @@
 package io.limberapp.common.serialization
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.limberapp.common.typeConversion.typeConverter.UuidTypeConverter
 import java.util.UUID
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFails
+import kotlin.test.assertFailsWith
 
 internal class ObjectMapperTest {
   @Test
-  fun default() {
+  fun `default mapper - happy path`() {
     val objectMapper = LimberObjectMapper()
 
     assertEquals(
         expected = TestClass1("strVal", 42),
         actual = objectMapper.readValue("{\"str\":\"strVal\",\"int\":42}"),
     )
-    assertFails {
-      objectMapper.readValue("{\"str\":\"strVal\",\"int\":42,\"new\":true}")
-    }
     assertEquals(
         expected = "{\"str\":\"strVal\",\"int\":42}",
         actual = objectMapper.writeValueAsString(TestClass1("strVal", 42)),
@@ -26,16 +27,34 @@ internal class ObjectMapperTest {
   }
 
   @Test
-  fun yamlFactory() {
+  fun `default mapper - additional property`() {
+    val objectMapper = LimberObjectMapper()
+
+    assertFailsWith<UnrecognizedPropertyException> {
+      objectMapper.readValue<TestClass1>("{\"str\":\"strVal\",\"int\":42,\"new\":true}")
+    }
+  }
+
+  @Test
+  fun `default mapper - missing property`() {
+    val objectMapper = LimberObjectMapper()
+
+    assertFailsWith<MissingKotlinParameterException> {
+      objectMapper.readValue<TestClass1>("{\"int\":\"42\"}")
+    }
+    assertFailsWith<MismatchedInputException> {
+      objectMapper.readValue<TestClass1>("{\"str\":\"strVal\"}")
+    }
+  }
+
+  @Test
+  fun `mapper with YAML factory - happy path`() {
     val objectMapper = LimberObjectMapper(factory = LimberObjectMapper.Factory.YAML)
 
     assertEquals(
         expected = TestClass1("strVal", 42),
         actual = objectMapper.readValue("str: 'strVal'\nint: 42"),
     )
-    assertFails {
-      objectMapper.readValue("str: 'strVal'\nint: 42\nnew: true")
-    }
     assertEquals(
         expected = "---\nstr: \"strVal\"\nint: 42\n",
         actual = objectMapper.writeValueAsString(TestClass1("strVal", 42)),
@@ -43,16 +62,13 @@ internal class ObjectMapperTest {
   }
 
   @Test
-  fun typeConverter() {
+  fun `mapper with type converter - happy path`() {
     val objectMapper = LimberObjectMapper(typeConverters = listOf(UuidTypeConverter))
 
     assertEquals(
         expected = TestClass2(UUID.fromString("a0709f3f-bb5e-4dea-bcd0-c88664de3497"), 42),
         actual = objectMapper.readValue("{\"guid\":\"oHCfP7teTeq80MiGZN40lw==\",\"int\":42}"),
     )
-    assertFails {
-      objectMapper.readValue("{\"guid\":\"oHCfP7teTeq80MiGZN40lw==\",\"int\":42,\"new\":true}")
-    }
     assertEquals(
         expected = "{\"guid\":\"a0709f3f-bb5e-4dea-bcd0-c88664de3497\",\"int\":42}",
         actual = objectMapper.writeValueAsString(
@@ -62,7 +78,7 @@ internal class ObjectMapperTest {
   }
 
   @Test
-  fun allowUnknownProperties() {
+  fun `mapper that allows unknown properties - happy path`() {
     val objectMapper = LimberObjectMapper(allowUnknownProperties = true)
 
     assertEquals(
@@ -73,14 +89,10 @@ internal class ObjectMapperTest {
         expected = TestClass1("strVal", 42),
         actual = objectMapper.readValue("{\"str\":\"strVal\",\"int\":42,\"new\":true}"),
     )
-    assertEquals(
-        expected = "{\"str\":\"strVal\",\"int\":42}",
-        actual = objectMapper.writeValueAsString(TestClass1("strVal", 42)),
-    )
   }
 
   @Test
-  fun prettyPrint() {
+  fun `mapper with pretty print enabled - happy path`() {
     val objectMapper = LimberObjectMapper(prettyPrint = true)
 
     assertEquals(
