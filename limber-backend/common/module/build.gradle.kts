709bcd24928db0f280e5581b816441df86a320c3
Jeff Hudson <jeff.hudson@toasttab.com>
2021-01-29 18:40:45 -0700

    Allow type converters in modules (#1042)

--- limber-backend/common/module/build.gradle.kts
+++ limber-backend/common/module/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
   api(project(":limber-backend:common:exceptions"))
   api(project(":limber-backend:common:serialization"))
+  api(project(":limber-backend:common:type-conversion:interface"))
   api(Dependencies.Google.guice)
 }
--- limber-backend/common/module/src/main/kotlin/io/limberapp/common/module/Module.kt
+++ limber-backend/common/module/src/main/kotlin/io/limberapp/common/module/Module.kt
@@ -1,8 +1,11 @@
 package io.limberapp.common.module
 
 import com.google.inject.AbstractModule
+import io.limberapp.common.typeConversion.TypeConverter
 
 abstract class Module : AbstractModule() {
+  open val typeConverters: Set<TypeConverter<*>> = emptySet()
+
   override fun configure() {
     bind()
   }
