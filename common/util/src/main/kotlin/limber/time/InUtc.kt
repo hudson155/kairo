ca4e3b873135541d08f50c86056ad079eaeb49e5
Jeff Hudson <jhudson@jhudson.ca>
2022-08-23 15:54:07 -0600

    Create inUtc helper method (#1165)

---
+++ common/util/src/main/kotlin/limber/time/InUtc.kt
@@ -0,0 +1,9 @@
+package limber.time
+
+import java.time.ZoneId
+import java.time.ZonedDateTime
+
+private val utcZoneId = ZoneId.of("UTC")
+
+public fun ZonedDateTime.inUtc(): ZonedDateTime =
+  withZoneSameInstant(utcZoneId)
