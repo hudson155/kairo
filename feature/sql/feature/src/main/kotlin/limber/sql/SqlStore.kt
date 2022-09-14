73c8bd6fbc66ad02a95ab657da872529409aa2d1
Jeff Hudson <jhudson@jhudson.ca>
2022-09-14 11:21:08 -0600

    Create alias to read resources (#1225)

--- feature/sql/feature/src/main/kotlin/limber/sql/SqlStore.kt
+++ feature/sql/feature/src/main/kotlin/limber/sql/SqlStore.kt
@@ -1,9 +1,14 @@
 package limber.sql
 
+import com.google.common.io.Resources
 import org.jdbi.v3.core.Handle
 import org.jdbi.v3.core.Jdbi
 
-public abstract class SqlStore(protected val jdbi: Jdbi)
+public abstract class SqlStore(protected val jdbi: Jdbi) {
+  @Suppress("FunctionMinLength") // Short for the purpose of brevity due to frequent reuse.
+  protected fun rs(resourceName: String): String =
+    Resources.getResource(resourceName).readText()
+}
 
 public fun <R> Jdbi.transaction(callback: (Handle) -> R): R =
   inTransaction<R, Exception>(callback)
