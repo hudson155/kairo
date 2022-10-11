005b2f80ccf782cac81cb4b1879abe12ca5b3c13
Jeff Hudson <jhudson@jhudson.ca>
2022-10-11 15:37:18 -0600

    Add React Router (#1288)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import reportWebVitals from 'reportWebVitals';
+import RootRouter from 'routing/RootRouter';
 import 'the-new-css-reset/css/reset.css';
-import App from './App';
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <BrowserRouter>
+        <RootRouter />
+      </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>,
 );
---
+++ web/src/routing/RootRouter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Routes } from 'react-router-dom';
+
+const RootRouter: React.FC = () => {
+  return (
+    <Routes>
+    </Routes>
+  );
+};
+
+export default RootRouter;
