df6d3ff34ec17987493674133d10e326c60c71e5
Jeff Hudson <jhudson@jhudson.ca>
2022-10-27 12:43:02 -0600

    Add organization fixtures to local DB (#1351)

---
+++ db/limber/src/main/resources/db/limber/migration/fixtures/V7__insert_organization_fixtures.sql
@@ -0,0 +1,12 @@
+insert into organization.organization (guid, name)
+values (gen_random_uuid(), 'Acme Co.');
+
+insert into organization.organization_auth (organization_guid, guid, auth0_organization_id)
+values ((select guid from organization.organization), gen_random_uuid(), 'org_yDiVK18hoeddya8J');
+
+insert into organization.organization_hostname (organization_guid, guid, hostname)
+values ((select guid from organization.organization), gen_random_uuid(), 'localhost:3000');
+
+insert into organization.feature (organization_guid, guid, is_default, type, name, root_path)
+values ((select guid from organization.organization), gen_random_uuid(), true, 'Placeholder', 'Home', '/placeholder'),
+       ((select guid from organization.organization), gen_random_uuid(), false, 'Forms', 'My forms', '/forms');
