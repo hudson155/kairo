62bb0ca3c2d4fd02085905638a0141f17430c74d
Jeff Hudson <jhudson@jhudson.ca>
2022-10-26 22:20:22 -0600

    Update logging library documentation (#1339)

--- common/logging/build.gradle.kts
+++ common/logging/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 main {
   dependencies {
-    api(Dependencies.Logging.kotlinLogging)
+    api(Dependencies.Logging.kotlinLogging) // Make this available to library users.
     implementation(Dependencies.Logging.Log4j.core)
     implementation(Dependencies.Logging.Log4j.layoutTemplateJson)
     implementation(Dependencies.Logging.Log4j.slf4jImpl)
