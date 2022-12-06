47ad27063ad1af521138e0c44c2771ac4bbbb1b3
Jeff Hudson <jhudson@jhudson.ca>
2022-12-05 17:45:00 -0700

    Create production SQL database (#1496)

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/google"
       version = "4.44.1"
     }
+    postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = "1.18.0"
+    }
   }
   backend "gcs" {
     bucket = "limberapp-terraform"
@@ -12,4 +16,11 @@ terraform {
 
 provider "google" {
   project = "limberapp-io"
+}
+
+provider "postgresql" {
+  host = google_sql_database_instance.limber.public_ip_address
+  database = google_sql_database.limber_limber.name
+  username = google_sql_user.limber_app_engine.name
+  password = data.google_secret_manager_secret_version.sql_limber_app_engine_password.secret_data
 }
