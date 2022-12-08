f9650bd4ac0b55a225f5dc96e4cc7e79801422b0
Jeff Hudson <jhudson@jhudson.ca>
2022-12-07 21:18:41 -0700

    Refactor spacing throughout the app (#1511)

--- web/src/component/paper/Paper.stories.tsx
+++ web/src/component/paper/Paper.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, Story } from '@storybook/react';
 import Heading2 from 'component/text/Heading2';
+import Paragraph from 'component/text/Paragraph';
 import { ComponentProps } from 'react';
 import Paper from './Paper';
 
@@ -8,7 +9,8 @@ export default {} as ComponentMeta<typeof Paper>;
 const Template: Story<ComponentProps<typeof Paper>> = () => {
   return (
     <Paper>
-      <Heading2>{'Some heading'}</Heading2>
+      <Heading2>Some heading</Heading2>
+      <Paragraph>Some content</Paragraph>
     </Paper>
   );
 };
