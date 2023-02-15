890716ac644682930728c317348e22a7491edbb2
Jeff Hudson <jhudson@jhudson.ca>
2023-02-15 15:41:28 -0500

    Re-organize SQL queries (#1568)

--- feature/organization/feature/src/main/resources/store/feature/create.sql
+++ feature/organization/feature/src/main/resources/store/feature/create.sql
@@ -1,3 +1,3 @@
-insert into organization.feature (organization_guid, guid, is_default, type, name, icon_name, root_path)
-values (:organizationGuid, :guid, :isDefault, :type, :name, :iconName, lower(:rootPath))
+insert into organization.feature (guid, organization_guid, is_default, type, name, icon_name, root_path)
+values (:guid, :organizationGuid, :isDefault, :type, :name, :iconName, lower(:rootPath))
 returning *
