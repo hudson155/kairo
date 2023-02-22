6f49e7d5490f0599e33cd1fd33f306383dfa2f33
Jeff Hudson <jhudson@jhudson.ca>
2023-02-22 12:29:22 -0500

    Rewrite profile photo component (#1601)

--- web/src/component/profilePhoto/ProfilePhoto.stories.tsx
+++ web/src/component/profilePhoto/ProfilePhoto.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, Story } from '@storybook/react';
+import Container from 'component/container/Container';
 import { ComponentProps } from 'react';
 import ProfilePhoto from './ProfilePhotoDelegate';
 
@@ -6,10 +7,10 @@ export default {} as ComponentMeta<typeof ProfilePhoto>;
 
 const Template: Story<ComponentProps<typeof ProfilePhoto>> = () => {
   return (
-    <>
+    <Container direction="horizontal">
       <ProfilePhoto url={undefined} />
       <ProfilePhoto url="https://avatars.githubusercontent.com/u/1360420" />
-    </>
+    </Container>
   );
 };
 
