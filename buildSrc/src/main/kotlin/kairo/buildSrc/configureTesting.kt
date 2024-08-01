c02f747d908071567ba1d03a7bf1e6e0b54996b6
Jeff Hudson <jhudson@jhudson.ca>
2024-08-01 18:47:31 -0400

    Testing updates

--- buildSrc/src/main/kotlin/kairo/buildSrc/configureTesting.kt
+++ buildSrc/src/main/kotlin/kairo/buildSrc/configureTesting.kt
@@ -3,15 +3,15 @@ package kairo.buildSrc
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.AbstractTestTask
 import org.gradle.api.tasks.testing.Test
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
+import org.gradle.testing.base.TestingExtension
 
 internal fun Project.configureTesting() {
-  tasks.withType<AbstractTestTask> {
+  tasks.withType<Test> {
     testLogging {
       events("passed", "skipped", "failed")
     }
-  }
-  tasks.withType<Test> {
     useJUnitPlatform()
     ignoreFailures = project.hasProperty("ignoreTestFailures") // This property may be set during CI.
   }
