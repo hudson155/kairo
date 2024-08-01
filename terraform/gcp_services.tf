98cddcde35292cdad22f3a0bf563e5ece2aabe47
Jeff Hudson <jhudson@jhudson.ca>
2024-08-01 16:14:06 -0600

    Initialize GCP project with Terraform (#4)

---
+++ terraform/gcp_services.tf
@@ -0,0 +1,26 @@
+resource "google_project_service" "gcp_services" {
+  for_each = toset([
+    "analyticshub.googleapis.com",
+    "bigquery.googleapis.com",
+    "bigqueryconnection.googleapis.com",
+    "bigquerydatapolicy.googleapis.com",
+    "bigquerymigration.googleapis.com",
+    "bigqueryreservation.googleapis.com",
+    "bigquerystorage.googleapis.com",
+    "cloudapis.googleapis.com",
+    "cloudtrace.googleapis.com",
+    "dataform.googleapis.com",
+    "dataplex.googleapis.com",
+    "datastore.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "servicemanagement.googleapis.com",
+    "serviceusage.googleapis.com",
+    "sql-component.googleapis.com",
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+    "storage.googleapis.com",
+  ])
+  service = each.key
+  disable_on_destroy = true
+}
