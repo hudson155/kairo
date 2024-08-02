58a771566da68999e759750cd12800080c5aefba
Jeff Hudson <jhudson@jhudson.ca>
2024-08-02 11:49:27 -0400

    Remove unused serialization dependency

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,6 @@ repositories {
 dependencies {
   // https://kotlinlang.org/docs/releases.html#release-details
   api(kotlin("gradle-plugin", "2.0.0"))
-  api(kotlin("serialization"))
 
   // https://plugins.gradle.org/plugin/com.google.cloud.artifactregistry.gradle-plugin
   api("gradle.plugin.com.google.cloud.artifactregistry:artifactregistry-gradle-plugin:2.2.2")
