fb3f4e0ca0e883cf93086e086ba2ec6dca252e08
Jeff Hudson <jhudson@jhudson.ca>
2024-08-02 09:55:16 -0400

    Alphabetize

--- kairo-logging/build.gradle.kts
+++ kairo-logging/build.gradle.kts
@@ -10,8 +10,8 @@ main {
   dependencies {
     api(Dependencies.kotlinLoggingJvm)
     implementation(Dependencies.kotlinxCoroutinesSlf4j)
+    implementation(Dependencies.log4jConfigYaml)
     implementation(Dependencies.log4jCore)
     implementation(Dependencies.log4jSlf4j2Impl)
-    implementation(Dependencies.log4jConfigYaml)
   }
 }
