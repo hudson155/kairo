8d83b05e20e54c3b1c2526da114e86154c429b19
Jeff Hudson <jhudson@jhudson.ca>
2024-08-02 18:19:15 -0600

    Release 0.2.0 (#19)

--- buildSrc/src/main/kotlin/kairo-publish.gradle.kts
+++ buildSrc/src/main/kotlin/kairo-publish.gradle.kts
@@ -14,7 +14,7 @@ publishing {
       create<MavenPublication>("maven") {
         groupId = "kairo"
         artifactId = project.name
-        version = "0.1.0"
+        version = "0.2.0"
         from(components["java"])
       }
     }
