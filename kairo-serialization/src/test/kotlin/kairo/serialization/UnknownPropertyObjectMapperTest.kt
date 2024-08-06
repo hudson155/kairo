163812b4d1ff691d567af9fb81c379e36715fa25
Jeff Hudson <jhudson@jhudson.ca>
2024-08-05 20:44:19 -0600

    Add serialization test for unknown property handling (#27)

---
+++ kairo-serialization/src/test/kotlin/kairo/serialization/UnknownPropertyObjectMapperTest.kt
@@ -0,0 +1,30 @@
+package kairo.serialization
+
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+/**
+ * This test is intended to test behaviour strictly related to unknown property deserialization.
+ * Therefore, some test cases are not included since they are not strictly related to unknown properties.
+ */
+internal class UnknownPropertyObjectMapperTest : FunSpec({
+  test("disallowed (default)") {
+    val mapper = ObjectMapperFactory.builder(ObjectMapperFormat.Json).build()
+    shouldThrow<UnrecognizedPropertyException> {
+      mapper.readValue<MyClass>("{\"foo\": \"bar\", \"baz\": \"qux\"}")
+    }
+  }
+  test("allowed") {
+    val mapper = ObjectMapperFactory.builder(ObjectMapperFormat.Json) {
+      allowUnknownProperties = true
+    }.build()
+    mapper.readValue<MyClass>("{\"foo\": \"bar\", \"baz\": \"qux\"}").shouldBe(MyClass("bar"))
+  }
+}) {
+  private data class MyClass(
+    val foo: String,
+  )
+}
