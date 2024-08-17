2a1ebea80634007e2069f4427a35e4370d5f042b
Jeff Hudson <jhudson@jhudson.ca>
2024-08-17 09:28:11 -0600

    Fix paren

--- kairo-serialization/src/test/kotlin/kairo/serialization/BooleanIsObjectMapperTest.kt
+++ kairo-serialization/src/test/kotlin/kairo/serialization/BooleanIsObjectMapperTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test
  * since they are not strictly related to booleans.
  *
  * Jackson has some default behaviours with is-prefixed properties, where it serializes [isValue] to "value"
- * (dropping the "is" prefix and handling case corrections.
+ * (dropping the "is" prefix and handling case corrections).
  * The main purpose of this test is to ensure that behaviour is disabled.
  */
 internal class BooleanIsObjectMapperTest {
