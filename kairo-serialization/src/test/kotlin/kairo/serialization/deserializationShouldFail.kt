e9841c88c58b75591ff822b040b5afa168006380
Jeff Hudson <jhudson@jhudson.ca>
2024-08-17 09:57:09 -0600

    Add comment

--- kairo-serialization/src/test/kotlin/kairo/serialization/deserializationShouldFail.kt
+++ kairo-serialization/src/test/kotlin/kairo/serialization/deserializationShouldFail.kt
@@ -3,5 +3,9 @@ package kairo.serialization
 import com.fasterxml.jackson.databind.JsonMappingException
 import io.kotest.assertions.throwables.shouldThrow
 
+/**
+ * For simplicity, we only check for [JsonMappingException] to ensure deserialization failed.
+ * It's possible but unnecessary to check for more specific exception types.
+ */
 internal inline fun deserializationShouldFail(block: () -> Any?): JsonMappingException =
   shouldThrow<JsonMappingException>(block)
