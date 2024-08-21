6f9cf6654154db42c7ed6b867eaa73554b1ef063
Jeff Hudson <jhudson@jhudson.ca>
2024-08-20 22:52:37 -0600

    Update comment

--- kairo-serialization/src/main/kotlin/kairo/serialization/ZoneIdDeserializer.kt
+++ kairo-serialization/src/main/kotlin/kairo/serialization/ZoneIdDeserializer.kt
@@ -10,6 +10,9 @@ import java.time.ZoneOffset
 /**
  * We don't use com.fasterxml.jackson.datatype:jackson-datatype-jsr310,
  * and instead roll our own time module. See [TimeModule].
+ *
+ * This serializer intentionally only supports [ZoneRegion] and [ZoneOffset.UTC].
+ * Non-UTC [ZoneOffset]s are not supported.
  */
 internal class ZoneIdDeserializer : StdDeserializer<ZoneId>(ZoneId::class.java) {
   override fun deserialize(p: JsonParser, ctxt: DeserializationContext): ZoneId {
