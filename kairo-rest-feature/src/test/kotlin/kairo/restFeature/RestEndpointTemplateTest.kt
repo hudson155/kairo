60a2e679e9daed8c966ff8b20544cf09682a120d
Jeff Hudson <jhudson@jhudson.ca>
2024-08-23 13:32:47 -0600

    Adjust REST endpoint template toString() method

--- kairo-rest-feature/src/test/kotlin/kairo/restFeature/RestEndpointTemplateTest.kt
+++ kairo-rest-feature/src/test/kotlin/kairo/restFeature/RestEndpointTemplateTest.kt
@@ -12,42 +12,42 @@ internal class RestEndpointTemplateTest {
   @Test
   fun `get, toString method`() {
     RestEndpointTemplate.parse(TypicalLibraryBookApi.Get::class).toString()
-      .shouldBe("[ -> application/json] GET library/library-books/:libraryBookId")
+      .shouldBe("RestEndpointTemplate([ -> application/json] GET library/library-books/:libraryBookId)")
   }
 
   @Test
   fun `listAll, toString method`() {
     RestEndpointTemplate.parse(TypicalLibraryBookApi.ListAll::class).toString()
-      .shouldBe("[ -> application/json] GET library/library-books")
+      .shouldBe("RestEndpointTemplate([ -> application/json] GET library/library-books)")
   }
 
   @Test
   fun `searchByIsbn, toString method`() {
     RestEndpointTemplate.parse(TypicalLibraryBookApi.SearchByIsbn::class).toString()
-      .shouldBe("[ -> application/json] GET library/library-books (isbn)")
+      .shouldBe("RestEndpointTemplate([ -> application/json] GET library/library-books (isbn))")
   }
 
   @Test
   fun `searchByTitle, toString method`() {
     RestEndpointTemplate.parse(TypicalLibraryBookApi.SearchByText::class).toString()
-      .shouldBe("[ -> application/json] GET library/library-books (title?, author?)")
+      .shouldBe("RestEndpointTemplate([ -> application/json] GET library/library-books (title?, author?))")
   }
 
   @Test
   fun `create, toString method`() {
     RestEndpointTemplate.parse(TypicalLibraryBookApi.Create::class).toString()
-      .shouldBe("[application/json -> application/json] POST library/library-books")
+      .shouldBe("RestEndpointTemplate([application/json -> application/json] POST library/library-books)")
   }
 
   @Test
   fun `update, toString method`() {
     RestEndpointTemplate.parse(TypicalLibraryBookApi.Update::class).toString()
-      .shouldBe("[application/json -> application/json] PATCH library/library-books/:libraryBookId")
+      .shouldBe("RestEndpointTemplate([application/json -> application/json] PATCH library/library-books/:libraryBookId)")
   }
 
   @Test
   fun `delete, toString method`() {
     RestEndpointTemplate.parse(TypicalLibraryBookApi.Delete::class).toString()
-      .shouldBe("[ -> application/json] DELETE library/library-books/:libraryBookId")
+      .shouldBe("RestEndpointTemplate([ -> application/json] DELETE library/library-books/:libraryBookId)")
   }
 }
