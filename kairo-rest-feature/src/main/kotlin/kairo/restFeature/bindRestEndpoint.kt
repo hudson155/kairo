04fda9da69867ced540a5b03f3cce569cad94663
Jeff Hudson <jhudson@jhudson.ca>
2024-08-24 16:12:36 -0600

    Create bindRestEndpoint helper (#94)

---
+++ kairo-rest-feature/src/main/kotlin/kairo/restFeature/bindRestEndpoint.kt
@@ -0,0 +1,15 @@
+package kairo.restFeature
+
+import com.google.inject.PrivateBinder
+import com.google.inject.multibindings.Multibinder
+import kairo.dependencyInjection.type
+
+/**
+ * Binding REST endpoints uses a [Multibinder],
+ * which allows multiple instances to be bound separately but injected together as a set.
+ */
+public inline fun <reified Handler : RestHandler<*>> PrivateBinder.bindRestEndpoint() {
+  val multibinder = Multibinder.newSetBinder(this, type<RestHandler<*>>())
+  multibinder.addBinding().to(Handler::class.java)
+  expose(type<Set<RestHandler<*>>>())
+}
