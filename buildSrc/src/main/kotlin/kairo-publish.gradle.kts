91c74ca9778525946b86e6998e394e42eb3aa459
Jeff Hudson <jhudson@jhudson.ca>
2024-08-24 18:56:34 -0600

    Configure Gradle to publish SNAPSHOT versions locally

--- buildSrc/src/main/kotlin/kairo-publish.gradle.kts
+++ buildSrc/src/main/kotlin/kairo-publish.gradle.kts
@@ -10,13 +10,19 @@ publishing {
         url = uri("artifactregistry://us-central1-maven.pkg.dev/kairo-13/kairo-13")
       }
     }
-    publications {
-      create<MavenPublication>("maven") {
-        groupId = "kairo"
-        artifactId = project.name
-        version = "0.5.0"
-        from(components["java"])
-      }
+    create<MavenPublication>("maven") {
+      groupId = "kairo"
+      artifactId = project.name
+      version = "0.5.0"
+      from(components["java"])
+    }
+  }
+}
+
+gradle.taskGraph.whenReady {
+  if (allTasks.any { it.name == "publishToMavenLocal" }) {
+    publishing.publications.getByName<MavenPublication>("maven") {
+      version += "-SNAPSHOT"
     }
   }
 }
