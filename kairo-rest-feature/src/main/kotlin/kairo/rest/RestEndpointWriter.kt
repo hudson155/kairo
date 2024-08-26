c915d9527f61dc954c34a6eafc665aa3ec2e549f
Jeff Hudson <jhudson@jhudson.ca>
2024-08-26 11:57:55 -0600

    Change REST Feature package

---
+++ kairo-rest-feature/src/main/kotlin/kairo/rest/RestEndpointWriter.kt
@@ -0,0 +1,9 @@
+package kairo.rest
+
+/**
+ * There are varying use cases for converting [RestEndpointTemplate] instances to strings.
+ * Implementations of this class achieve those varying use cases.
+ */
+internal abstract class RestEndpointWriter {
+  abstract fun write(template: RestEndpointTemplate): String
+}
