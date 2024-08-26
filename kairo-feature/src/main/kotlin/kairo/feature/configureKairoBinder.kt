b212c5c24656b7bcd4327cdec210bf207d57e137
Jeff Hudson <jhudson@jhudson.ca>
2024-08-26 17:38:30 -0600

    Create Guice provider for REST server (#106)

--- kairo-feature/src/main/kotlin/kairo/feature/configureKairoBinder.kt
+++ kairo-feature/src/main/kotlin/kairo/feature/configureKairoBinder.kt
@@ -3,6 +3,7 @@ package kairo.feature
 import com.google.inject.Binder
 
 public fun Binder.configureKairoBinder() {
+  disableCircularProxies()
   requireAtInjectOnConstructors()
   requireExactBindingAnnotations()
 }
