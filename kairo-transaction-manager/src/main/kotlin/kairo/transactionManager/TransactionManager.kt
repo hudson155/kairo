9d61624f9c813ac729a7f8fd830d1856f029e8ae
Jeff Hudson <jhudson@jhudson.ca>
2024-08-26 18:30:09 -0600

    Alter KDoc

--- kairo-transaction-manager/src/main/kotlin/kairo/transactionManager/TransactionManager.kt
+++ kairo-transaction-manager/src/main/kotlin/kairo/transactionManager/TransactionManager.kt
@@ -17,7 +17,6 @@ import kotlin.reflect.KClass
  *   - Transactionality: If the operation completes successfully, the transaction will be committed.
  *     If the operation fails (throws an exception), the transaction will be rolled back.
  *     There is no way to roll the transaction back without throwing an exception.
- *
  *   - Error handling: Errors during the transaction "begin", "commit", and "rollback" phases
  *     should be avoided by the underlying implementations.
  *     Of course, it's not possible to guarantee a perfect success rate.
@@ -31,7 +30,6 @@ import kotlin.reflect.KClass
  *         outer transactions will continue to be rolled back.
  *         Inner transactions were already rolled back.
  *         Data corruption is possible.
- *
  *   - Nested transactions: Nested transactions of the same type reuse the outer transaction.
  *     In the case of nested transactions of the same type,
  *     it's as if the inner transaction didn't even exist, and was a direct operation call instead.
