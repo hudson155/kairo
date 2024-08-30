71008d5cedc8114934d6a15db603f23781bbd808
Jeff Hudson <jhudson@jhudson.ca>
2024-08-30 12:17:13 -0600

    Nothing

--- kairo-rest-feature/src/main/kotlin/kairo/rest/typeInfo.kt
+++ kairo-rest-feature/src/main/kotlin/kairo/rest/typeInfo.kt
@@ -2,8 +2,13 @@ package kairo.rest
 
 import io.ktor.util.reflect.TypeInfo
 import io.ktor.util.reflect.platformType
+import io.ktor.util.reflect.typeInfoImpl
 import kotlin.reflect.KType
 import kotlin.reflect.jvm.jvmErasure
 
 internal fun typeInfo(type: KType): TypeInfo =
-  TypeInfo(type.jvmErasure, type.platformType, type)
+  typeInfoImpl(
+    reifiedType = type.platformType,
+    kClass = type.jvmErasure,
+    kType = type,
+  )
