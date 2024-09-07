b51425ba6ab7d33a347414406fff1c764840069f
Jeff Hudson <jhudson@jhudson.ca>
2024-09-07 10:29:38 -0600

    Resolve remaining gaps in SQL Feature (#138)

---
+++ kairo-sql-feature/src/main/kotlin/kairo/sql/JdbiProvider.kt
@@ -0,0 +1,24 @@
+package kairo.sql
+
+import com.google.inject.Inject
+import com.google.inject.Singleton
+import com.zaxxer.hikari.HikariDataSource
+import kairo.dependencyInjection.LazySingletonProvider
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.KotlinPlugin
+import org.jdbi.v3.postgres.PostgresPlugin
+
+/**
+ * There is a single global [Jdbi] instance in Kairo.
+ */
+@Singleton
+public class JdbiProvider @Inject constructor(
+  private val dataSource: HikariDataSource,
+) : LazySingletonProvider<Jdbi>() {
+  override fun create(): Jdbi =
+    Jdbi.create(dataSource).apply {
+      installPlugin(KairoPlugin())
+      installPlugin(KotlinPlugin())
+      installPlugin(PostgresPlugin())
+    }
+}
