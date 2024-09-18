fcb2be74a57a5ff70995b6eb63017cec891e1720
Jeff Hudson <jhudson@jhudson.ca>
2024-09-17 19:14:49 -0600

    Keep refactors

--- kairo-serialization/src/main/kotlin/kairo/serialization/StringDeserializer.kt
+++ kairo-serialization/src/main/kotlin/kairo/serialization/StringDeserializer.kt
@@ -12,17 +12,14 @@ public class StringDeserializer(
   override val tokens: Set<JsonToken> =
     setOf(JsonToken.VALUE_STRING)
 
-  override fun createContextual(ctxt: DeserializationContext, property: BeanProperty?): StringDeserializer {
-    val trim = property?.let { it.getAnnotation(TrimWhitespace::class.java)?.type } ?: return this
-    return StringDeserializer(trim)
-  }
+  override fun createContextual(ctxt: DeserializationContext, property: BeanProperty?): StringDeserializer =
+    StringDeserializer(
+      trimWhitespace = property?.getAnnotation<TrimWhitespace>()?.type ?: trimWhitespace,
+    )
 
   override fun extract(p: JsonParser): String {
     var result = p.text
-    with(trimWhitespace) {
-      if (trimStart) result = result.trimStart()
-      if (trimEnd) result = result.trimEnd()
-    }
+    result = trimWhitespace.transform(result)
     return result
   }
 }
--- kairo-serialization/src/main/kotlin/kairo/serialization/TrimWhitespace.kt
+++ kairo-serialization/src/main/kotlin/kairo/serialization/TrimWhitespace.kt
@@ -1,15 +1,25 @@
 package kairo.serialization
 
 /**
- * String properties have their starting and ending whitespace trimmed.
- * This behaviour can be customized using this annotation.
+ * Removes whitespace from the start and/or end of strings during deserialization.
  */
 @Target(AnnotationTarget.FIELD)
 public annotation class TrimWhitespace(val type: Type) {
-  public enum class Type(public val trimStart: Boolean, public val trimEnd: Boolean) {
+  public enum class Type(
+    public val trimStart: Boolean,
+    public val trimEnd: Boolean,
+  ) {
     TrimNone(trimStart = false, trimEnd = false),
     TrimStart(trimStart = true, trimEnd = false),
     TrimEnd(trimStart = false, trimEnd = true),
     TrimBoth(trimStart = true, trimEnd = true),
+    ;
+
+    public fun transform(original: String): String {
+      var result = original
+      if (trimStart) result = result.trimStart()
+      if (trimEnd) result = result.trimEnd()
+      return result
+    }
   }
 }
