dfa8c4bc916bbd0a224ad9da25518712805cd736
Jeff Hudson <jhudson@jhudson.ca>
2024-09-24 13:17:54 -0600

    Docs

--- kairo-logging/src/main/kotlin/kairo/logging/KLogger.kt
+++ kairo-logging/src/main/kotlin/kairo/logging/KLogger.kt
@@ -3,6 +3,9 @@ package kairo.logging
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.Marker
 
+/**
+ * Helper to log a message but only log the exception if DEBUG-level logs are enabled.
+ */
 public fun KLogger.ifDebugEnabled(e: Throwable?, marker: Marker? = null): Throwable? {
   if (!isDebugEnabled(marker)) return null
   return e
