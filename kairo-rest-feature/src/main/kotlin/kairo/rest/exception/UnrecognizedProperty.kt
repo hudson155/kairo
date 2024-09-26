8f5b018a3bec647612ace6247156112d6fe3aa7c
Jeff Hudson <jhudson@jhudson.ca>
2024-09-26 00:14:12 -0600

    Improve fallback Jackson handling

--- kairo-rest-feature/src/main/kotlin/kairo/rest/exception/UnrecognizedProperty.kt
+++ kairo-rest-feature/src/main/kotlin/kairo/rest/exception/UnrecognizedProperty.kt
@@ -1,19 +1,19 @@
 package kairo.rest.exception
 
-import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import com.fasterxml.jackson.databind.JsonMappingException
 
 public class UnrecognizedProperty(
   override val path: String?,
-  override val location: Location,
-) : JacksonBadRequestException.WithPathAndLocation(
+  override val location: Location?,
+) : JsonBadRequestException.WithPathAndLocation(
   message = "Unrecognized property." +
     " This property is not recognized. Is it named incorrectly?",
 ) {
   public companion object {
-    internal fun from(e: MismatchedInputException): UnrecognizedProperty =
+    internal fun from(e: JsonMappingException): UnrecognizedProperty =
       UnrecognizedProperty(
         path = parsePath(e.path),
-        location = parseLocation(e.location),
+        location = e.location?.let { parseLocation(it) },
       )
   }
 }
