99fb42c9df845bc4a3d4dab0e810438339b04d05
Jeff Hudson <jhudson@jhudson.ca>
2024-09-25 21:18:03 -0600

    Change serialization settings

--- kairo-rest-client/src/main/kotlin/kairo/client/ktorClientMapper.kt
+++ kairo-rest-client/src/main/kotlin/kairo/client/ktorClientMapper.kt
@@ -3,10 +3,9 @@ package kairo.client
 import com.fasterxml.jackson.databind.json.JsonMapper
 import kairo.serialization.ObjectMapperFactory
 import kairo.serialization.ObjectMapperFormat
-import kairo.serialization.module.primitives.TrimWhitespace
 
 internal val ktorClientMapper: JsonMapper =
   ObjectMapperFactory.builder(ObjectMapperFormat.Json) {
+    allowUnknownProperties = true
     prettyPrint = true
-    trimWhitespace = TrimWhitespace.Type.TrimBoth
   }.build()
