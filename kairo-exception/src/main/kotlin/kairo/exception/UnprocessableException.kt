b45f7785a057d15d6b6292ea705e1911aebc2ca8
Jeff Hudson <jhudson@jhudson.ca>
2024-09-25 23:50:42 -0600

    Introduce unprocessable exceptions

---
+++ kairo-exception/src/main/kotlin/kairo/exception/UnprocessableException.kt
@@ -0,0 +1,15 @@
+package kairo.exception
+
+import io.ktor.http.HttpStatusCode
+
+public class UnprocessableException internal constructor(
+  private val e: NotFoundException,
+) : KairoException(e.message) {
+  override val statusCode: HttpStatusCode = HttpStatusCode.UnprocessableEntity
+
+  override val response: Map<String, Any>
+    get() = e.response
+}
+
+public fun unprocessable(e: NotFoundException): UnprocessableException =
+  UnprocessableException(e)
