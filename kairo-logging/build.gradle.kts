4a3039b8f60b457a226a45268ccd2805cc5eb7d7
Jeff Hudson <jhudson@jhudson.ca>
2024-10-08 14:53:36 -0400

    Support logging templates (#215)

--- kairo-logging/build.gradle.kts
+++ kairo-logging/build.gradle.kts
@@ -7,5 +7,6 @@ dependencies {
   api(libs.kotlinLoggingJvm)
   implementation(libs.log4j2ConfigYaml)
   implementation(libs.log4j2Core)
+  implementation(libs.log4j2LayoutTemplateJson)
   implementation(libs.log4j2Slf4j2Impl)
 }
