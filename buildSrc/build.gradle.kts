4001e9eada6fdfd776cb7acc420767a7aab12da1
Jeff Hudson <jhudson@jhudson.ca>
2024-10-11 11:38:33 -0400

    Upgrade dependencies, including Ktor 3.0 (#238)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,13 +8,13 @@ repositories {
 
 dependencies {
   // https://kotlinlang.org/docs/releases.html#release-details
-  implementation(kotlin("gradle-plugin", "2.0.20"))
+  implementation(kotlin("gradle-plugin", "2.0.21"))
 
   // https://plugins.gradle.org/plugin/com.google.cloud.artifactregistry.gradle-plugin
-  implementation("gradle.plugin.com.google.cloud.artifactregistry", "artifactregistry-gradle-plugin", "2.2.2")
+  implementation("com.google.cloud.artifactregistry", "artifactregistry-gradle-plugin", "2.2.3")
 
   // https://plugins.gradle.org/plugin/io.gitlab.arturbosch.detekt
-  implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.6")
+  implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.7")
 }
 
 kotlin {
