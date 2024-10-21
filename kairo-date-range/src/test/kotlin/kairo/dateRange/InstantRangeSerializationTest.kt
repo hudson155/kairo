b04087c087f91fe058f70f908514cd633b3b671c
Jeff Hudson <jhudson@jhudson.ca>
2024-10-21 14:00:47 -0600

    Introduce InstantRange type

---
+++ kairo-date-range/src/test/kotlin/kairo/dateRange/InstantRangeSerializationTest.kt
@@ -0,0 +1,43 @@
+package kairo.dateRange
+
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import java.time.Instant
+import kairo.serialization.ObjectMapperFactory
+import kairo.serialization.ObjectMapperFormat
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+internal class InstantRangeSerializationTest {
+  private val mapper: JsonMapper = ObjectMapperFactory.builder(ObjectMapperFormat.Json).build()
+
+  private val typicalRange: InstantRange =
+    InstantRange.exclusive(
+      start = Instant.parse("2023-11-13T19:44:32.123456789Z"),
+      endExclusive = Instant.parse("2024-01-04T00:01:59.567890123Z"),
+    )
+
+  @Test
+  fun serialize(): Unit = runTest {
+    mapper.writeValueAsString(typicalRange)
+      .shouldBe(
+        "{\"start\":\"2023-11-13T19:44:32.123456789Z\",\"endExclusive\":\"2024-01-04T00:01:59.567890123Z\"}",
+      )
+  }
+
+  @Test
+  fun deserialize(): Unit = runTest {
+    mapper.readValue<InstantRange>(
+      "{\"start\":\"2023-11-13T19:44:32.123456789Z\",\"endExclusive\":\"2024-01-04T00:01:59.567890123Z\"}",
+    ).shouldBe(typicalRange)
+
+    shouldThrow<JsonMappingException> {
+      mapper.readValue<InstantRange>(
+        "{\"start\":\"2023-11-13T19:44:32.123456789Z\",\"endExclusive\":\"2023-11-13T19:44:32.123456788Z\"}",
+      )
+    }
+  }
+}
