427fe353b7829b162fee7272ede0645a8cc5c3cf
Jeff Hudson <jhudson@jhudson.ca>
2024-10-21 14:40:15 -0400

    Introduce LocalDateRange type (#270)

---
+++ kairo-date-range/src/main/kotlin/kairo/dateRange/LocalDateRange.kt
@@ -0,0 +1,84 @@
+package kairo.dateRange
+
+import com.fasterxml.jackson.annotation.JsonIncludeProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDate
+import java.time.Period
+import java.time.Year
+import java.time.YearMonth
+import java.time.temporal.ChronoUnit
+
+@JsonIncludeProperties("start", "endInclusive")
+public data class LocalDateRange private constructor(
+  @JsonProperty("start")
+  @field:JsonProperty("start")
+  override val start: LocalDate,
+  @JsonProperty("endInclusive")
+  @field:JsonProperty("endInclusive")
+  override val endInclusive: LocalDate,
+) : ClosedRange<LocalDate>, OpenEndRange<LocalDate> {
+  override val endExclusive: LocalDate = endInclusive.plusDays(1)
+
+  init {
+    require(start <= endExclusive) { "Invalid date range." }
+  }
+
+  public val size: Long
+    get() = ChronoUnit.DAYS.between(start, endInclusive) + 1
+
+  override fun isEmpty(): Boolean =
+    start > endInclusive
+
+  @Suppress("ConvertTwoComparisonsToRangeCheck")
+  override fun contains(value: LocalDate): Boolean =
+    value >= start && value <= endInclusive
+
+  public fun toList(increment: Period = Period.ofDays(1)): List<LocalDate> =
+    buildList {
+      var i = 0
+      while (true) {
+        val date = start.plus(increment.multipliedBy(i))
+        if (date.isAfter(endInclusive)) break
+        add(date)
+        i++
+      }
+    }
+
+  public companion object {
+    public fun inclusive(start: LocalDate, endInclusive: LocalDate): LocalDateRange =
+      LocalDateRange(start, endInclusive)
+
+    public fun exclusive(start: LocalDate, endExclusive: LocalDate): LocalDateRange =
+      LocalDateRange(start, endExclusive.minusDays(1))
+
+    public fun forYear(year: Year): LocalDateRange {
+      val start = year.atDay(1)
+      val endExclusive = start.plusYears(1)
+      return exclusive(start = start, endExclusive = endExclusive)
+    }
+
+    public fun forMonth(month: YearMonth): LocalDateRange {
+      val start = month.atDay(1)
+      val endExclusive = start.plusMonths(1)
+      return exclusive(start = start, endExclusive = endExclusive)
+    }
+
+    public fun forDate(date: LocalDate): LocalDateRange =
+      exclusive(start = date, endExclusive = date.plusDays(1))
+  }
+}
+
+public operator fun LocalDate.rangeTo(that: LocalDate): LocalDateRange =
+  LocalDateRange.inclusive(this, that)
+
+public operator fun LocalDate.rangeUntil(that: LocalDate): LocalDateRange =
+  LocalDateRange.exclusive(this, that)
+
+public fun Year.toLocalDateRange(): LocalDateRange =
+  LocalDateRange.forYear(this)
+
+public fun YearMonth.toLocalDateRange(): LocalDateRange =
+  LocalDateRange.forMonth(this)
+
+public fun LocalDate.toLocalDateRange(): LocalDateRange =
+  LocalDateRange.forDate(this)
---
+++ kairo-date-range/src/test/kotlin/kairo/dateRange/LocalDateRangeSerializationTest.kt
@@ -0,0 +1,38 @@
+package kairo.dateRange
+
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import java.time.LocalDate
+import kairo.serialization.ObjectMapperFactory
+import kairo.serialization.ObjectMapperFormat
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+internal class LocalDateRangeSerializationTest {
+  private val mapper: JsonMapper = ObjectMapperFactory.builder(ObjectMapperFormat.Json).build()
+
+  private val typicalRange: LocalDateRange =
+    LocalDateRange.inclusive(
+      start = LocalDate.parse("2023-11-13"),
+      endInclusive = LocalDate.parse("2024-01-04"),
+    )
+
+  @Test
+  fun serialize(): Unit = runTest {
+    mapper.writeValueAsString(typicalRange)
+      .shouldBe("{\"start\":\"2023-11-13\",\"endInclusive\":\"2024-01-04\"}")
+  }
+
+  @Test
+  fun deserialize(): Unit = runTest {
+    mapper.readValue<LocalDateRange>("{\"start\":\"2023-11-13\",\"endInclusive\":\"2024-01-04\"}")
+      .shouldBe(typicalRange)
+
+    shouldThrow<JsonMappingException> {
+      mapper.readValue<LocalDateRange>("{\"start\":\"2023-11-13\",\"endInclusive\":\"2023-11-11\"}")
+    }
+  }
+}
