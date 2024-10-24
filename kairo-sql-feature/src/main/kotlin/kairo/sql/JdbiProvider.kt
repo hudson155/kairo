deb7583ae86df3c35e210554e7a333594b391e46
Jeff Hudson <jhudson@jhudson.ca>
2024-10-24 18:15:32 -0400

    Add JSONB support (#289)

--- kairo-sql-feature/src/main/kotlin/kairo/sql/JdbiProvider.kt
+++ kairo-sql-feature/src/main/kotlin/kairo/sql/JdbiProvider.kt
@@ -4,6 +4,7 @@ import com.google.inject.Inject
 import com.google.inject.Singleton
 import com.zaxxer.hikari.HikariDataSource
 import kairo.dependencyInjection.LazySingletonProvider
+import kairo.sql.plugin.jackson.JacksonPlugin
 import kairo.sql.plugin.kairo.KairoPlugin
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.kotlin.KotlinPlugin
@@ -18,6 +19,7 @@ public class JdbiProvider @Inject constructor(
 ) : LazySingletonProvider<Jdbi>() {
   override fun create(): Jdbi =
     Jdbi.create(dataSource).apply {
+      installPlugin(JacksonPlugin())
       installPlugin(KairoPlugin())
       installPlugin(KotlinPlugin())
       installPlugin(PostgresPlugin())
