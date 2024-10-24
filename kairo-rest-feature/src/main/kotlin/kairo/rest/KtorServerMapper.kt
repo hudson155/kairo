8ec4b621b56fa0387de996fb29d9531ebcb9cde5
Jeff Hudson <jhudson@jhudson.ca>
2024-10-23 23:41:21 -0400

    Add REST client XML support (#285)

--- kairo-rest-feature/src/main/kotlin/kairo/rest/KtorServerMapper.kt
+++ kairo-rest-feature/src/main/kotlin/kairo/rest/KtorServerMapper.kt
@@ -1,11 +1,18 @@
 package kairo.rest
 
 import com.fasterxml.jackson.databind.json.JsonMapper
+import kairo.serialization.ObjectMapperFactory
 import kairo.serialization.jsonMapper
 import kairo.serialization.module.primitives.TrimWhitespace
 
-public val ktorServerMapper: JsonMapper =
-  jsonMapper {
+public object KtorServerMapper {
+  public val json: JsonMapper =
+    jsonMapper {
+      configureKtorServerMapper()
+    }
+
+  private fun ObjectMapperFactory<*, *>.configureKtorServerMapper() {
     prettyPrint = true
     trimWhitespace = TrimWhitespace.Type.TrimBoth
   }
+}
