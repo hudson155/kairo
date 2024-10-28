bac5c492e1bb564154deaaf361d7ec4002c8dbea
Jeff Hudson <jhudson@jhudson.ca>
2024-10-27 23:38:28 -0600

    Release 0.27.0

--- buildSrc/src/main/kotlin/kairo-publish.gradle.kts
+++ buildSrc/src/main/kotlin/kairo-publish.gradle.kts
@@ -26,7 +26,7 @@ publishing {
         val match = requireNotNull(regex.matchEntire(path)) { "Invalid project name: $path." }
         return@run match.groupValues[1].replace(':', '-')
       }
-      version = "0.26.0"
+      version = "0.27.0"
       from(components["java"])
     }
   }
