a622870b95ad01222fcc083b24e844aba9bd529c
Jeff Hudson <jhudson@jhudson.ca>
2024-11-07 02:06:36 -0700

    Remove unused

--- kairo-google-cloud-scheduler-feature/src/main/kotlin/kairo/googleCloudScheduler/JobCreator.kt
+++ kairo-google-cloud-scheduler-feature/src/main/kotlin/kairo/googleCloudScheduler/JobCreator.kt
@@ -6,9 +6,6 @@ import kairo.rest.endpoint.RestEndpoint
  * Primarily, [RealJobCreator].
  */
 public abstract class JobCreator {
-  @Target(AnnotationTarget.CLASS)
-  public annotation class Queue(val name: String)
-
   public data class Config(
     val name: String,
     val description: String,
