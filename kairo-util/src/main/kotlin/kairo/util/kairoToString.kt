1c605bdd15fefb66a47f0c414c5eff48a27d7378
Jeff Hudson <jhudson@jhudson.ca>
2024-11-09 09:44:39 -0700

    Lint

--- kairo-util/src/main/kotlin/kairo/util/kairoToString.kt
+++ kairo-util/src/main/kotlin/kairo/util/kairoToString.kt
@@ -8,7 +8,7 @@ public fun <T : Any> kairoToString(obj: T, kClass: KClass<T>): String =
     name = kClass.simpleName!!,
     properties = kClass.memberProperties.map { property ->
       Pair(property.name, property.get(obj))
-    }
+    },
   )
 
 public fun kairoToString(name: String, properties: List<Pair<String, Any?>>): String =
