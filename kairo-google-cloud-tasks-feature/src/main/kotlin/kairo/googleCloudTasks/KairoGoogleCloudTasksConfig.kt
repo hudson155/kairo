6beadef7b7ff4b72f26883a5bf300e0dc01cc59b
Jeff Hudson <jhudson@jhudson.ca>
2024-12-27 00:13:30 -0800

    Support non-GAE Google Cloud Tasks

--- kairo-google-cloud-tasks-feature/src/main/kotlin/kairo/googleCloudTasks/KairoGoogleCloudTasksConfig.kt
+++ kairo-google-cloud-tasks-feature/src/main/kotlin/kairo/googleCloudTasks/KairoGoogleCloudTasksConfig.kt
@@ -5,9 +5,10 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes(
+  JsonSubTypes.Type(KairoGoogleCloudTasksConfig.GoogleAppEngine::class, name = "GoogleAppEngine"),
+  JsonSubTypes.Type(KairoGoogleCloudTasksConfig.Http::class, name = "Http"),
   JsonSubTypes.Type(KairoGoogleCloudTasksConfig.Local::class, name = "Local"),
   JsonSubTypes.Type(KairoGoogleCloudTasksConfig.Noop::class, name = "Noop"),
-  JsonSubTypes.Type(KairoGoogleCloudTasksConfig.Real::class, name = "Real"),
   JsonSubTypes.Type(KairoGoogleCloudTasksConfig.TransactionAware::class, name = "TransactionAware"),
 )
 public sealed class KairoGoogleCloudTasksConfig {
@@ -15,6 +16,20 @@ public sealed class KairoGoogleCloudTasksConfig {
     val prefix: String?,
     val suffix: String?,
   )
+
+  public data class GoogleAppEngine(
+    override val projectId: String,
+    override val location: String,
+    override val queueName: QueueName,
+    val service: String,
+  ) : Real()
+
+  public data class Http(
+    val baseUrl: String,
+    override val projectId: String,
+    override val location: String,
+    override val queueName: QueueName,
+  ) : Real()
 
   /**
    * See [LocalGoogleCloudTasks].
@@ -29,12 +44,11 @@ public sealed class KairoGoogleCloudTasksConfig {
   /**
    * See [RealGoogleCloudTasks].
    */
-  public data class Real(
-    val projectId: String,
-    val location: String,
-    val queueName: QueueName,
-    val service: String,
-  ) : KairoGoogleCloudTasksConfig()
+  public sealed class Real : KairoGoogleCloudTasksConfig() {
+    public abstract val projectId: String
+    public abstract val location: String
+    public abstract val queueName: QueueName
+  }
 
   /**
    * See [TransactionAwareGoogleCloudTasks].
--- kairo-google-cloud-tasks-feature/src/main/kotlin/kairo/googleCloudTasks/RealGoogleCloudTasks.kt
+++ kairo-google-cloud-tasks-feature/src/main/kotlin/kairo/googleCloudTasks/RealGoogleCloudTasks.kt
@@ -1,27 +1,21 @@
 package kairo.googleCloudTasks
 
-import com.google.cloud.tasks.v2.AppEngineHttpRequest
-import com.google.cloud.tasks.v2.AppEngineRouting
 import com.google.cloud.tasks.v2.CloudTasksClient
 import com.google.cloud.tasks.v2.CreateTaskRequest
-import com.google.cloud.tasks.v2.HttpMethod
 import com.google.cloud.tasks.v2.QueueName
-import com.google.protobuf.ByteString
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
-import io.ktor.http.HttpHeaders
 import kairo.googleCommon.await
-import kairo.rest.KtorServerMapper
 import kairo.rest.endpoint.RestEndpoint
 import kairo.rest.endpoint.RestEndpointDetails
 
 private val logger: KLogger = KotlinLogging.logger {}
 
-public class RealGoogleCloudTasks(
+public abstract class RealGoogleCloudTasks(
   private val cloudTasksClient: CloudTasksClient,
   private val tasksConfig: KairoGoogleCloudTasksConfig.Real,
 ) : GoogleCloudTasks() {
-  override suspend fun create(endpoint: RestEndpoint<*, *>) {
+  final override suspend fun create(endpoint: RestEndpoint<*, *>) {
     logger.info { "Creating task for endpoint: $endpoint." }
     val task = task(endpoint)
     logger.info { "Creating task: $task." }
@@ -32,7 +26,7 @@ public class RealGoogleCloudTasks(
     cloudTasksClient.createTaskCallable().futureCall(request).await()
   }
 
-  override fun close() {
+  final override fun close() {
     cloudTasksClient.close()
   }
 
@@ -45,21 +39,5 @@ public class RealGoogleCloudTasks(
     return QueueName.of(tasksConfig.projectId, tasksConfig.location, queue)
   }
 
-  private fun buildTask(details: RestEndpointDetails): com.google.cloud.tasks.v2.Task =
-    com.google.cloud.tasks.v2.Task.newBuilder().apply {
-      @Suppress("DuplicatedCode")
-      appEngineHttpRequest = AppEngineHttpRequest.newBuilder().apply {
-        appEngineRouting = buildAppEngineRouting()
-        httpMethod = HttpMethod.valueOf(details.method.value)
-        relativeUri = details.path
-        details.contentType?.let { putHeaders(HttpHeaders.ContentType, it.toString()) }
-        details.accept?.let { putHeaders(HttpHeaders.Accept, it.toString()) }
-        details.body?.let { body = ByteString.copyFrom(KtorServerMapper.json.writeValueAsBytes(it)) }
-      }.build()
-    }.build()
-
-  private fun buildAppEngineRouting(): AppEngineRouting =
-    AppEngineRouting.newBuilder().apply {
-      service = tasksConfig.service
-    }.build()
+  protected abstract fun buildTask(details: RestEndpointDetails): com.google.cloud.tasks.v2.Task
 }
