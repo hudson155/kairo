2766d0a0fbc41f8a2b055a8d72291483b6d07293
Jeff Hudson <jhudson@jhudson.ca>
2024-12-29 17:08:42 -0800

    Rename transaction

--- kairo-sql-feature/src/main/kotlin/kairo/sql/Sql.kt
+++ kairo-sql-feature/src/main/kotlin/kairo/sql/Sql.kt
@@ -4,18 +4,25 @@ import com.google.inject.Key
 import kairo.sql.store.SqlStore
 import kairo.transactionManager.TransactionManager
 import org.jdbi.v3.core.Handle
+import org.jdbi.v3.core.Jdbi
 
 /**
  * This wrapper is useful both for injecting into [SqlStore]
  * and for using SQL transactions from outside stores.
  */
 public class Sql(
+  private val jdbi: Jdbi,
   private val sqlTransaction: Key<SqlTransaction>,
   private val transactionManager: TransactionManager,
 ) {
-  public suspend fun <T> sql(block: suspend (handle: Handle) -> T): T =
+  public suspend fun <T> transaction(block: suspend (handle: Handle) -> T): T =
     transactionManager.transaction(sqlTransaction) {
       val context = checkNotNull(getSqlContext())
       return@transaction block(context.handle)
+    }
+
+  public suspend fun <T> sql(block: suspend (handle: Handle) -> T): T =
+    jdbi.open().use { handle ->
+      return@use block(handle)
     }
 }
--- kairo-sql-feature/src/main/kotlin/kairo/sql/SqlProvider.kt
+++ kairo-sql-feature/src/main/kotlin/kairo/sql/SqlProvider.kt
@@ -8,6 +8,7 @@ import kairo.dependencyInjection.getInstance
 import kairo.dependencyInjection.getNamedInstance
 import kairo.dependencyInjection.namedKey
 import kairo.transactionManager.TransactionManager
+import org.jdbi.v3.core.Jdbi
 
 @Singleton
 public class SqlProvider @Inject constructor(
@@ -17,11 +18,13 @@ public class SqlProvider @Inject constructor(
   private lateinit var injector: Injector
 
   private val config: KairoSqlConfig by lazy { injector.getNamedInstance(name) }
+  private val jdbi: Jdbi by lazy { injector.getNamedInstance(name) }
   private val transactionManager: TransactionManager by lazy { injector.getInstance() }
 
   override fun create(): Sql {
     val sqlTransaction = namedKey<SqlTransaction>(config.name)
     return Sql(
+      jdbi = jdbi,
       sqlTransaction = sqlTransaction,
       transactionManager = transactionManager,
     )
