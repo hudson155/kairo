faaa17412ac76b3d3646fe8974a22ed850c29ba8
Jeff Hudson <jhudson@jhudson.ca>
2025-01-14 20:19:53 -0500

    Improve Guice injector extensions (#411)

--- kairo-dependency-injection/src/main/kotlin/kairo/dependencyInjection/Injector.kt
+++ kairo-dependency-injection/src/main/kotlin/kairo/dependencyInjection/Injector.kt
@@ -9,17 +9,28 @@ import kotlin.reflect.KClass
 public inline fun <reified T : Any> Injector.getInstance(): T =
   getInstance(key())
 
+public inline fun <reified T : Any> Injector.hasInstance(): Boolean =
+  hasInstance(key<T>())
+
+public inline fun <reified T : Any> Injector.hasInstance(key: Key<T>): Boolean =
+  getExistingBinding<T>(key) != null
+
 public fun <T : Any> Injector.getInstanceByClass(kClass: KClass<T>): T =
   getInstance(Key.get(kClass.java))
 
 public inline fun <reified T : Any> Injector.getNamedInstance(name: String): T =
-  getInstance(namedKey<T>(name))
+  getInstance(namedKey(name))
 
 public inline fun <reified T : Any> Injector.getInstanceOptional(): T? =
-  getExistingBinding(key<T>())?.let { it.provider.get() }
+  getInstanceOptional(key())
 
 public fun <T : Any> Injector.getInstanceOptionalByClass(kClass: KClass<T>): T? =
-  getExistingBinding(Key.get(kClass.java))?.let { it.provider.get() }
+  getInstanceOptional(Key.get(kClass.java))
 
 public inline fun <reified T : Any> Injector.getNamedInstanceOptional(name: String): T? =
-  getExistingBinding(namedKey<T>(name))?.let { it.provider.get() }
+  getInstanceOptional(namedKey(name))
+
+public inline fun <reified T : Any> Injector.getInstanceOptional(key: Key<T>): T? {
+  if (!hasInstance(key)) return null
+  return getInstance(key)
+}
