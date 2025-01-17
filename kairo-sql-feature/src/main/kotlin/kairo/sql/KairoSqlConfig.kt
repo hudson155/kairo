8c2507cf1d03ea5f8cf6c37af12dfb2e63830db2
Jeff Hudson <jeff@highbeam.co>
2025-01-17 13:15:22 -0500

    Allow setting SQL schema

--- kairo-sql-feature/src/main/kotlin/kairo/sql/KairoSqlConfig.kt
+++ kairo-sql-feature/src/main/kotlin/kairo/sql/KairoSqlConfig.kt
@@ -5,6 +5,7 @@ import kairo.protectedString.ProtectedString
 public data class KairoSqlConfig(
   val name: String,
   val jdbcUrl: String,
+  val schema: String?,
   val username: String?,
   val password: ProtectedString?,
   val properties: Map<String, String>,
