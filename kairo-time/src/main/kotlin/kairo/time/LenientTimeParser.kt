490a368f23db615fb12bbdab5635f679a3da73ac
Jeff Hudson <jeff@highbeam.co>
2025-01-18 14:21:47 -0500

    Introduce lenient time parser

---
+++ kairo-time/src/main/kotlin/kairo/time/LenientTimeParser.kt
@@ -0,0 +1,103 @@
+package kairo.time
+
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Year
+import java.time.YearMonth
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.format.DateTimeParseException
+import java.time.format.SignStyle
+import java.time.temporal.ChronoField
+
+public object LenientTimeParser {
+  private val year: DateTimeFormatter = build(SmallestRequiredUnit.Year)
+
+  private val yearMonth: DateTimeFormatter = build(SmallestRequiredUnit.Month)
+
+  private val localDate: DateTimeFormatter = build(SmallestRequiredUnit.Day)
+
+  private val localDateTime: DateTimeFormatter = build(SmallestRequiredUnit.Day)
+
+  private val zonedDateTime: DateTimeFormatter = build(SmallestRequiredUnit.Day)
+
+  public fun year(string: String): Year =
+    Year.parse(string, year)
+
+  public fun yearMonth(string: String): YearMonth =
+    YearMonth.parse(string, yearMonth)
+
+  public fun localDate(string: String): LocalDate =
+    LocalDate.parse(string, localDate)
+
+  public fun instant(string: String): Instant {
+    try {
+      return ZonedDateTime.parse(string, zonedDateTime).toInstant()
+    } catch (zonedException: DateTimeParseException) {
+      try {
+        return LocalDateTime.parse(string, localDateTime).toInstant(ZoneOffset.UTC)
+      } catch (localException: DateTimeParseException) {
+        zonedException.addSuppressed(localException)
+        throw zonedException
+      }
+    }
+  }
+
+  private enum class SmallestRequiredUnit(
+    val monthIsRequired: Boolean,
+    val dayIsRequired: Boolean,
+  ) {
+    Year(monthIsRequired = false, dayIsRequired = false),
+    Month(monthIsRequired = true, dayIsRequired = false),
+    Day(monthIsRequired = true, dayIsRequired = true),
+  }
+
+  private fun build(smallestRequiredUnit: SmallestRequiredUnit): DateTimeFormatter =
+    DateTimeFormatterBuilder().apply {
+      appendValue(ChronoField.YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
+      append(required = smallestRequiredUnit.monthIsRequired) {
+        appendLiteral('-')
+        appendValue(ChronoField.MONTH_OF_YEAR, 2)
+        append(required = smallestRequiredUnit.dayIsRequired) {
+          appendLiteral('-')
+          appendValue(ChronoField.DAY_OF_MONTH, 2)
+          append(required = false) {
+            appendLiteral('T')
+            append(required = false) {
+              appendValue(ChronoField.HOUR_OF_DAY, 2)
+              append(required = false) {
+                appendLiteral(':')
+                appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+                append(required = false) {
+                  appendLiteral(':')
+                  appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+                  append(required = false) {
+                    appendFraction(ChronoField.NANO_OF_SECOND, 1, 9, true)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      append(required = false) {
+        appendOffsetId()
+      }
+      append(required = false) {
+        appendLiteral('[')
+        parseCaseSensitive()
+        appendZoneRegionId()
+        appendLiteral(']')
+      }
+      parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+    }.toFormatter()
+
+  private fun DateTimeFormatterBuilder.append(required: Boolean, block: DateTimeFormatterBuilder.() -> Unit) {
+    if (!required) optionalStart()
+    block()
+    if (!required) optionalEnd()
+  }
+}
---
+++ kairo-time/src/test/kotlin/kairo/time/LenientTimeParserTest.kt
@@ -0,0 +1,197 @@
+package kairo.time
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import java.time.Instant
+import java.time.LocalDate
+import java.time.Month
+import java.time.Year
+import java.time.YearMonth
+import java.time.format.DateTimeParseException
+import org.junit.jupiter.api.Test
+
+internal class LenientTimeParserTest {
+  @Test
+  fun `year, only year`() {
+    LenientTimeParser.year("2023")
+      .shouldBe(Year.of(2023))
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.year("2023T")
+    }
+  }
+
+  @Test
+  fun `year, additional`() {
+    LenientTimeParser.year("2023-12")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20:51")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20:51:58")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20:51:58.084")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20:51:58.084577243")
+      .shouldBe(Year.of(2023))
+  }
+
+  @Test
+  fun `year, time zones`() {
+    LenientTimeParser.year("2023-12-31T20:51:58.084577243")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20:51:58.084577243Z")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20:51:58.084577243Z[America/Edmonton]")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20:51:58.084577243-07:00")
+      .shouldBe(Year.of(2023))
+    LenientTimeParser.year("2023-12-31T20:51:58.084577243-07:00[America/Edmonton]")
+      .shouldBe(Year.of(2023))
+  }
+
+  @Test
+  fun `yearMonth, insufficient`() {
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.yearMonth("2023")
+    }
+  }
+
+  @Test
+  fun `yearMonth, only yearMonth`() {
+    LenientTimeParser.yearMonth("2023-12")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.yearMonth("2023-12T")
+    }
+  }
+
+  @Test
+  fun `yearMonth, additional`() {
+    LenientTimeParser.yearMonth("2023-12-31")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20:51")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20:51:58")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20:51:58.084")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20:51:58.084577243")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+  }
+
+  @Test
+  fun `yearMonth, time zones`() {
+    LenientTimeParser.yearMonth("2023-12-31T20:51:58.084577243")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20:51:58.084577243Z")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20:51:58.084577243Z[America/Edmonton]")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20:51:58.084577243-07:00")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+    LenientTimeParser.yearMonth("2023-12-31T20:51:58.084577243-07:00[America/Edmonton]")
+      .shouldBe(YearMonth.of(2023, Month.DECEMBER))
+  }
+
+  @Test
+  fun `localDate, insufficient`() {
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.localDate("2023")
+    }
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.localDate("2023-12")
+    }
+  }
+
+  @Test
+  fun `localDate, only localDate`() {
+    LenientTimeParser.localDate("2023-12-31")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T")
+  }
+
+  @Test
+  fun `localDate, additional`() {
+    LenientTimeParser.localDate("2023-12-31T20")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T20:51")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T20:51:58")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T20:51:58.084")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T20:51:58.084577243")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+  }
+
+  @Test
+  fun `localDate, time zones`() {
+    LenientTimeParser.localDate("2023-12-31T20:51:58.084577243")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T20:51:58.084577243Z")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T20:51:58.084577243Z[America/Edmonton]")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T20:51:58.084577243-07:00")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+    LenientTimeParser.localDate("2023-12-31T20:51:58.084577243-07:00[America/Edmonton]")
+      .shouldBe(LocalDate.of(2023, Month.DECEMBER, 31))
+  }
+
+  @Test
+  fun `instant, insufficient`() {
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.instant("2023")
+    }
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.instant("2023T")
+    }
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.instant("2023-12")
+    }
+    shouldThrow<DateTimeParseException> {
+      LenientTimeParser.instant("2023-12T")
+    }
+  }
+
+  @Test
+  fun `instant, only instant`() {
+    LenientTimeParser.instant("2023-12-31")
+      .shouldBe(Instant.parse("2023-12-31T00:00:00Z"))
+    LenientTimeParser.instant("2023-12-31T")
+      .shouldBe(Instant.parse("2023-12-31T00:00:00Z"))
+    LenientTimeParser.instant("2023-12-31T20")
+      .shouldBe(Instant.parse("2023-12-31T20:00:00Z"))
+    LenientTimeParser.instant("2023-12-31T20:51")
+      .shouldBe(Instant.parse("2023-12-31T20:51:00Z"))
+    LenientTimeParser.instant("2023-12-31T20:51:58")
+      .shouldBe(Instant.parse("2023-12-31T20:51:58Z"))
+    LenientTimeParser.instant("2023-12-31T20:51:58.085")
+      .shouldBe(Instant.parse("2023-12-31T20:51:58.085Z"))
+    LenientTimeParser.instant("2023-12-31T20:51:58.084577243")
+      .shouldBe(Instant.parse("2023-12-31T20:51:58.084577243Z"))
+  }
+
+  @Test
+  fun `instant, time zones`() {
+    LenientTimeParser.instant("2023-12-31T20:51:58.084577243")
+      .shouldBe(Instant.parse("2023-12-31T20:51:58.084577243Z"))
+    LenientTimeParser.instant("2023-12-31T20:51:58.084577243Z")
+      .shouldBe(Instant.parse("2023-12-31T20:51:58.084577243Z"))
+    LenientTimeParser.instant("2023-12-31T20:51:58.084577243Z[America/Edmonton]")
+      .shouldBe(Instant.parse("2023-12-31T20:51:58.084577243Z"))
+    LenientTimeParser.instant("2023-12-31T20:51:58.084577243-07:00")
+      .shouldBe(Instant.parse("2024-01-01T03:51:58.084577243Z"))
+    LenientTimeParser.instant("2023-12-31T20:51:58.084577243-07:00[America/Edmonton]")
+      .shouldBe(Instant.parse("2024-01-01T03:51:58.084577243Z"))
+  }
+}
