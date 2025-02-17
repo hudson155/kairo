8332e8875742ddb15ff0ae223d66813b0d9b8329
Jeff Hudson <jeff@highbeam.co>
2025-02-17 13:38:08 -0500

    Add JDBI mapper helper for KairoId

--- kairo-sql-feature/src/main/kotlin/kairo/sql/plugin/kairo/KairoIdColumnMapper.kt
+++ kairo-sql-feature/src/main/kotlin/kairo/sql/plugin/kairo/KairoIdColumnMapper.kt
@@ -2,7 +2,10 @@ package kairo.sql.plugin.kairo
 
 import java.sql.ResultSet
 import kairo.id.KairoId
+import org.jdbi.v3.core.kotlin.mapTo
 import org.jdbi.v3.core.mapper.ColumnMapper
+import org.jdbi.v3.core.result.ResultIterable
+import org.jdbi.v3.core.statement.Query
 import org.jdbi.v3.core.statement.StatementContext
 
 public class KairoIdColumnMapper : ColumnMapper<KairoId> {
@@ -11,3 +14,6 @@ public class KairoIdColumnMapper : ColumnMapper<KairoId> {
     return KairoId.fromString(string)
   }
 }
+
+public fun Query.mapToKairoId(): ResultIterable<KairoId> =
+  mapTo<String>().map { KairoId.fromString(it) }
