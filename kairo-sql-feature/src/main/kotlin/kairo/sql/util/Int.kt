51419670069e8f58149f2c189f5a29a450a1b876
Jeff Hudson <jeff@highbeam.co>
2025-02-26 16:29:05 -0500

    Messages

--- kairo-sql-feature/src/main/kotlin/kairo/sql/util/Int.kt
+++ kairo-sql-feature/src/main/kotlin/kairo/sql/util/Int.kt
@@ -1,7 +1,7 @@
 package kairo.sql.util
 
 public fun Int.expectSingle(
-  block: () -> Nothing = { throw IllegalArgumentException("IntArray was empty.") },
+  block: () -> Nothing = { throw IllegalArgumentException("Int was 0.") },
 ) {
   if (this == 0) block()
   require(this == 1) { "IntArray has an unexpected element: $this." }
