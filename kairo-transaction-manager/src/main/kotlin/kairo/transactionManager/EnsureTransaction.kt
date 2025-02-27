c6b6ee1516ccc6f90f68a13cebaccc9a09a03d90
Shubham Goyal <goyal.shubham2701@gmail.com>
2025-02-27 16:35:38 -0500

    Add suspense

--- kairo-transaction-manager/src/main/kotlin/kairo/transactionManager/EnsureTransaction.kt
+++ kairo-transaction-manager/src/main/kotlin/kairo/transactionManager/EnsureTransaction.kt
@@ -8,7 +8,7 @@ private val logger: KLogger = KotlinLogging.logger {}
 /**
  * Extension function that checks for transaction context before executing a suspending function.
  */
-public suspend fun <T> ensureTransaction(block: () -> T): T {
+public suspend fun <T> ensureTransaction(block: suspend () -> T): T {
   getTransactionContext() ?: run {
     logger.error { "Transaction context is missing" }
     throw MissingTransactionException()
