63a5983d26f0a31ea3371558a71314f5040feb49
Jeff Hudson <jeff@highbeam.co>
2025-04-04 17:04:09 -0400

    Release 2.0.1

--- buildSrc/src/main/kotlin/kairo-publish.gradle.kts
+++ buildSrc/src/main/kotlin/kairo-publish.gradle.kts
@@ -26,7 +26,7 @@ publishing {
         val match = requireNotNull(regex.matchEntire(path)) { "Invalid project name: $path." }
         return@run match.groupValues[1].replace(':', '-')
       }
-      version = "2.0.0"
+      version = "2.0.1"
       from(components["java"])
     }
   }
