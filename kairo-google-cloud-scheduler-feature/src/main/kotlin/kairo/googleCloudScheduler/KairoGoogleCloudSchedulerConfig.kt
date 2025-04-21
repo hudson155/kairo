51b53025bdce62887805d92e4f41ec81ce6e04bb
Jeff Hudson <jeff@highbeam.co>
2025-04-21 09:08:52 -0600

    Remove native GAE support

--- kairo-google-cloud-scheduler-feature/src/main/kotlin/kairo/googleCloudScheduler/KairoGoogleCloudSchedulerConfig.kt
+++ kairo-google-cloud-scheduler-feature/src/main/kotlin/kairo/googleCloudScheduler/KairoGoogleCloudSchedulerConfig.kt
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes(
-  JsonSubTypes.Type(KairoGoogleCloudSchedulerConfig.GoogleAppEngine::class, name = "GoogleAppEngine"),
   JsonSubTypes.Type(KairoGoogleCloudSchedulerConfig.Http::class, name = "Http"),
   JsonSubTypes.Type(KairoGoogleCloudSchedulerConfig.Noop::class, name = "Noop"),
 )
@@ -14,16 +13,6 @@ public sealed class KairoGoogleCloudSchedulerConfig {
     val prefix: String?,
     val suffix: String?,
   )
-
-  /**
-   * See [GoogleAppEngineGoogleCloudScheduler].
-   */
-  public data class GoogleAppEngine(
-    override val projectId: String,
-    override val location: String,
-    override val jobName: JobName,
-    val service: String,
-  ) : Real()
 
   /**
    * See [HttpGoogleCloudScheduler].
