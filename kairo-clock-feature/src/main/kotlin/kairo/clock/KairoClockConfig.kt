199dda2c26f9ea5e52c0ea01f503a4b447c37693
Jeff Hudson <jeffryderhudson@gmail.com>
2025-07-17 06:30:09 -0600

    More clock documentation (#624)

--- kairo-clock-feature/src/main/kotlin/kairo/clock/KairoClockConfig.kt
+++ kairo-clock-feature/src/main/kotlin/kairo/clock/KairoClockConfig.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.time.Instant
 import java.time.ZoneId
+import java.time.ZoneOffset
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes(
@@ -11,11 +12,17 @@ import java.time.ZoneId
   JsonSubTypes.Type(KairoClockConfig.System::class, name = "System"),
 )
 public sealed class KairoClockConfig {
+  /**
+   * Use a fixed clock for tests.
+   */
   public data class Fixed(
     val instant: Instant,
     val timeZone: ZoneId,
   ) : KairoClockConfig()
 
+  /**
+   * Use the system clock for production code, probably with a time zone of [ZoneOffset.UTC].
+   */
   public data class System(
     val timeZone: ZoneId,
   ) : KairoClockConfig()
