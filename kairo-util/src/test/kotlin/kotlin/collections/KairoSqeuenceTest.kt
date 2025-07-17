84acd271a82e8472b7db4cecd0a94fb244e87d84
Jeff Hudson <jeff@highbeam.co>
2025-07-17 06:40:30 -0600

    Add sequence support to singleNullOrThrow

---
+++ kairo-util/src/test/kotlin/kotlin/collections/KairoSqeuenceTest.kt
@@ -0,0 +1,25 @@
+@file:Suppress("InvalidPackageDeclaration")
+
+package kotlin.collections
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+internal class KairoSqeuenceTest {
+  @Test
+  fun `singleNullOrThrow (no predicate)`(): Unit = runTest {
+    sequenceOf<Int>().singleNullOrThrow().shouldBeNull()
+    listOf(1).singleNullOrThrow().shouldBe(1)
+    shouldThrow<IllegalArgumentException> { listOf(1, 2).singleNullOrThrow() }
+  }
+
+  @Test
+  fun `singleNullOrThrow (with predicate)`(): Unit = runTest {
+    sequenceOf(1, 2).singleNullOrThrow { it < 1 }.shouldBeNull()
+    sequenceOf(1, 2).singleNullOrThrow { it < 2 }.shouldBe(1)
+    shouldThrow<IllegalArgumentException> { listOf(1, 2).singleNullOrThrow { it < 3 } }
+  }
+}
