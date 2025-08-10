6932eb1a68b31aa3412242bc5c732146047a5405
Jeff Hudson <jeff@highbeam.co>
2025-08-10 13:54:29 -0600

    Return type

--- kairo-command-runner/src/main/kotlin/kairo/commandRunner/NoopCommandRunner.kt
+++ kairo-command-runner/src/main/kotlin/kairo/commandRunner/NoopCommandRunner.kt
@@ -2,7 +2,7 @@ package kairo.commandRunner
 
 public object NoopCommandRunner : CommandRunner() {
   @Insecure
-  override fun run(command: String): Sequence<String> {
+  override fun run(command: String): Nothing {
     throw NotImplementedError("This command runner is no-op.")
   }
 }
