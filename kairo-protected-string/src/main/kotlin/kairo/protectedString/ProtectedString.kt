33231714f2df4e1507456254839e7c8e72ae0d1d
Jeff Hudson <jeff@highbeam.co>
2025-08-10 16:46:20 -0600

    Introduce protected string (no serialization yet)

---
+++ kairo-protected-string/src/main/kotlin/kairo/protectedString/ProtectedString.kt
@@ -0,0 +1,30 @@
+package kairo.protectedString
+
+/**
+ * Represents a string value that should not be logged or otherwise exposed, such as an API key.
+ * The [toString] implementation redacts the value.
+ * Accessing [value] is allowed by opting into [ProtectedString.Access], but please use it with care.
+ * Note that this is a simplistic implementation; it does not clean up memory after itself.
+ */
+@Suppress("UseDataClass")
+@OptIn(ProtectedString.Access::class)
+public class ProtectedString @Access constructor(
+  @Access
+  public val value: String,
+) {
+  @RequiresOptIn
+  @Target(AnnotationTarget.CONSTRUCTOR, AnnotationTarget.PROPERTY)
+  public annotation class Access
+
+  override fun equals(other: Any?): Boolean {
+    if (other !is ProtectedString) return false
+    if (value != other.value) return false
+    return true
+  }
+
+  override fun hashCode(): Int =
+    value.hashCode()
+
+  override fun toString(): String =
+    "ProtectedString(value='REDACTED')"
+}
