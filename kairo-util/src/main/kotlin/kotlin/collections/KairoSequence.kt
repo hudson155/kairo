14c97e202ed8cdfa26709d2c10b827cb56706b38
Jeff Hudson <jeffryderhudson@gmail.com>
2025-08-10 13:14:19 -0600

    Introduce util module (#650)

---
+++ kairo-util/src/main/kotlin/kotlin/collections/KairoSequence.kt
@@ -0,0 +1,40 @@
+@file:Suppress("InvalidPackageDeclaration")
+
+package kotlin.collections
+
+/**
+ * Adapted from [Sequence.single] and [Sequence.singleOrNull].
+ * Returns the single element, or null if the collection is empty.
+ * If the collection has more than one element, throws [IllegalArgumentException].
+ *
+ * This is less surprising than [singleOrNull]
+ * because it doesn't return null when there are multiple items in the collection.
+ */
+public fun <T> Sequence<T>.singleNullOrThrow(): T? {
+  val iterator = iterator()
+  if (!iterator.hasNext()) return null
+  val single = iterator.next()
+  if (!iterator.hasNext()) return single
+  throw IllegalArgumentException("Collection has more than one element.")
+}
+
+/**
+ * Adapted from [Sequence.single] and [Sequence.singleOrNull].
+ * Returns the single element matching the given [predicate], or null if no elements match.
+ * If more than one element matches, throws [IllegalArgumentException].
+ *
+ * This is less surprising than [singleOrNull]
+ * because it doesn't return null when there are multiple items in the collection.
+ */
+public inline fun <T> Sequence<T>.singleNullOrThrow(predicate: (T) -> Boolean): T? {
+  var single: T? = null
+  var found = false
+  for (element in this) {
+    if (!predicate(element)) continue
+    require(!found) { "Predicate matches more than one element." }
+    single = element
+    found = true
+  }
+  if (!found) return null
+  return single
+}
---
+++ kairo-util/src/test/kotlin/kotlin/collections/SingleNullOrThrowTest.kt
@@ -0,0 +1,182 @@
+@file:Suppress("InvalidPackageDeclaration")
+
+package kotlin.collections
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import java.util.ArrayDeque
+import java.util.Queue
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+@Suppress("ArrayPrimitive")
+internal class SingleNullOrThrowTest {
+  @Test
+  fun `no predicate, 0 elements, array`() {
+    runTest {
+      val array: Array<Int> = emptyArray<Int>()
+      array.singleNullOrThrow()
+        .shouldBeNull()
+    }
+  }
+
+  @Test
+  fun `no predicate, 0 elements, list`() {
+    runTest {
+      val list: List<Int> = emptyList()
+      list.singleNullOrThrow()
+        .shouldBeNull()
+    }
+  }
+
+  @Test
+  fun `no predicate, 0 elements, queue`() {
+    runTest {
+      val queue: Queue<Int> = ArrayDeque(emptyList())
+      queue.singleNullOrThrow()
+        .shouldBeNull()
+    }
+  }
+
+  @Test
+  fun `no predicate, 1 element, array`() {
+    runTest {
+      val array: Array<Int> = arrayOf(9)
+      array.singleNullOrThrow()
+        .shouldBe(9)
+    }
+  }
+
+  @Test
+  fun `no predicate, 1 element, list`() {
+    runTest {
+      val list: List<Int> = listOf(9)
+      list.singleNullOrThrow()
+        .shouldBe(9)
+    }
+  }
+
+  @Test
+  fun `no predicate, 1 element, queue`() {
+    runTest {
+      val queue: Queue<Int> = ArrayDeque(listOf(9))
+      queue.singleNullOrThrow()
+        .shouldBe(9)
+    }
+  }
+
+  @Test
+  fun `no predicate, 2 elements, array`() {
+    runTest {
+      val array: Array<Int> = arrayOf(4, 8)
+      shouldThrow<IllegalArgumentException> {
+        array.singleNullOrThrow()
+      }
+    }
+  }
+
+  @Test
+  fun `no predicate, 2 elements, list`() {
+    runTest {
+      val list: List<Int> = listOf(4, 8)
+      shouldThrow<IllegalArgumentException> {
+        list.singleNullOrThrow()
+      }
+    }
+  }
+
+  @Test
+  fun `no predicate, 2 elements, queue`() {
+    runTest {
+      val queue: Queue<Int> = ArrayDeque(listOf(4, 8))
+      shouldThrow<IllegalArgumentException> {
+        queue.singleNullOrThrow()
+      }
+    }
+  }
+
+  @Test
+  fun `with predicate, 0 matches, array`() {
+    runTest {
+      val array: Array<Int> = arrayOf(3, 4, 5)
+      array.singleNullOrThrow { it > 5 }
+        .shouldBeNull()
+    }
+  }
+
+  @Test
+  fun `with predicate, 0 matches, list`() {
+    runTest {
+      val list: List<Int> = listOf(3, 4, 5)
+      list.singleNullOrThrow { it > 5 }
+        .shouldBeNull()
+    }
+  }
+
+  @Test
+  fun `with predicate, 0 matches, queue`() {
+    runTest {
+      val queue: Queue<Int> = ArrayDeque(listOf(3, 4, 5))
+      queue.singleNullOrThrow { it > 5 }
+        .shouldBeNull()
+    }
+  }
+
+  @Test
+  fun `with predicate, 1 match, array`() {
+    runTest {
+      val array: Array<Int> = arrayOf(3, 4, 5)
+      array.singleNullOrThrow { it > 4 }
+        .shouldBe(5)
+    }
+  }
+
+  @Test
+  fun `with predicate, 1 match, list`() {
+    runTest {
+      val list: List<Int> = listOf(3, 4, 5)
+      list.singleNullOrThrow { it > 4 }
+        .shouldBe(5)
+    }
+  }
+
+  @Test
+  fun `with predicate, 1 match, queue`() {
+    runTest {
+      val queue: Queue<Int> = ArrayDeque(listOf(3, 4, 5))
+      queue.singleNullOrThrow { it > 4 }
+        .shouldBe(5)
+    }
+  }
+
+  @Test
+  fun `with predicate, 2 matches, array`() {
+    runTest {
+      val array: Array<Int> = arrayOf(3, 4, 5)
+      shouldThrow<IllegalArgumentException> {
+        array.singleNullOrThrow { it > 3 }
+      }
+    }
+  }
+
+  @Test
+  fun `with predicate, 2 matches, list`() {
+    runTest {
+      val list: List<Int> = listOf(3, 4, 5)
+      shouldThrow<IllegalArgumentException> {
+        list.singleNullOrThrow { it > 3 }
+      }
+    }
+  }
+
+  @Test
+  fun `with predicate, 2 matches, queue`() {
+    runTest {
+      val queue: Queue<Int> = ArrayDeque(listOf(3, 4, 5))
+      shouldThrow<IllegalArgumentException> {
+        queue.singleNullOrThrow { it > 3 }
+      }
+    }
+  }
+}
