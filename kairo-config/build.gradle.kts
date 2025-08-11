bef07db7c05c9c69721d744a7697d07689e7dfc0
Jeff Hudson <jeff@highbeam.co>
2025-08-11 10:18:18 -0600

    Remove comments

--- kairo-config/build.gradle.kts
+++ kairo-config/build.gradle.kts
@@ -13,17 +13,10 @@ kotlin {
 dependencies {
   implementation(project(":bom"))
 
-  // api(project(":kairo-command-runner"))
-  // api(project(":kairo-environment-variable-supplier"))
-  // api(project(":kairo-gcp-secret-supplier"))
-  // implementation(project(":kairo-logging"))
-  // api(project(":kairo-protected-string")) // Exposed for clients.
   api(project(":kairo-serialization"))
-  // implementation(project(":kairo-util"))
 
   implementation(libs.hocon)
   implementation(libs.kotlinxSerializationHocon)
 
-  // testImplementation(project(":kairo-logging:testing"))
   testImplementation(project(":kairo-testing"))
 }
