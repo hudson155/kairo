62be3d25e17608d25dc93d992d6b574b9e0f9dbc
Jeff Hudson <jeff@highbeam.co>
2025-08-10 22:52:39 -0600

    Guava BOM

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
   api(platform(libs.gcpBom))
+  api(platform(libs.guavaBom))
   api(platform(libs.kotlinxCoroutinesBom))
   api(platform(libs.kotlinxSerializationBom))
   api(platform(libs.log4jBom))
