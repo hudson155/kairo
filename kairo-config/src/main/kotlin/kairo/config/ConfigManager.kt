fb6caa2598485315d81b9be58a005b3e8446e0a6
Jeff Hudson <jeff@highbeam.co>
2025-08-11 16:33:10 -0600

    Split config manager

--- kairo-config/src/main/kotlin/kairo/config/ConfigManager.kt
+++ kairo-config/src/main/kotlin/kairo/config/ConfigManager.kt
@@ -1,53 +1,15 @@
 package kairo.config
 
-import com.typesafe.config.Config
-import io.github.oshai.kotlinlogging.KLogger
-import io.github.oshai.kotlinlogging.KotlinLogging
 import kairo.config.ConfigManager.Companion.defaultSources
-import kairo.config.ConfigPropertySource.ConfigProperty
-import kotlin.reflect.KClass
-import kotlinx.serialization.DeserializationStrategy
-import kotlinx.serialization.hocon.Hocon
-import kotlinx.serialization.modules.PolymorphicModuleBuilder
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.polymorphic
-import kotlinx.serialization.serializer
-
-private val logger: KLogger = KotlinLogging.logger {}
 
 /**
  * The config manager allows you to resolve config properties from multiple sources.
  * There are a few built-in sources (see [defaultSources]). Feel free to implement your own too.
  */
 public class ConfigManager(sources: List<ConfigPropertySource<*>> = defaultSources) {
-  private val hocon: Hocon =
-    Hocon {
-      serializersModule = SerializersModule {
-        polymorphic(ConfigProperty::class) {
-          sources.forEach { subclass(it) }
-        }
-      }
-    }
+  public val deserializer: ConfigDeserializer = ConfigDeserializer(sources)
 
-  private val sources: Map<KClass<out ConfigProperty>, ConfigPropertySource<*>> =
-    sources.associateBy { it.kClass }
-
-  init {
-    logger.info { "Initialized ConfigManager with sources: $sources." }
-  }
-
-  public inline fun <reified T : Any> deserialize(config: Config): T =
-    deserialize(serializer<T>(), config)
-
-  public fun <T : Any> deserialize(deserializer: DeserializationStrategy<T>, config: Config): T =
-    hocon.decodeFromConfig(deserializer, config)
-
-  @Suppress("UNCHECKED_CAST")
-  public suspend fun <P : ConfigProperty> resolveProperty(configProperty: P): String? {
-    logger.debug { "Resolving property: $configProperty." }
-    val configPropertySource = checkNotNull(sources[configProperty::class])
-    return (configPropertySource as ConfigPropertySource<P>).resolve(configProperty)
-  }
+  public val propertyResolver: ConfigPropertyResolver = ConfigPropertyResolver(sources)
 
   public companion object {
     private val defaultSources: List<ConfigPropertySource<*>> =
@@ -56,10 +18,4 @@ public class ConfigManager(sources: List<ConfigPropertySource<*>> = defaultSourc
         PlaintextConfigPropertySource(),
       )
   }
-}
-
-private fun <P : ConfigProperty> PolymorphicModuleBuilder<ConfigProperty>.subclass(
-  source: ConfigPropertySource<P>,
-) {
-  subclass(source.kClass, source.serializer)
 }
--- kairo-config/src/test/kotlin/kairo/config/ConfigLoaderTest.kt
+++ kairo-config/src/test/kotlin/kairo/config/ConfigLoaderTest.kt
@@ -34,7 +34,7 @@ internal class ConfigLoaderTest {
   fun test(): Unit =
     runTest {
       val config = ConfigFactory.parseResources("config/testing.conf").resolve()
-      configManager.deserialize<Config>(config).shouldBe(
+      configManager.deserializer.deserialize<Config>(config).shouldBe(
         Config(
           name = "My config",
           message = "Hello, World!",
