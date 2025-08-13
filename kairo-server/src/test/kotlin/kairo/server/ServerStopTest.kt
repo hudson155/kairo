a6e4d25f39c0b99dd45b0f7fc8ef20f18a496d14
Jeff Hudson <jeff@highbeam.co>
2025-08-13 12:07:29 -0600

    Stop tests

---
+++ kairo-server/src/test/kotlin/kairo/server/ServerStopTest.kt
@@ -0,0 +1,165 @@
+package kairo.server
+
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import kairo.feature.Feature
+import kairo.feature.LifecycleEvent
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+internal class ServerStopTest {
+  @Test
+  fun `happy path`(): Unit =
+    runTest {
+      val features = listOf(
+        object : Feature() {
+          override val name: String = "Test (0)"
+        },
+        object : Feature() {
+          override val name: String = "Test (1)"
+        },
+      )
+      val server = Server(
+        name = "Test",
+        features = features,
+      )
+      server.start()
+      server.state.shouldBe(ServerState.Running)
+      server.stop()
+      server.state.shouldBe(ServerState.Default)
+    }
+
+  @Test
+  fun `Feature stop exception (first one fails)`(): Unit =
+    runTest {
+      val events = MutableStateFlow(emptyList<String>())
+      /**
+       * The signal is used to ensure that "Test(0)" stops before "Test(1)".
+       */
+      val signal = CompletableDeferred<Unit>()
+      val features = listOf(
+        object : Feature() {
+          override val name: String = "Test (0)"
+          override suspend fun on(event: LifecycleEvent) {
+            when (event) {
+              LifecycleEvent.Start -> {
+                events.update { it + "start Test (0)" }
+              }
+              LifecycleEvent.Stop -> {
+                events.update { it + "stop Test (0)" }
+                try {
+                  @Suppress("ThrowingExceptionsWithoutMessageOrCause", "TooGenericExceptionThrown")
+                  throw RuntimeException("Exception from Test (1)")
+                } finally {
+                  signal.complete(Unit)
+                }
+              }
+            }
+          }
+        },
+        object : Feature() {
+          override val name: String = "Test (1)"
+          override suspend fun on(event: LifecycleEvent) {
+            when (event) {
+              LifecycleEvent.Start -> {
+                events.update { it + "start Test (1)" }
+              }
+              LifecycleEvent.Stop -> {
+                signal.await()
+                delay(1.seconds)
+                events.update { it + "stop Test (1)" }
+              }
+            }
+          }
+        },
+      )
+      val server = Server(
+        name = "Test",
+        features = features,
+      )
+      server.start()
+      server.state.shouldBe(ServerState.Running)
+      shouldNotThrowAny {
+        server.stop()
+      }
+      server.state.shouldBe(ServerState.Default)
+      events.value.should { events ->
+        events[0].shouldBe("start Test (0)")
+        events[1].shouldBe("start Test (1)")
+        events.subList(2, 4).shouldContainExactlyInAnyOrder(
+          "stop Test (0)",
+          "stop Test (1)",
+        )
+        events.shouldHaveSize(4)
+      }
+    }
+
+  @Test
+  fun `Feature stop exception (last one fails)`(): Unit =
+    runTest {
+      val events = MutableStateFlow(emptyList<String>())
+      /**
+       * The signal is used to ensure that "Test(0)" stops before "Test(1)".
+       */
+      val signal = CompletableDeferred<Unit>()
+      val features = listOf(
+        object : Feature() {
+          override val name: String = "Test (0)"
+          override suspend fun on(event: LifecycleEvent) {
+            when (event) {
+              LifecycleEvent.Start -> {
+                events.update { it + "start Test (0)" }
+              }
+              LifecycleEvent.Stop -> {
+                events.update { it + "stop Test (0)" }
+                signal.complete(Unit)
+              }
+            }
+          }
+        },
+        object : Feature() {
+          override val name: String = "Test (1)"
+          override suspend fun on(event: LifecycleEvent) {
+            when (event) {
+              LifecycleEvent.Start -> {
+                events.update { it + "start Test (1)" }
+              }
+              LifecycleEvent.Stop -> {
+                signal.await()
+                events.update { it + "stop Test (1)" }
+                @Suppress("ThrowingExceptionsWithoutMessageOrCause", "TooGenericExceptionThrown")
+                throw RuntimeException("Exception from Test (1)")
+              }
+            }
+          }
+        },
+      )
+      val server = Server(
+        name = "Test",
+        features = features,
+      )
+      server.start()
+      server.state.shouldBe(ServerState.Running)
+      shouldNotThrowAny {
+        server.stop()
+      }
+      server.state.shouldBe(ServerState.Default)
+      events.value.should { events ->
+        events[0].shouldBe("start Test (0)")
+        events[1].shouldBe("start Test (1)")
+        events.subList(2, 4).shouldContainExactlyInAnyOrder(
+          "stop Test (0)",
+          "stop Test (1)",
+        )
+        events.shouldHaveSize(4)
+      }
+    }
+}
