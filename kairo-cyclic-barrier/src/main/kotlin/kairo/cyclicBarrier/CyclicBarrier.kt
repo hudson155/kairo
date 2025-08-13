16a749400ab7170bac3aa91732b5b0765304297b
Jeff Hudson <jeffryderhudson@gmail.com>
2025-08-12 18:47:12 -0600

    Introduce cyclic barrier (#684)

---
+++ kairo-cyclic-barrier/src/main/kotlin/kairo/cyclicBarrier/CyclicBarrier.kt
@@ -0,0 +1,105 @@
+package kairo.cyclicBarrier
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+/**
+ * Kotlin coroutine equivalent of Java's [java.util.concurrent.CyclicBarrier].
+ * See https://en.wikipedia.org/wiki/Barrier_(computer_science) to understand barriers.
+ * Called "cyclic" because it can be re-used after being broken.
+ */
+public class CyclicBarrier(
+  /**
+   * The number of parties that must call [await] before the barrier breaks.
+   */
+  private val parties: Int,
+  /**
+   * This command will be executed after the barrier is broken,
+   * before any of the parties are released.
+   */
+  private val barrierCommand: (suspend () -> Unit)? = null,
+) {
+  public class Exception internal constructor(cause: Throwable) : CancellationException() {
+    init {
+      initCause(cause)
+    }
+
+    public companion object {
+      internal fun from(e: Throwable): Throwable =
+        e as? CancellationException ?: Exception(e)
+    }
+  }
+
+  @Suppress("UseDataClass")
+  private class Generation(var broken: Boolean)
+
+  private val lock: Mutex = Mutex()
+  private var generation: Generation = Generation(broken = false)
+  private var waiting: Int = 0
+  private var gate: CompletableDeferred<Unit> = CompletableDeferred()
+
+  init {
+    require(parties > 0) { "Parties must be positive." }
+  }
+
+  public suspend fun await() {
+    val generation: Generation
+    val gate: CompletableDeferred<Unit>
+
+    lock.withLock {
+      this.waiting++
+      if (this.waiting == this.parties) {
+        breakBarrier()
+        return
+      } else {
+        generation = this.generation
+        gate = this.gate
+      }
+    }
+
+    try {
+      gate.await()
+    } catch (e: Throwable) {
+      handleException(e, generation)
+    }
+  }
+
+  private suspend fun breakBarrier() {
+    val gate = this.gate
+    nextGeneration()
+    if (barrierCommand == null) {
+      gate.complete(Unit)
+      return
+    }
+    try {
+      barrierCommand()
+      gate.complete(Unit)
+    } catch (e: Throwable) {
+      @Suppress("NoNameShadowing")
+      val e = Exception.from(e)
+      gate.completeExceptionally(e)
+      throw e
+    }
+  }
+
+  private suspend fun handleException(e: Throwable, generation: Generation): Nothing {
+    var gate: CompletableDeferred<Unit>? = null
+    lock.withLock {
+      if (this.generation === generation && !this.generation.broken) {
+        this.generation.broken = true
+        gate = this.gate
+        nextGeneration()
+      }
+    }
+    gate?.completeExceptionally(Exception.from(e))
+    throw e
+  }
+
+  private fun nextGeneration() {
+    this.generation = Generation(broken = false)
+    this.waiting = 0
+    this.gate = CompletableDeferred()
+  }
+}
---
+++ kairo-cyclic-barrier/src/test/kotlin/kairo/cyclicBarrier/CyclicBarrierTest.kt
@@ -0,0 +1,280 @@
+package kairo.cyclicBarrier
+
+import io.kotest.assertions.async.shouldTimeout
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.throwable.shouldHaveCauseInstanceOf
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withTimeout
+import org.junit.jupiter.api.Test
+
+internal class CyclicBarrierTest {
+  @Test
+  fun `non-positive parties`(): Unit =
+    runTest {
+      shouldThrow<IllegalArgumentException> {
+        CyclicBarrier(0)
+      }
+      shouldThrow<IllegalArgumentException> {
+        CyclicBarrier(-1)
+      }
+    }
+
+  @Test
+  fun `happy path (1 party)`(): Unit =
+    runTest {
+      val barrier = CyclicBarrier(1)
+      repeat(2) {
+        shouldNotThrowAny {
+          barrier.await()
+        }
+      }
+    }
+
+  @Test
+  fun `happy path (2 parties)`(): Unit =
+    runTest {
+      val barrier = CyclicBarrier(2)
+      repeat(2) {
+        launch {
+          shouldNotThrowAny {
+            barrier.await()
+          }
+        }
+      }
+    }
+
+  @Test
+  fun `await timeout`(): Unit =
+    runTest {
+      val barrier = CyclicBarrier(2)
+      launch {
+        shouldTimeout(1.seconds) {
+          barrier.await()
+        }
+      }
+    }
+
+  @Test
+  fun `10,000 parties`(): Unit =
+    runTest {
+      val barrier = CyclicBarrier(10_000)
+      withTimeout(1.seconds) {
+        List(10_000) {
+          launch {
+            barrier.await()
+          }
+        }.joinAll()
+      }
+    }
+
+  @Test
+  fun `10,000 cycles`(): Unit =
+    runTest {
+      val barrier = CyclicBarrier(2)
+      withTimeout(1.seconds) {
+        List(20_000) {
+          launch {
+            barrier.await()
+          }
+        }.joinAll()
+      }
+    }
+
+  @Test
+  fun cancellation(): Unit =
+    runTest {
+      val barrier = CyclicBarrier(3)
+      launch {
+        shouldThrow<CancellationException> {
+          barrier.await()
+        }
+      }
+      val second = launch {
+        shouldThrow<CancellationException> {
+          barrier.await()
+        }
+      }
+      delay(10.milliseconds)
+      second.cancel()
+      repeat(3) {
+        launch {
+          shouldNotThrowAny {
+            barrier.await()
+          }
+        }
+      }
+    }
+
+  @Test
+  fun `barrier cancellation`(): Unit =
+    runTest {
+      val cancel = generateSequence(true) { false }.iterator()
+      val barrier = CyclicBarrier(3) {
+        if (cancel.next()) {
+          throw CancellationException()
+        }
+      }
+      repeat(3) {
+        launch {
+          shouldThrow<CancellationException> {
+            barrier.await()
+          }
+        }
+      }
+      repeat(3) {
+        launch {
+          shouldNotThrowAny {
+            barrier.await()
+          }
+        }
+      }
+    }
+
+  @Test
+  fun `barrier exception`(): Unit =
+    runTest {
+      val cancel = generateSequence(true) { false }.iterator()
+      val barrier = CyclicBarrier(3) {
+        if (cancel.next()) {
+          @Suppress("ThrowingExceptionsWithoutMessageOrCause", "TooGenericExceptionThrown")
+          throw RuntimeException()
+        }
+      }
+      repeat(3) {
+        launch {
+          shouldThrow<CyclicBarrier.Exception> {
+            barrier.await()
+          }.shouldHaveCauseInstanceOf<RuntimeException>()
+        }
+      }
+      repeat(3) {
+        launch {
+          shouldNotThrowAny {
+            barrier.await()
+          }
+        }
+      }
+    }
+
+  @Test
+  fun `event trace, single iteration`(): Unit =
+    runTest {
+      val events = MutableStateFlow(emptyList<String>())
+      val barrier = CyclicBarrier(4)
+      List(4) {
+        launch {
+          events.update { it + "before await" }
+          barrier.await()
+          events.update { it + "after await" }
+        }
+      }.joinAll()
+      events.value.shouldContainExactly(
+        "before await",
+        "before await",
+        "before await",
+        "before await",
+        "after await",
+        "after await",
+        "after await",
+        "after await",
+      )
+    }
+
+  @Test
+  fun `event trace, multiple iterations`(): Unit =
+    runTest {
+      val events = MutableStateFlow(emptyList<String>())
+      val barrier = CyclicBarrier(4)
+      List(80) {
+        launch {
+          events.update { it + "before await" }
+          barrier.await()
+          events.update { it + "after await" }
+        }
+      }.joinAll()
+      var eventList = events.value
+      repeat(20) {
+        eventList.take(4).shouldContainExactly(
+          "before await",
+          "before await",
+          "before await",
+          "before await",
+        )
+        eventList = eventList.drop(4)
+        repeat(4) {
+          eventList = eventList
+            .indexOfFirst { event -> event == "after await" }
+            .let { i -> eventList.take(i) + eventList.drop(i + 1) }
+        }
+      }
+      eventList.shouldBeEmpty()
+    }
+
+  @Test
+  fun `event trace with barrier command, single iteration`(): Unit =
+    runTest {
+      val events = MutableStateFlow(emptyList<String>())
+      val barrier = CyclicBarrier(4) { events.update { it + "barrier command" } }
+      List(4) {
+        launch {
+          events.update { it + "before await" }
+          barrier.await()
+          events.update { it + "after await" }
+        }
+      }.joinAll()
+      events.value.shouldContainExactly(
+        "before await",
+        "before await",
+        "before await",
+        "before await",
+        "barrier command",
+        "after await",
+        "after await",
+        "after await",
+        "after await",
+      )
+    }
+
+  @Test
+  fun `event trace with barrier command, multiple iterations`(): Unit =
+    runTest {
+      val events = MutableStateFlow(emptyList<String>())
+      val barrier = CyclicBarrier(4) { events.update { it + "barrier command" } }
+      List(80) {
+        launch {
+          events.update { it + "before await" }
+          barrier.await()
+          events.update { it + "after await" }
+        }
+      }.joinAll()
+      var eventList = events.value
+      repeat(20) {
+        eventList.take(5).shouldContainExactly(
+          "before await",
+          "before await",
+          "before await",
+          "before await",
+          "barrier command",
+        )
+        eventList = eventList.drop(5)
+        repeat(4) {
+          eventList =
+            eventList
+              .indexOfFirst { event -> event == "after await" }
+              .let { i -> eventList.take(i) + eventList.drop(i + 1) }
+        }
+      }
+      eventList.shouldBeEmpty()
+    }
+}
