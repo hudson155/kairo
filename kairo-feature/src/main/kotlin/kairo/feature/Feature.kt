290a6db83a65d0c4034659d0c6cff8bf3cbaec75
Jeff Hudson <jeff@highbeam.co>
2025-08-13 11:13:23 -0600

    Start again

---
+++ kairo-feature/src/main/kotlin/kairo/feature/Feature.kt
@@ -0,0 +1,23 @@
+package kairo.feature
+
+/**
+ * Features are the core building block of a Kairo Server.
+ * A Feature defines an encapsulated set of functionality,
+ * such as support for a specific database or managed service,
+ * or a domain-specific set of functionality.
+ */
+public abstract class Feature {
+  /**
+   * Feature names should be unique for debugging purposes,
+   * but they don't strictly need to be.
+   */
+  public abstract val name: String
+
+  /**
+   * Called during various lifecycle events.
+   * This method is called concurrently with other Features,
+   * so IT MUST BE THREAD-SAFE.
+   */
+  public open suspend fun on(event: LifecycleEvent): Unit =
+    Unit
+}
