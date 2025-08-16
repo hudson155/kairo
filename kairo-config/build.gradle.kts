2f90333da9cb8f7e5f33aacc26d54ad0d48c1b76
Jeff Hudson <jeff@highbeam.co>
2025-08-16 16:06:39 -0600

    Change config library to a proxy

--- kairo-config/build.gradle.kts
+++ kairo-config/build.gradle.kts
@@ -1,23 +1,8 @@
 plugins {
   id("kairo-library")
   id("kairo-library-publish")
-  id("kairo-serialization")
-}
-
-kotlin {
-  compilerOptions {
-    freeCompilerArgs.add("-opt-in=kotlinx.serialization.ExperimentalSerializationApi")
-  }
 }
 
 dependencies {
-  implementation(project(":kairo-gcp-secret-supplier"))
-  implementation(project(":kairo-logging"))
-  implementation(project(":kairo-util"))
-
-  implementation(libs.hocon)
-  implementation(libs.serialization.hocon)
-
-  testImplementation(project(":kairo-gcp-secret-supplier:testing"))
-  testImplementation(project(":kairo-testing"))
+  api(libs.hocon)
 }
--- kairo-config/src/main/kotlin/kairo/config/ConfigProperty.kt
+++
@@ -1,9 +0,0 @@
-package kairo.config
-
-import kotlinx.serialization.Serializable
-
-/**
- * This must be a data class that defines the shape of the config property.
- */
-@Serializable
-public abstract class ConfigProperty
