5db8c50ab7f29a6c3f53e884490db32c97f95ab5
Jeff Hudson <jeffryderhudson@gmail.com>
2025-08-16 11:21:16 -0600

    Remove environment variable supplier (#704)

--- kairo-environment-variable-supplier/testing/src/main/kotlin/kairo/environmentVariableSupplier/NoopEnvironmentVariableSupplier.kt
+++
@@ -1,7 +0,0 @@
-package kairo.environmentVariableSupplier
-
-public class NoopEnvironmentVariableSupplier : EnvironmentVariableSupplier() {
-  override fun get(name: String): Nothing {
-    throw NotImplementedError("This environment variable supplier is no-op.")
-  }
-}
