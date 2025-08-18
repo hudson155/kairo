3eae7d06d537324314a44d56d711338fd968c56b
Jeff Hudson <jeff@highbeam.co>
2025-08-17 21:06:08 -0600

    Allow more Ktor configuration (and better defaults)

--- kairo-rest/src/main/kotlin/kairo/rest/KtorServerFactory.kt
+++ kairo-rest/src/main/kotlin/kairo/rest/KtorServerFactory.kt
@@ -8,6 +8,7 @@ import io.ktor.server.engine.applicationEnvironment
 import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.netty.NettyApplicationEngine
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.resources.Resources
@@ -20,18 +21,29 @@ internal object KtorServerFactory {
   fun create(
     config: RestFeatureConfig,
     features: List<Feature>,
+    ktorConfiguration: NettyApplicationEngine.Configuration.() -> Unit = {},
   ): KtorServer =
     embeddedServer(
       factory = Netty,
       environment = applicationEnvironment(),
       configure = {
-        // TODO: Configure this.
+        runningLimit = config.parallelism.runningLimit
+        shareWorkGroup = config.parallelism.shareWorkGroup
+        config.parallelism.connectionGroupSize?.let { connectionGroupSize = it }
+        config.parallelism.workerGroupSize?.let { workerGroupSize = it }
+        config.parallelism.callGroupSize?.let { callGroupSize = it }
+        requestReadTimeoutSeconds = config.timeouts.requestRead.inWholeSeconds.toInt()
+        responseWriteTimeoutSeconds = config.timeouts.responseWrite.inWholeSeconds.toInt()
+        maxInitialLineLength = 8 * 1024
+        maxHeaderSize = 16 * 1024
+        maxChunkSize = 8 * 1024
+        shutdownGracePeriod = config.lifecycle.shutdownGracePeriod.inWholeMilliseconds
+        shutdownTimeout = config.lifecycle.shutdownTimeout.inWholeMilliseconds
         connector {
           host = config.connector.host
           port = config.connector.port
         }
-        shutdownGracePeriod = config.lifecycle.shutdownGracePeriod.inWholeMilliseconds
-        shutdownTimeout = config.lifecycle.shutdownTimeout.inWholeMilliseconds
+        ktorConfiguration()
       },
       module = {
         // TODO: Configure this.
