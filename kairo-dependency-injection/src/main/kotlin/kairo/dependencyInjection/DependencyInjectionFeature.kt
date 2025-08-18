0af8574ddac1ed4a7b3f0b76ae78efefbb00287a
Jeff Hudson <jeff@highbeam.co>
2025-08-18 09:04:00 -0600

    KDoc

--- kairo-dependency-injection/src/main/kotlin/kairo/dependencyInjection/DependencyInjectionFeature.kt
+++ kairo-dependency-injection/src/main/kotlin/kairo/dependencyInjection/DependencyInjectionFeature.kt
@@ -5,6 +5,10 @@ import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.KoinAppDeclaration
 
+/**
+ * The Dependency Injection Feature enables Koin for dependency injection,
+ * running the Koin application for the lifecycle of the Server.
+ */
 public class DependencyInjectionFeature(
   private val block: KoinAppDeclaration,
 ) : Feature() {
