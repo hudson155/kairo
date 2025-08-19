dca94ab05bdf8d4c9d22b8e28150e495a278c3e2
Jeff Hudson <jeff@highbeam.co>
2025-08-19 08:57:35 -0600

    Support more lifecycle events

--- kairo-feature/src/main/kotlin/kairo/feature/LifecycleEventListener.kt
+++ kairo-feature/src/main/kotlin/kairo/feature/LifecycleEventListener.kt
@@ -2,13 +2,39 @@ package kairo.feature
 
 /**
  * These methods are called when a lifecycle event is fired.
- * These methods are called concurrently with other Features,
- * so THEY MUST BE THREAD/COROUTINE-SAFE.
  */
 public interface LifecycleEventListener {
+  /**
+   * Does the work of starting each Feature.
+   *
+   * This method is called concurrently with other Features,
+   * so IT MUST BE THREAD/COROUTINE-SAFE.
+   */
   public suspend fun start(features: List<Feature>): Unit =
     Unit
 
+  /**
+   * Informs Features that start is complete.
+   * Should only do trivial work (not suspend).
+   * Most Features shouldn't need this.
+   */
+  public fun afterStart(): Unit =
+    Unit
+
+  /**
+   * Informs Features that stop is about to be called.
+   * Should only do trivial work (not suspend).
+   * Most Features shouldn't need this.
+   */
+  public fun beforeStop(): Unit =
+    Unit
+
+  /**
+   * Does the work of stopping each Feature.
+   *
+   * This method is called concurrently with other Features,
+   * so IT MUST BE THREAD/COROUTINE-SAFE.
+   */
   public suspend fun stop(): Unit =
     Unit
 }
