2d73e5d0e5bc8954cf07b450a232eeb2589b42ea
Jeff Hudson <jeff@highbeam.co>
2025-08-25 14:03:35 -0600

    Self-review

--- kairo-rest/src/main/kotlin/kairo/rest/RestEndpointHandler.kt
+++ kairo-rest/src/main/kotlin/kairo/rest/RestEndpointHandler.kt
@@ -3,14 +3,13 @@ package kairo.rest
 import io.ktor.server.routing.Routing
 import kotlin.reflect.KClass
 
-public class RestEndpointHandler<I : Any, O : Any, E : RestEndpoint<I, O>>() {
-  public var handle: (suspend (endpoint: E) -> O)? = null
+public class RestEndpointHandler<I : Any, O : Any, E : RestEndpoint<I, O>> {
+  internal var handle: (suspend (endpoint: E) -> O)? = null
 
   public fun handle(handle: suspend (endpoint: E) -> O) {
+    // TODO: Verify handle is set exactly once.
     this.handle = handle
   }
-
-  // TODO: Verify handle is set exactly once.
 }
 
 @Suppress("unused") // TODO: Don't do this.
