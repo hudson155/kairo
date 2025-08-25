798bdd6d8b53dcdcaa993be8e6ec86ffb1893842
Jeff Hudson <jeff@highbeam.co>
2025-08-25 13:34:32 -0600

    No serializ annot

--- kairo-health-check/feature/src/main/kotlin/kairo/healthCheck/feature/HealthCheckApi.kt
+++ kairo-health-check/feature/src/main/kotlin/kairo/healthCheck/feature/HealthCheckApi.kt
@@ -1,16 +1,13 @@
 package kairo.healthCheck.feature
 
 import kairo.rest.RestEndpoint
-import kotlinx.serialization.Serializable
 
 // TODO: Add tests for this resource.
 public object HealthCheckApi {
-  @Serializable
   @RestEndpoint.Method("GET")
   @RestEndpoint.Path("/health/liveness")
   public data object Liveness : RestEndpoint<Unit, Unit>()
 
-  @Serializable
   @RestEndpoint.Method("GET")
   @RestEndpoint.Path("/health/readiness")
   public data object Readiness : RestEndpoint<Unit, HealthCheckRep>()
