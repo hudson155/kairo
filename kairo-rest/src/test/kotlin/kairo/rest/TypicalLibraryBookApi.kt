a9fad3b31b275386a3de6fee898ad7972a46f2b3
Jeff Hudson <jeff@highbeam.co>
2025-08-25 14:20:58 -0600

    Base test files

---
+++ kairo-rest/src/test/kotlin/kairo/rest/TypicalLibraryBookApi.kt
@@ -0,0 +1,59 @@
+package kairo.rest
+
+/**
+ * This API is used by many tests within the REST Feature.
+ * Verify all usages if making changes.
+ */
+internal object TypicalLibraryBookApi {
+  @RestEndpoint.Method("GET")
+  @RestEndpoint.Path("/library-books/:libraryBookId")
+  @RestEndpoint.Accept("application/json")
+  internal data class Get(
+    @PathParam val libraryBookId: KairoId,
+  ) : RestEndpoint<Nothing, LibraryBookRep>()
+
+  @RestEndpoint.Method("GET")
+  @RestEndpoint.Path("/library-books")
+  @RestEndpoint.Accept("application/json")
+  internal data object ListAll : RestEndpoint<Nothing, List<LibraryBookRep>>()
+
+  @RestEndpoint.Method("GET")
+  @RestEndpoint.Path("/library-books")
+  @RestEndpoint.Accept("application/json")
+  internal data class SearchByIsbn(
+    @QueryParam val isbn: String,
+    @QueryParam val strict: Boolean?,
+  ) : RestEndpoint<Nothing, List<LibraryBookRep>>()
+
+  @RestEndpoint.Method("GET")
+  @RestEndpoint.Path("/library-books")
+  @RestEndpoint.Accept("application/json")
+  internal data class SearchByText(
+    @QueryParam val title: String?,
+    @QueryParam val author: String?,
+  ) : RestEndpoint<Nothing, List<LibraryBookRep>>()
+
+  @RestEndpoint.Method("POST")
+  @RestEndpoint.Path("/library-books")
+  @RestEndpoint.ContentType("application/json")
+  @RestEndpoint.Accept("application/json")
+  internal data class Create(
+    override val body: LibraryBookRep.Creator,
+  ) : RestEndpoint<LibraryBookRep.Creator, LibraryBookRep>()
+
+  @RestEndpoint.Method("PATCH")
+  @RestEndpoint.Path("/library-books/:libraryBookId")
+  @RestEndpoint.ContentType("application/json")
+  @RestEndpoint.Accept("application/json")
+  internal data class Update(
+    @PathParam val libraryBookId: KairoId,
+    override val body: LibraryBookRep.Update,
+  ) : RestEndpoint<LibraryBookRep.Update, LibraryBookRep>()
+
+  @RestEndpoint.Method("DELETE")
+  @RestEndpoint.Path("/library-books/:libraryBookId")
+  @RestEndpoint.Accept("application/json")
+  internal data class Delete(
+    @PathParam val libraryBookId: KairoId,
+  ) : RestEndpoint<Nothing, LibraryBookRep>()
+}
