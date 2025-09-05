2cb0f6c46b5becd5b34b1802ed5fce5414aa25e6
Jeff Hudson <jeff@highbeam.co>
2025-09-04 18:01:52 -0600

    Make test descriptions optional

--- kairo-testing/src/main/kotlin/kairo/testing/Testing.kt
+++ kairo-testing/src/main/kotlin/kairo/testing/Testing.kt
@@ -7,7 +7,7 @@ import kotlin.contracts.contract
 @OptIn(ExperimentalContracts::class)
 public inline fun <T, R> T.setup(
   @Suppress("UNUSED_PARAMETER")
-  description: String, // For readability on the caller's side.
+  description: String? = null, // For readability on the caller's side.
   block: T.() -> R,
 ): R {
   contract {
@@ -19,7 +19,7 @@ public inline fun <T, R> T.setup(
 @OptIn(ExperimentalContracts::class)
 public inline fun <T, R> T.precondition(
   @Suppress("UNUSED_PARAMETER")
-  description: String, // For readability on the caller's side.
+  description: String? = null, // For readability on the caller's side.
   block: T.() -> R,
 ): R {
   contract {
@@ -31,7 +31,7 @@ public inline fun <T, R> T.precondition(
 @OptIn(ExperimentalContracts::class)
 public inline fun <T, R> T.test(
   @Suppress("UNUSED_PARAMETER")
-  description: String, // For readability on the caller's side.
+  description: String? = null, // For readability on the caller's side.
   block: T.() -> R,
 ): R {
   contract {
@@ -43,7 +43,7 @@ public inline fun <T, R> T.test(
 @OptIn(ExperimentalContracts::class)
 public inline fun <T, R> T.postcondition(
   @Suppress("UNUSED_PARAMETER")
-  description: String, // For readability on the caller's side.
+  description: String? = null, // For readability on the caller's side.
   block: T.() -> R,
 ): R {
   contract {
