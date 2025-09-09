33e4291a9ea0642f273f6fad97bb2ed5c4f32ce4
Jeff Hudson <jeff@highbeam.co>
2025-09-09 15:48:14 -0600

    Comment

--- kairo-health-check/feature/src/main/kotlin/kairo/healthCheck/HealthCheckApi.kt
+++ kairo-health-check/feature/src/main/kotlin/kairo/healthCheck/HealthCheckApi.kt
@@ -3,7 +3,6 @@ package kairo.healthCheck
 import kairo.rest.Rest
 import kairo.rest.RestEndpoint
 
-// TODO: Add tests for this resource.
 public object HealthCheckApi {
   @Rest("GET", "/health/liveness")
   public data object Liveness : RestEndpoint<Unit, Unit>()
