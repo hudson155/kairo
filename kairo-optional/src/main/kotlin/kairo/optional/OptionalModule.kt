cb4b3ec3c5242b2f62b9143063f65826c6de8f10
Jeff Hudson <jeff@highbeam.co>
2025-09-15 18:30:14 -0400

    Fix cast

--- kairo-optional/src/main/kotlin/kairo/optional/OptionalModule.kt
+++ kairo-optional/src/main/kotlin/kairo/optional/OptionalModule.kt
@@ -1,11 +1,12 @@
 package kairo.optional
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.modules.SerializersModule
 
 public val optionalModule: SerializersModule =
   SerializersModule {
     contextual(Optional::class) { typeArgumentsSerializers ->
       @Suppress("UNCHECKED_CAST")
-      OptionalSerializer(typeArgumentsSerializers[0] as OptionalSerializer<Any>)
+      OptionalSerializer(typeArgumentsSerializers[0] as KSerializer<Any>)
     }
   }
