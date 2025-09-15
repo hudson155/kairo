23e7206832f80c5ae5de7bdcdd0f477da6d16e90
Jeff Hudson <jeff@highbeam.co>
2025-09-15 18:28:52 -0400

    Don't allow nullable optionals

--- kairo-optional/src/main/kotlin/kairo/optional/OptionalModule.kt
+++ kairo-optional/src/main/kotlin/kairo/optional/OptionalModule.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.modules.SerializersModule
 public val optionalModule: SerializersModule =
   SerializersModule {
     contextual(Optional::class) { typeArgumentsSerializers ->
-      OptionalSerializer(typeArgumentsSerializers[0])
+      @Suppress("UNCHECKED_CAST")
+      OptionalSerializer(typeArgumentsSerializers[0] as OptionalSerializer<Any>)
     }
   }
