66699b81526e766d60ad5283dd0586aa5756aa6a
Jeff Hudson <jeff@highbeam.co>
2025-09-17 15:57:48 -0400

    KDoc

--- kairo-feature/src/main/kotlin/kairo/feature/LifecycleHandler.kt
+++ kairo-feature/src/main/kotlin/kairo/feature/LifecycleHandler.kt
@@ -6,11 +6,23 @@ public class LifecycleHandler internal constructor(
   private val handleStart: (suspend (features: List<Feature>) -> Unit)?,
   private val handleStop: (suspend (features: List<Feature>) -> Unit)?,
 ) {
+  /**
+   * Does the work of starting each Feature.
+   *
+   * This method is called concurrently with other Features of the same [priority],
+   * so IT MUST BE THREAD/COROUTINE-SAFE.
+   */
   public suspend fun start(features: List<Feature>) {
     handleStart ?: return
     handleStart(features)
   }
 
+  /**
+   * Does the work of stopping each Feature.
+   *
+   * This method is called concurrently with other Features of the same [priority],
+   * so IT MUST BE THREAD/COROUTINE-SAFE.
+   */
   public suspend fun stop(features: List<Feature>) {
     handleStop ?: return
     handleStop(features)
