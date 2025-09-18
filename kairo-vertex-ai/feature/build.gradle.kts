5e60c19cc1641d279471273f330a001ea63df786
Jeff Hudson <jeff@highbeam.co>
2025-09-18 15:44:20 -0400

    Write first eval

--- kairo-vertex-ai/feature/build.gradle.kts
+++ kairo-vertex-ai/feature/build.gradle.kts
@@ -9,4 +9,9 @@ dependencies {
   api(project(":kairo-feature"))
   implementation(project(":kairo-serialization"))
   api(project(":kairo-vertex-ai"))
+
+  testImplementation(project(":kairo-testing"))
+  testImplementation(project(":kairo-vertex-ai:testing"))
+
+  testImplementation(libs.serialization.json)
 }
--- kairo-vertex-ai/feature/src/main/kotlin/kairo/vertexAi/VertexAiClientFactory.kt
+++ kairo-vertex-ai/feature/src/main/kotlin/kairo/vertexAi/VertexAiClientFactory.kt
@@ -3,6 +3,16 @@ package kairo.vertexAi
 import com.google.genai.Client
 
 internal object VertexAiClientFactory {
+  @Suppress("ForbiddenMethodCall")
+  fun fromEnvironment(block: Client.Builder.() -> Unit = {}): Client =
+    create(
+      config = VertexAiFeatureConfig(
+        project = System.getenv("GCP_PROJECT"),
+        location = System.getenv("GCP_LOCATION"),
+      ),
+      block = block,
+    )
+
   fun create(
     config: VertexAiFeatureConfig,
     block: Client.Builder.() -> Unit,
