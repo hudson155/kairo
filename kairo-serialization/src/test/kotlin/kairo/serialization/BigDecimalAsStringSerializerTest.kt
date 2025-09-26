72515e4e6253a4a695ba49e77a428d0da80132b6
Jeff Hudson <jeffryderhudson@gmail.com>
2025-09-26 08:55:17 -0600

    Add BigDecimal and BigInteger support (#886)

---
+++ kairo-serialization/src/test/kotlin/kairo/serialization/BigDecimalAsStringSerializerTest.kt
@@ -0,0 +1,40 @@
+package kairo.serialization
+
+import io.kotest.matchers.shouldBe
+import java.math.BigDecimal
+import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Test
+
+internal class BigDecimalAsStringSerializerTest {
+  @Serializable
+  internal data class Wrapper(
+    @Serializable(with = BigDecimalSerializer.AsString::class)
+    val value: BigDecimal,
+  )
+
+  @Test
+  fun `serialize, 0`(): Unit =
+    runTest {
+      val value = BigDecimal("0")
+      Json.encodeToString(Wrapper(value))
+        .shouldBe("""{"value":"0"}""")
+    }
+
+  @Test
+  fun `serialize, maximum precision`(): Unit =
+    runTest {
+      val value = BigDecimal("0.8172843657189237")
+      Json.encodeToString(Wrapper(value))
+        .shouldBe("""{"value":"0.8172843657189237"}""")
+    }
+
+  @Test
+  fun `serialize, exceeds maximum precision`(): Unit =
+    runTest {
+      val value = BigDecimal("0.81728436571892375")
+      Json.encodeToString(Wrapper(value))
+        .shouldBe("""{"value":"0.81728436571892375"}""")
+    }
+}
