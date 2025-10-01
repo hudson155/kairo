b2a89f897fd52b5345da20c023bbf7c45d7f872b
Jeff Hudson <jeff@highbeam.co>
2025-10-01 09:30:52 -0600

    Comment

--- kairo-image/src/main/kotlin/kairo/image/convertImage.kt
+++ kairo-image/src/main/kotlin/kairo/image/convertImage.kt
@@ -3,6 +3,9 @@ package kairo.image
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
 
+/**
+ * Convenience wrapper for Java's built-in image conversion.
+ */
 public fun convertImage(image: ByteArray, formatName: String): ByteArray {
   ByteArrayOutputStream().use { outputStream ->
     val bufferedImage = image.inputStream().use { ImageIO.read(it) }
