7420540639fde9ae6bd7623845ecef3d4cfd54ad
Jeff Hudson <jeff@highbeam.co>
2025-10-02 06:46:30 -0600

    One more test

--- kairo-util/src/test/kotlin/kairo/util/CanonicalizeTest.kt
+++ kairo-util/src/test/kotlin/kairo/util/CanonicalizeTest.kt
@@ -19,6 +19,7 @@ internal class CanonicalizeTest {
       canonicalize("maña!na").shouldBe("manana")
       canonicalize(" co-op ").shouldBe("co op")
       canonicalize("a-b/c.d_e|f+g=h@i#j:k").shouldBe("a b c d e f g h i j k")
+      canonicalize("a-/._|+=@#:k").shouldBe("a k")
       canonicalize("élève étudiante").shouldBe("eleve etudiante")
       canonicalize("中文 test").shouldBe("test")
       canonicalize("   multiple   spaces   ").shouldBe("multiple spaces")
