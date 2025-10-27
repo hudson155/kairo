d034746d92a63cdf8ae9f9a255398c211c359f28
Jeff Hudson <jeff@highbeam.co>
2025-10-27 11:07:37 -0600

    Remove logger

--- kairo-rest/src/main/kotlin/kairo/rest/auth/AuthenticationConfig.kt
+++ kairo-rest/src/main/kotlin/kairo/rest/auth/AuthenticationConfig.kt
@@ -3,16 +3,12 @@ package kairo.rest.auth
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.exceptions.JWTVerificationException
 import com.auth0.jwt.exceptions.TokenExpiredException
-import io.github.oshai.kotlinlogging.KLogger
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.auth.AuthenticationConfig
 import io.ktor.server.auth.BearerTokenCredential
 import io.ktor.server.auth.bearer
 import io.ktor.server.auth.jwt.JWTPrincipal
 import kairo.rest.exception.ExpiredJwt
 import kairo.rest.exception.JwtVerificationFailed
-
-private val logger: KLogger = KotlinLogging.logger {}
 
 public fun AuthenticationConfig.jwt(createVerifier: (credential: BearerTokenCredential) -> JWTVerifier) {
   bearer {
