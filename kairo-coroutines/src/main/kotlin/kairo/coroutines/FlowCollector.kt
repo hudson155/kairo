fb219da78175aaf0ebb1acc5016ad837b17a4e56
Jeff Hudson <jeff@highbeam.co>
2025-10-29 11:33:18 -0600

    KDoc

--- kairo-coroutines/src/main/kotlin/kairo/coroutines/FlowCollector.kt
+++ kairo-coroutines/src/main/kotlin/kairo/coroutines/FlowCollector.kt
@@ -4,6 +4,9 @@ import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.emitAll
 
+/**
+ * Emits all values to the collector.
+ */
 public suspend fun <T> FlowCollector<T>.emitAll(collection: Iterable<T>) {
   emitAll(collection.asFlow())
 }
