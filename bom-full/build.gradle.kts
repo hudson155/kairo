17082f2277dab6a07b6d845bb408dea5f8c67b99
Jeff Hudson <jeff@highbeam.co>
2025-11-03 23:31:03 -0700

    GCP

--- bom-full/build.gradle.kts
+++ bom-full/build.gradle.kts
@@ -67,6 +67,11 @@ dependencies {
   val mockkVersion = "1.14.5"
   constraints.api("io.mockk:mockk:$mockkVersion")
 
+  // postgres-gcp
+  // https://github.com/GoogleCloudPlatform/cloud-sql-jdbc-socket-factory/releases
+  val postgresGcpVersion = "1.27.0"
+  constraints.api("com.google.cloud.sql:cloud-sql-connector-r2dbc-postgres:$postgresGcpVersion")
+
   // postgres-jdbc
   // https://github.com/pgjdbc/pgjdbc/releases
   val postgresJdbcVersion = "42.7.7"
