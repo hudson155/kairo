586d3f29da83b96263553346c1a2844742828102
Jeff Hudson <jeff@highbeam.co>
2025-11-05 09:05:52 -0700

    Upgrade Arrow to 2.2.0

--- bom-full/build.gradle.kts
+++ bom-full/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
 
   // arrow
   // https://github.com/arrow-kt/arrow/releases
-  api(platform("io.arrow-kt:arrow-stack:2.1.2"))
+  api(platform("io.arrow-kt:arrow-stack:2.2.0"))
 
   // coroutines
   // https://github.com/Kotlin/kotlinx.coroutines/releases
