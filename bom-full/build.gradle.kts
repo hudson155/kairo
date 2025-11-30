83b7621465f9c25404be446d47125c9277d5a94e
Jeff Hudson <jeff@highbeam.co>
2025-11-30 10:01:37 -0700

    Upgrade Ktor to 3.3.3

--- bom-full/build.gradle.kts
+++ bom-full/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 
   // ktor
   // https://github.com/ktorio/ktor/releases
-  api(platform("io.ktor:ktor-bom:3.3.2"))
+  api(platform("io.ktor:ktor-bom:3.3.3"))
 
   // log4j
   // https://github.com/apache/logging-log4j2/releases
