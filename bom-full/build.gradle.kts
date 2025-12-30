8539d3685483cddbe16f956cbee7d2ad2ef5ea58
Jeff Hudson <jeff@highbeam.co>
2025-12-29 19:43:19 -0700

    Upgrade Kotest to 1.14.7

--- bom-full/build.gradle.kts
+++ bom-full/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
 
   // mockk
   // https://github.com/mockk/mockk/releases
-  val mockkVersion = "1.14.6"
+  val mockkVersion = "1.14.7"
   constraints.api("io.mockk:mockk:$mockkVersion")
 
   // moneta
