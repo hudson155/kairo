6a90c9ccb1c67862c39b52aa82fc8c7fcc1f1ea2
Jeff Hudson <jeffryderhudson@gmail.com>
2025-12-29 19:56:46 -0700

    Upgrade Testcontainers to 2.0.3 (#1034)

--- bom-full/build.gradle.kts
+++ bom-full/build.gradle.kts
@@ -125,5 +125,5 @@ dependencies {
 
   // testcontainers
   // https://github.com/testcontainers/testcontainers-java/releases
-  api(platform("org.testcontainers:testcontainers-bom:2.0.2"))
+  api(platform("org.testcontainers:testcontainers-bom:2.0.3"))
 }
