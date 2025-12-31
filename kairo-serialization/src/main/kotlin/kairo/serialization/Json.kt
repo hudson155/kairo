8d22bfa55b88236a204c6e7795dcaf47465bf4c4
Jeff Hudson <jeff@highbeam.co>
2025-12-30 22:05:22 -0700

    Disable

--- kairo-serialization/src/main/kotlin/kairo/serialization/Json.kt
+++ kairo-serialization/src/main/kotlin/kairo/serialization/Json.kt
@@ -3,7 +3,6 @@ package kairo.serialization
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonBuilder
 
-@Suppress("ForbiddenMethodCall")
 public fun json(prettyPrint: Boolean = false, builder: JsonBuilder.() -> Unit = {}): Json =
   Json {
     kairo()
