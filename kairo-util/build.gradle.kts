5ee371c4264bc96295eb54ff2ce21b902e8afe84
Jeff Hudson <jeff@highbeam.co>
2025-12-30 22:31:17 -0700

    Update util docs

--- kairo-util/build.gradle.kts
+++ kairo-util/build.gradle.kts
@@ -5,11 +5,20 @@ plugins {
 
 kotlin {
   compilerOptions {
+    /**
+     * This allows us to define helper functions that don't need to be explicitly imported.
+     * The kotlin package should be used sparingly.
+     */
     freeCompilerArgs.add("-Xallow-kotlin-package")
   }
 }
 
 dependencies {
+  /**
+   * Guava is a large library.
+   * We only include it for compilation so that if a project isn't using it at runtime,
+   * kairo-util doesn't pull it in unnecessarily.
+   */
   compileOnly(libs.guava)
 
   testImplementation(project(":kairo-testing"))
