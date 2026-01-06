632f926b5cb037c78fafd126fd87d1cf5b663433
Jeff Hudson <jeffryderhudson@gmail.com>
2026-01-05 23:37:14 -0700

    Write tests for serialization config options (#1061)

---
+++ kairo-serialization/src/test/kotlin/kairo/serialization/PrettySerializationTest.kt
@@ -0,0 +1,63 @@
+package kairo.serialization
+
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+internal class PrettySerializationTest {
+  internal data class DataClass(
+    val boolean: Boolean,
+    val ints: List<Int>,
+    val nested: Nested,
+  ) {
+    internal data class Nested(
+      val string: String,
+    )
+  }
+
+  @Test
+  fun `serialize, default`(): Unit =
+    runTest {
+      val json = KairoJson()
+
+      json.serialize(
+        DataClass(
+          boolean = true,
+          ints = listOf(1, 2, 3),
+          nested = DataClass.Nested("Hello, World!"),
+        ),
+      ).shouldBe(
+        """{"boolean":true,"ints":[1,2,3],"nested":{"string":"Hello, World!"}}""",
+      )
+    }
+
+  @Test
+  fun `serialize, pretty`(): Unit =
+    runTest {
+      val json = KairoJson {
+        pretty = true
+      }
+
+      json.serialize(
+        DataClass(
+          boolean = true,
+          ints = listOf(1, 2, 3),
+          nested = DataClass.Nested("Hello, World!"),
+        ),
+      ).shouldBe(
+        """
+          {
+            "boolean": true,
+            "ints": [
+              1,
+              2,
+              3
+            ],
+            "nested": {
+              "string": "Hello, World!"
+            }
+          }
+        """.trimIndent(),
+      )
+    }
+}
