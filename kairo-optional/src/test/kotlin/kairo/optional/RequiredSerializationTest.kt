1eab65a27793d0abb476b010044cd8334a171dea
Jeff Hudson <jeff@highbeam.co>
2026-01-08 12:25:31 -0700

    More tests

--- kairo-optional/src/test/kotlin/kairo/optional/RequiredSerializationTest.kt
+++ kairo-optional/src/test/kotlin/kairo/optional/RequiredSerializationTest.kt
@@ -3,6 +3,7 @@ package kairo.optional
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldStartWith
 import kairo.serialization.KairoJson
@@ -56,5 +57,27 @@ internal class RequiredSerializationTest {
     runTest {
       json.deserialize<Wrapper>("""{"value":"some value"}""")
         .shouldBe(Wrapper(Required.Value("some value")))
+    }
+
+  @Test
+  fun `deserialize, wrong type (object)`(): Unit =
+    runTest {
+      shouldThrowExactly<MismatchedInputException> {
+        json.deserialize<String>("""{"value":{}}""")
+      }.message.shouldStartWith(
+        "Cannot deserialize value of type `java.lang.String`" +
+          " from Object value",
+      )
+    }
+
+  @Test
+  fun `deserialize, wrong type (array)`(): Unit =
+    runTest {
+      shouldThrowExactly<MismatchedInputException> {
+        json.deserialize<Wrapper>("""{"value":[]}""")
+      }.message.shouldStartWith(
+        "Cannot deserialize value of type `java.lang.String`" +
+          " from Array value",
+      )
     }
 }
