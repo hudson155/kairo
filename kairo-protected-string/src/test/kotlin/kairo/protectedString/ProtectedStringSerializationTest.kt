e6a819125746e8861b2fed355d1d10d51443c977
Jeff Hudson <jeff@highbeam.co>
2026-01-08 12:55:42 -0700

    More tests

--- kairo-protected-string/src/test/kotlin/kairo/protectedString/ProtectedStringSerializationTest.kt
+++ kairo-protected-string/src/test/kotlin/kairo/protectedString/ProtectedStringSerializationTest.kt
@@ -1,6 +1,11 @@
 package kairo.protectedString
 
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldStartWith
 import kairo.serialization.KairoJson
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
@@ -12,14 +17,49 @@ internal class ProtectedStringSerializationTest {
   @Test
   fun serialize(): Unit =
     runTest {
-      json.serialize(ProtectedString("1"))
-        .shouldBe("\"1\"")
+      json.serialize(ProtectedString("Hello, World!"))
+        .shouldBe("\"Hello, World!\"")
     }
 
   @Test
   fun deserialize(): Unit =
     runTest {
-      json.deserialize<ProtectedString>("\"1\"")
-        .shouldBe(ProtectedString("1"))
+      json.deserialize<ProtectedString>("\"Hello, World!\"")
+        .shouldBe(ProtectedString("Hello, World!"))
+    }
+
+  @Test
+  fun `deserialize, null`(): Unit =
+    runTest {
+      shouldThrowExactly<RuntimeJsonMappingException> {
+        json.deserialize<ProtectedString>("null")
+      }.message.shouldStartWith(
+        "Deserialized value did not match the specified type" +
+          "; specified kairo.protectedString.ProtectedString(non-null)" +
+          " but was null",
+      )
+
+      json.deserialize<ProtectedString?>("null").shouldBeNull()
+    }
+
+  @Test
+  fun `deserialize, wrong type (object)`(): Unit =
+    runTest {
+      shouldThrowExactly<MismatchedInputException> {
+        json.deserialize<ProtectedString>("""{}""")
+      }.message.shouldStartWith(
+        "Cannot construct instance of `kairo.protectedString.ProtectedString`",
+      )
+    }
+
+  @Test
+  fun `deserialize, wrong type (array)`(): Unit =
+    runTest {
+      shouldThrowExactly<MismatchedInputException> {
+        json.deserialize<ProtectedString>("""[]""")
+      }.message.shouldStartWith(
+        "Cannot deserialize value of type `kairo.protectedString.ProtectedString`" +
+          " from Array value",
+      )
     }
 }
