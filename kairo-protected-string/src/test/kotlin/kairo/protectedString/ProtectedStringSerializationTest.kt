fd7862f0d8ff84948b3c7e37cd3015d7beb581f7
Jeff Hudson <jeff@highbeam.co>
2026-01-08 12:36:47 -0700

    Re-configure ProtectedString to work with Jackson

--- kairo-protected-string/src/test/kotlin/kairo/protectedString/ProtectedStringSerializationTest.kt
+++ kairo-protected-string/src/test/kotlin/kairo/protectedString/ProtectedStringSerializationTest.kt
@@ -1,26 +1,25 @@
 package kairo.protectedString
 
 import io.kotest.matchers.shouldBe
-import kairo.serialization.json
+import kairo.serialization.KairoJson
 import kotlinx.coroutines.test.runTest
-import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Test
 
 @OptIn(ProtectedString.Access::class)
 internal class ProtectedStringSerializationTest {
-  private val json: Json = json()
+  private val json: KairoJson = KairoJson()
 
   @Test
   fun serialize(): Unit =
     runTest {
-      json.encodeToString(ProtectedString("1"))
+      json.serialize(ProtectedString("1"))
         .shouldBe("\"1\"")
     }
 
   @Test
   fun deserialize(): Unit =
     runTest {
-      json.decodeFromString<ProtectedString>("\"1\"")
+      json.deserialize<ProtectedString>("\"1\"")
         .shouldBe(ProtectedString("1"))
     }
 }
