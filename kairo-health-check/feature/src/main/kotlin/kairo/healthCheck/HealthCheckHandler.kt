86b781ad646fe72b0071aff01b93f36ecba39a98
Jeff Hudson <jeffryderhudson@gmail.com>
2026-01-14 19:38:36 -0500

    Minor routing fixes (#1088)

--- kairo-health-check/feature/src/main/kotlin/kairo/healthCheck/HealthCheckHandler.kt
+++ kairo-health-check/feature/src/main/kotlin/kairo/healthCheck/HealthCheckHandler.kt
@@ -14,6 +14,7 @@ internal class HealthCheckHandler(
 ) : HasRouting {
   private val healthCheckService: HealthCheckService = HealthCheckService(healthChecks, timeout)
 
+  @Suppress("LongMethod")
   override fun Application.routing() {
     routing {
       route(HealthCheckApi.Liveness::class) {
