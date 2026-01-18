3b9af849bc1c58f201007658e9a0c3bbd629ade0
Jeff Hudson <jeff@highbeam.co>
2026-01-18 08:07:33 -0700

    Fix

--- kairo-exception/testing/build.gradle.kts
+++ kairo-exception/testing/build.gradle.kts
@@ -5,5 +5,8 @@ plugins {
 
 dependencies {
   compileOnly(project(":kairo-exception")) // Forced peer dependency.
-  compileOnly(project(":kairo-testing"))
+  compileOnly(project(":kairo-testing")) // Forced peer dependency.
+
+  testImplementation(project(":kairo-exception"))
+  testImplementation(project(":kairo-testing"))
 }
