5b9c165828b71423abca22f3e9ea7abdb3098ade
Jeff Hudson <jeff@highbeam.co>
2026-01-22 12:27:31 -0700

    Restrict imports

--- kairo-server/src/main/kotlin/kairo/server/Server.kt
+++ kairo-server/src/main/kotlin/kairo/server/Server.kt
@@ -3,7 +3,6 @@ package kairo.server
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kairo.feature.Feature
-import kairo.feature.LifecycleHandler
 import kotlin.time.measureTime
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.joinAll
