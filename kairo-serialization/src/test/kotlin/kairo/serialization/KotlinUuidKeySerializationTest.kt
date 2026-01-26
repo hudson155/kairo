3f0baafe7b746149582d3e2391b1c2917b63b1e6
Jeff Hudson <jeff@highbeam.co>
2026-01-26 05:48:16 -0700

    Prefer shouldContainExactly when asserting maps

--- kairo-serialization/src/test/kotlin/kairo/serialization/KotlinUuidKeySerializationTest.kt
+++ kairo-serialization/src/test/kotlin/kairo/serialization/KotlinUuidKeySerializationTest.kt
@@ -1,5 +1,6 @@
 package kairo.serialization
 
+import io.kotest.matchers.maps.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import kotlin.uuid.Uuid
 import kotlinx.coroutines.test.runTest
@@ -19,12 +20,12 @@ internal class KotlinUuidKeySerializationTest {
   fun deserialize(): Unit =
     runTest {
       json.deserialize<Map<Uuid, String>>("""{"a042df12-b775-42b2-aeb1-5bdd4ea78dc5":"value"}""")
-        .shouldBe(mapOf(Uuid.parse("a042df12-b775-42b2-aeb1-5bdd4ea78dc5") to "value"))
+        .shouldContainExactly(mapOf(Uuid.parse("a042df12-b775-42b2-aeb1-5bdd4ea78dc5") to "value"))
 
       json.deserialize<Map<Uuid, String>>("""{"A042DF12-B775-42B2-AEB1-5BDD4EA78DC5":"value"}""")
-        .shouldBe(mapOf(Uuid.parse("a042df12-b775-42b2-aeb1-5bdd4ea78dc5") to "value"))
+        .shouldContainExactly(mapOf(Uuid.parse("a042df12-b775-42b2-aeb1-5bdd4ea78dc5") to "value"))
 
       json.deserialize<Map<Uuid, String>>("""{"a042df12b77542b2aeb15bdd4ea78dc5":"value"}""")
-        .shouldBe(mapOf(Uuid.parse("a042df12-b775-42b2-aeb1-5bdd4ea78dc5") to "value"))
+        .shouldContainExactly(mapOf(Uuid.parse("a042df12-b775-42b2-aeb1-5bdd4ea78dc5") to "value"))
     }
 }
